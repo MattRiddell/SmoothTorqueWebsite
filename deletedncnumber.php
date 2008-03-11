61398f2b94ffc849d03afab88b6b2a5ec6218c9a
matt <MattRiddell@users.noreply.github.com>
2008-03-11 23:07:04 +0000

    Remove pointless limit 1

--- deletedncnumber.php
+++ deletedncnumber.php
@@ -5,11 +5,11 @@ mysql_select_db("SineDialer", $link);
 $_POST = array_map(mysql_real_escape_string,$_POST);
 $_GET = array_map(mysql_real_escape_string,$_GET);
 
-/* 
+/*
    seeing as we're using '=' in the comparison, we can only ever delete a single value for the database at a time,
    so might as well make it more difficult for people to delete everyting via an injection
 */
-$sql="DELETE FROM dncnumber where phonenumber=".($_GET[number])."limit 1";
+$sql="DELETE FROM dncnumber where phonenumber=".($_GET[number]);
 $result=mysql_query($sql, $link) or die (mysql_error());;
 include("viewdncnumbers.php");
 exit;
