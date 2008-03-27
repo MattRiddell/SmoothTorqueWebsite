a50ce4f01662f91bcf3131c4814f17bd25215199
matt <MattRiddell@users.noreply.github.com>
2008-03-27 20:56:27 +0000

    Something strange is going on

--- viewcdr.php
+++ viewcdr.php
@@ -6,16 +6,16 @@ $db_user=$config_values['CDR_USER'];
 $db_pass=$config_values['CDR_PASS'];
 if ($level==sha1("level100")){
     if (isset($_GET[accountcode])) {
-        $accountcode = $_GET[accountcode];
+        $accountcode_in = $_GET[accountcode];
     } else {
-        $accountcode = "stl-".$_COOKIE[user];
+        $accountcode_in = "stl-".$_COOKIE[user];
     }
 } else {
-    $accountcode = "stl-".$_COOKIE[user];
+    $accountcode_in = "stl-".$_COOKIE[user];
 }
 $cdrlink = mysql_connect($db_host, $db_user, $db_pass) OR die(mysql_error());
 mysql_select_db($config_values['CDR_DB'], $cdrlink);
-$sql = "SELECT count(*) from ".$config_values['CDR_TABLE']." WHERE dcontext!='default' and dcontext!='load-simulation' and dcontext!='staff' and dcontext!='ls3' and userfield!='' and accountcode='$accountcode'";
+$sql = "SELECT count(*) from ".$config_values['CDR_TABLE']." WHERE dcontext!='default' and dcontext!='load-simulation' and dcontext!='staff' and dcontext!='ls3' and userfield!='' and accountcode='$accountcode_in'";
 $result = mysql_query($sql,$cdrlink);
 $count = mysql_result($result,0,0);
 //echo $count." Total Records";
@@ -47,7 +47,7 @@ for ($i = $pagex;$i<($count/100);$i++) {
 echo '<a href="viewcdr.php?page='.($page+1).'"><img src="/images/resultset_next.png" border="0"></a> ';
 echo '<a href="viewcdr.php?page='.round($count/100).'"><img src="/images/resultset_last.png" border="0"></a> ';
 //$sql = "SELECT * from ".$config_values['CDR_TABLE']." order by calldate DESC LIMIT $start,100";
-$sql = "SELECT * from ".$config_values['CDR_TABLE']." WHERE dcontext!='default' and dcontext!='load-simulation' and dcontext!='staff' and dcontext!='ls3' and userfield!='' and accountcode='$accountcode' order by calldate DESC LIMIT $start,100";
+$sql = "SELECT * from ".$config_values['CDR_TABLE']." WHERE dcontext!='default' and dcontext!='load-simulation' and dcontext!='staff' and dcontext!='ls3' and userfield!='' and accountcode='$accountcode_in' order by calldate DESC LIMIT $start,100";
 
 $result = mysql_query($sql,$cdrlink);
 $i = 0;
