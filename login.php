32c84cef3990fa63ad5a41e88385f324656de5e6
matt <MattRiddell@users.noreply.github.com>
2008-03-27 01:20:43 +0000

    Changes for logging

--- login.php
+++ login.php
@@ -8,7 +8,7 @@ $passwordHash = sha1($_POST['pass']);
 $_POST = array_map(mysql_real_escape_string,$_POST);
 $_GET = array_map(mysql_real_escape_string,$_GET);
 
-$sql = "INSERT INTO log (username, activity) VALUES ('$_POST[user]', 'Attempted login')";
+$sql = "INSERT INTO log (timestamp, username, activity) VALUES (NOW(), '$_POST[user]', 'Attempted login')";
 $result=mysql_query($sql, $link);
 if (mysql_error() == "Table 'SineDialer.log' doesn't exist") {
 $sql = "
@@ -18,7 +18,7 @@ CREATE TABLE `log` (
   `username` varchar(255) default NULL
 )";
 $result=mysql_query($sql, $link);
-$sql = "INSERT INTO log (username, activity) VALUES ('$_POST[user]', 'Attempted login')";
+$sql = "INSERT INTO log (timestamp, username, activity) VALUES (NOW(), '$_POST[user]', 'Attempted login')";
 $result=mysql_query($sql, $link);
 
 }
@@ -30,6 +30,7 @@ $sql = 'SELECT password, security FROM customer WHERE username=\''.$_POST[user].
 $result=mysql_query($sql, $link) or die (mysql_error());;
 $dbpass=mysql_result($result,0,'password');
 if (trim($dbpass)==trim($passwordHash)){
+
     setcookie("loggedin",sha1("LoggedIn".$_POST[user]),time()+6000);
     setcookie("user",$_POST[user],time()+6000);
     if (mysql_result($result,0,'security')==100){
@@ -38,6 +39,8 @@ if (trim($dbpass)==trim($passwordHash)){
         $level=sha1("level0");
     }
     setcookie("level",$level,time()+6000);
+    $sql = "INSERT INTO log (timestamp, username, activity) VALUES (NOW(), '$_POST[user]', 'Successful login')";
+    $result=mysql_query($sql, $link);
     if (strlen($_GET[redirect]) > 0) {
         header("Location: ".$_GET[redirect]);
     } else {
@@ -47,6 +50,8 @@ if (trim($dbpass)==trim($passwordHash)){
 } else {
     setcookie("loggedin","--",time()+6000);
     setcookie("user",$_POST[user],time()+6000);
+    $sql = "INSERT INTO log (timestamp, username, activity) VALUES (NOW(), '$_POST[user]', 'Unuccessful login')";
+    $result=mysql_query($sql, $link);
     header("Location: index.php?error=Incorrect%20UserName%20or%20Password");
     exit;
 ?>
