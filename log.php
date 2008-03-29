790e94d863ca07ff0305a807e17b37230a9c107c
matt <MattRiddell@users.noreply.github.com>
2008-03-29 13:01:57 +0000

    Added viewed the system logs log message

--- log.php
+++ log.php
@@ -1,5 +1,9 @@
 <?
     require "header.php";
+/*================= Log Access ======================================*/
+$sql = "INSERT INTO log (timestamp, username, activity) VALUES (NOW(), '$_COOKIE[user]', 'Viewed the system logs')";
+$result=mysql_query($sql, $link);
+/*================= Log Access ======================================*/
     $sql = "select * from log order by timestamp desc";
     $result=mysql_query($sql, $link);
     echo "<center><table border=\"1\">";
@@ -13,4 +17,3 @@
     }
     echo "</table>";
 ?>
-
