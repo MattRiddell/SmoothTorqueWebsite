b82fe25f6cc3ed37061a084352c59ed8a00088f3
matt <MattRiddell@users.noreply.github.com>
2008-03-30 23:37:10 +0000

    Help stuff

---
+++ help/index.php
@@ -0,0 +1,11 @@
+<html>
+<head><title>SmoothTorque Linux Help Manual<title></head>
+<body>
+<?php
+
+echo "<H2>Quick Start</h2>";
+include 'src/quickstart.php'
+
+?>
+</body>
+</html>
---
+++ help/src/quickstart.php
@@ -0,0 +1,16 @@
+<h3>Introduction</h3>
+<p>The aim of this part is to provide a quick introduction for SmoothTorque. This quick start will show you how to create campaigns. For this we're going to create a simple Load Simulation campaign.</p>
+
+<h3>Create New Campaign</h3>
+<p>Click on the Campaigns tab. When you first login there are no campaigns created for you. We're going to create one now. Click on the Add Campaign button. The next screen has a form that needs to be filled out. Follow the example in the image below.</p>
+
+<img src="images/add_campaign.png" alt="Add campaign">
+
+<p>Once the values are filled in, then click on the Add Campaign button. You'll then be presented with the screen below.</p>
+
+<img src="images/campaign_status.png" alt="The status of a campaign">
+
+<p>What each of the different icons do will be explained later on.</p>
+
+<h3>Add Numbers</h3>
+
