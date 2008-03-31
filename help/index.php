1947a086473c2e7b7c73a31fb5fd64e9ad23e9f7
matt <MattRiddell@users.noreply.github.com>
2008-03-31 03:23:05 +0000

    Changes

--- help/index.php
+++ help/index.php
@@ -2,11 +2,13 @@
 <head><title>SmoothTorque Linux Help Manual<title></head>
 <body>
 <?php
+echo '<h1>Normal User</h1>';
+echo '<H2>Quick Start</h2>';
+echo '<div id="quick_start">';
+include 'src/quickstart.php';
+echo '</div>';
+echo '<h2>Messages</h2>';
 
-echo "<H2>Quick Start</h2>";
-//echo "<div id=\"quick_start\">";
-include 'src/quickstart.php'
-//echo "</div>";
 
 ?>
 </body>
