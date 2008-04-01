eb56bad9555562be19b9333d363a458a14d2c593
matt <MattRiddell@users.noreply.github.com>
2008-04-01 03:59:01 +0000

    as before, these files weren't added before the commit

---
+++ help/src/all_numbers.php
@@ -0,0 +1,62 @@
+<h3>Number Lists</h3>
+<p>There are several different ways to get numbers in to a campaign. For each of these you will need to select which campaign you want to add the numbers to. The following table presents these different options.</p>
+
+<ul>
+	<li>System Lists</li>
+	<li>Upload from a text file</li>
+	<li>Add numbers manually</li>
+	<li>Generate numbers automatically</li>
+</ul>
+
+<h4>System Lists</h4>
+<p>These lists are provided by an administrator and my be along the lines of certain geographic regions, or similar.</p>
+
+<h4>Upload from a text file</h4>
+<p>The text file that you use must contain numbers on seperate lines.</p>
+
+<h4>Add numbers manually</h4>
+<p>Using this method you will be presented with a box where you put numbers in.</p>
+
+<h4>Generating numbers automatically</h4>
+<p>SmoothTorque generates numbers for you. See the quick start for an example of usage.</p>
+
+<h3>Other options</h3>
+<p>You can also view, search, and export number lists. When you view and export you can filter the numbers by their status. The meaning of the different status messages are outlined below.</p>
+
+<table>
+	<tr>
+		<th>Message</th>
+		<th>Meaning</th>
+	</tr>
+	<tr>
+		<td>Answered</td>
+		<td>The call was answered</td>
+	</tr>
+	<tr>
+		<td>Pressed 1</td>
+		<td>Those called who pressed 1</td>
+	</tr>
+	<tr>
+		<td>Busy</td>
+		<td>Numbers tried that were engaged</td>
+	<tr>
+	<tr>	
+		<td>Failed</td>
+		<td>Numbers that failed for some reason, for example if the number doesn't exist</td>
+	</tr>
+	<tr>
+		<td>Answer Machine</td>
+		<td>Calls where an answer machine was detected</td>
+	</tr>
+	<tr>
+		<td>Congested</td>
+		<td>Calls that were tried but the provider was too congested to make the call</td>
+	</tr>
+	<tr>
+		<td>Unknown</td>
+		<td>Anything that doesn't fit in the above categories</td>
+	</tr>
+</table>
+
+<h3>Do Not Call (DNC) Numbers</h3>
+<p>Here similar options for DNC Number exists. You can view and add numbers (via the same text file upload and manually entering mechanisms) as for normal numbers. When you view the numbers you will be able to delete the numbers. Note: The consequences of deleting numbers from the DNC is entirely up to you. This feature is only there to aid testing.</p>
