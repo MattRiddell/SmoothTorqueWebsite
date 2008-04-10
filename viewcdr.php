2c3b8a302c8d8b357b33c604272976a7d0697b13
matt <MattRiddell@users.noreply.github.com>
2008-04-10 00:31:26 +0000

    Ok, this should work now

--- viewcdr.php
+++ viewcdr.php
@@ -10,6 +10,9 @@ From: <input name="startdate">
 <input type=button value="select" onclick="displayDatePicker('startdate', false, 'ymd', '-');"><BR>
 To: <input name="enddate">
 <input type=button value="select" onclick="displayDatePicker('enddate', false, 'ymd', '-');"><BR>
+<?if (isset($_GET[accountcode])) {?>
+<input type="hidden" name="accountcode" value="<?echo $_GET[accountcode];?>">
+<?}?>
 <input type="submit" value="Select">
 </form>
 <?
