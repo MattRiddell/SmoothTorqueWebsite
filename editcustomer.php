464de93645cfdb67415442582bca41aefc140186
matt <MattRiddell@users.noreply.github.com>
2008-04-11 03:41:10 +0000

    Changes

--- editcustomer.php
+++ editcustomer.php
@@ -2,7 +2,7 @@
 include "admin/db_config.php";//mysql_connect('localhost', 'root', '') OR die(mysql_error());
 mysql_select_db("SineDialer", $link);
 
-if (isset($_POST[name])){
+if (isset($_POST['enabled'])){
 $values = $_POST['enabled'];
 foreach ($values as $a){
     $adminlists.=$a.",";
