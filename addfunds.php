7a41b226b820d2111296861bfc16b97810bab4b2
matt <MattRiddell@users.noreply.github.com>
2008-04-21 03:16:49 +0000

    Changes

---
+++ addfunds.php
@@ -0,0 +1,146 @@
+<?
+include "admin/db_config.php";//mysql_connect('localhost', 'root', '') OR die(mysql_error());
+mysql_select_db("SineDialer", $link);
+
+$_POST = array_map(mysql_real_escape_string,$_POST);
+$_GET = array_map(mysql_real_escape_string,$_GET);
+
+$sql = 'SELECT campaigngroupid FROM customer WHERE username=\''.$_COOKIE[user].'\'';
+$result=mysql_query($sql, $link) or die (mysql_error());;
+$campaigngroupid=mysql_result($result,0,'campaigngroupid');
+
+if (isset($_POST[firstperiod])){
+    $firstperiod=$_POST[firstperiod];
+    $customerid = $_POST[id];
+    $priceperminute = $_POST[priceperminute];
+    $increment = $_POST[increment];
+    $pricepercall = $_POST[pricepercall];
+    $priceperconnectedcall = $_POST[priceperconnectedcall];
+    $priceperpress1 = $_POST[priceperpress1];
+    $credit = $_POST[credit];
+    $creditlimit = $_POST[creditlimit];
+
+
+    $sql="update billing ".
+         "set firstperiod='$firstperiod', increment='$increment', priceperminute='$priceperminute',
+         pricepercall = '$pricepercall', priceperconnectedcall='$priceperconnectedcall', priceperpress1='$priceperpress1',
+         credit='$credit', creditlimit='$creditlimit' where customerid=".$customerid;
+    $result=mysql_query($sql, $link) or die (mysql_error());;
+/*    $SMDB2->executeUpdate($sql);*/
+
+/*================= Log Access ======================================*/
+$sql = "INSERT INTO log (timestamp, username, activity) VALUES (NOW(), '$_COOKIE[user]', 'Updated a billing record for customer id: $customerid (Credit: $credit Credit Limit $creditlimit)')";
+$result=mysql_query($sql, $link);
+/*================= Log Access ======================================*/
+
+    header("Location: /customers.php");
+    exit;
+}
+
+//require "header_campaign.php";
+$pagenum="2";
+require "header.php";
+//require "header_trunk.php";
+$campaigngroupid=$groupid;
+$sql = 'SELECT * FROM billing WHERE customerid='.$_GET[id];
+$result=mysql_query($sql, $link);
+if (mysql_error()=="Table 'SineDialer.billing' doesn't exist") {
+    $sql = "CREATE TABLE `billing` (
+  `customerid` int(11) unsigned NOT NULL default '0',
+  `accountcode` varchar(250) NOT NULL default '',
+  `priceperminute` double(10,5) default '0.00000',
+  `firstperiod` int(10) unsigned default '1',
+  `increment` int(10) unsigned default '1',
+  `credit` double(100,10) default '0.0000000000',
+  `creditlimit` double(100,10) default '0.0000000000',
+  `pricepercall` double(10,5) default '0.00000',
+  `priceperconnectedcall` double(10,5) default '0.00000',
+  `priceperpress1` double(10,5) default '0.00000',
+  PRIMARY KEY  (`customerid`,`accountcode`)
+)";
+$result=mysql_query($sql, $link);
+
+
+}
+if (mysql_num_rows($result) == 0) {
+    $sql = 'SELECT * FROM customer WHERE id='.$_GET[id];
+    $result=mysql_query($sql, $link) or die (mysql_error());;
+    $accountcode = "stl-".mysql_result($result,0,"username");
+    ?>
+    <br />
+    There is no billing information for this customer yet<br />
+    <br />
+    Would you like to create a record?<br />
+    <br />
+    <a href="addbilling.php?accountcode=<?echo $accountcode."&id=".$_GET[id];?>">Yes please</a><br /><br />
+    <a href="customers.php">No thanks</a>
+    <?
+} else {
+while ($row = mysql_fetch_assoc($result)) {
+?>
+
+<FORM ACTION="billing.php" METHOD="POST">
+<table class="tborder" align="center" border="0" cellpadding="0" cellspacing="2">
+<?
+?>
+<TR><TD CLASS="thead">AccountCode</TD><TD>
+<?echo $row[accountcode];?>
+</TD>
+</TR>
+
+<TR><TD CLASS="thead">Price Per Minute</TD><TD>
+<INPUT TYPE="HIDDEN" NAME="customerid" VALUE="<?echo $groupid;?>">
+<INPUT TYPE="HIDDEN" NAME="id" VALUE="<?echo $row[customerid];?>">
+<INPUT TYPE="TEXT" NAME="priceperminute" VALUE="<?echo $row[priceperminute];?>" size="60">
+</TD>
+</TR>
+
+<TR><TD CLASS="thead">First Period</TD><TD>
+<INPUT TYPE="TEXT" NAME="firstperiod" VALUE="<?echo $row[firstperiod];?>" size="60">
+</TD>
+</TR>
+
+<TR><TD CLASS="thead">Increment</TD><TD>
+<INPUT TYPE="TEXT" NAME="increment" VALUE="<?echo $row[increment];?>" size="60">
+</TD>
+</TR>
+
+<TR><TD CLASS="thead">Price Per Lead</TD><TD>
+<INPUT TYPE="TEXT" NAME="pricepercall" VALUE="<?echo $row[pricepercall];?>" size="60">
+</TD>
+</TR>
+
+<TR><TD CLASS="thead">Price Per Connected Call</TD><TD>
+<INPUT TYPE="TEXT" NAME="priceperconnectedcall" VALUE="<?echo $row[priceperconnectedcall];?>" size="60">
+</TD>
+</TR>
+
+<TR><TD CLASS="thead">Price Per Press 1</TD><TD>
+<INPUT TYPE="TEXT" NAME="priceperpress1" VALUE="<?echo $row[priceperpress1];?>" size="60">
+</TD>
+</TR>
+
+<TR><TD CLASS="thead">Credit</TD><TD>
+<INPUT TYPE="TEXT" NAME="credit" VALUE="<?echo $row[credit];?>" size="60">
+</TD>
+</TR>
+
+<TR><TD CLASS="thead">Credit Limit (0.00 for prepay)</TD><TD>
+<INPUT TYPE="TEXT" NAME="creditlimit" VALUE="<?echo $row[creditlimit];?>" size="60">
+</TD>
+</TR>
+
+<TR><TD COLSPAN=2 ALIGN="RIGHT">
+<INPUT TYPE="SUBMIT" VALUE="Save Billing Information">
+</TD>
+</TR>
+<?
+}
+}
+?>
+
+</TABLE>
+</FORM>
+<?
+require "footer.php";
+?>
--- login.php
+++ login.php
@@ -37,6 +37,22 @@ if (!mysql_is_table($db_host,$db_user,$db_pass,"SineDialer","billing")){
 }
 
 /*======================================================================
+                            Billing Log Table
+  ======================================================================*/
+if (!mysql_is_table($db_host,$db_user,$db_pass,"SineDialer","billinglog")){
+  include "admin/db_config.php";
+
+  $sql = "CREATE TABLE `billinglog` (
+  `timestamp` timestamp NULL default NULL on update CURRENT_TIMESTAMP,
+  `activity` varchar(255) default NULL,
+  `username` varchar(255) default NULL
+  )";
+  $result = mysql_query($sql,$link);
+  $sql = "INSERT INTO log (timestamp, username, activity) VALUES (NOW(), '$_POST[user]', 'Created Billing Log Table')";
+  $result=mysql_query($sql, $link);
+}
+
+/*======================================================================
                             Log Table
   ======================================================================*/
 if (!mysql_is_table($db_host,$db_user,$db_pass,"SineDialer","log")){
@@ -148,11 +164,219 @@ if (!mysql_is_table($db_host,$db_user,$db_pass,"SineDialer","iax_buddies")){
     $result = mysql_query($sql,$link);
 
 }
-
-if (!mysql_is_table($db_host,$db_user,$db_pass,"SineDialer","iax_buddies")){
+/*======================================================================
+                            Campaign
+  ======================================================================*/
+if (!mysql_is_table($db_host,$db_user,$db_pass,"SineDialer","campaign")){
   include "admin/db_config.php";
+	$sql = "Create table `campaign` (
+	`id` int(200) NOT NULL auto_increment,
+	`description` varchar(250) default NULL,
+	`name` varchar(200) NOT NULL default '',
+	`groupid` int(200) NOT NULL default '0',
+	`messageid` int(200) NOT NULL default '0',
+	`campaignconfigid` int(11) NOT NULL default '0',
+	`messageid2` INT(200) NOT NULL unsigned default '0',
+	`messageid3` INT(200) NOT NULL unsigned default '0',
+	`astqueuename` VARCHAR(255) default NULL,
+	`mode` INT(11)  default '0',
+	`clid` varchar(255) default 'nocallerid <>',
+	`trclid` varchar(255) default 'nocallerid',
+	`maxagents` int(11) default '30',
+	`did` varchar(255) default 'nodid',
+	`context` varchar(255) default 'ls3',
+	`cost` varchar(10) default NULL,
+	PRIMARY KEY (`id`)
+	);";
+	$result = mysql_query($sql,$link);
+}
+
+/*======================================================================
+                            Campaign Config
+  ======================================================================*/
+if (!mysql_is_table($db_host,$db_user,$db_pass,"SineDialer","campaignconfig")){
+  include "admin/db_config.php";
+        $sql = "Create table `campaignconfig` (
+	`id` int(10) unsigned not null auto_increment,
+	`type` int (11) default '0',
+	`astqueuename` varchar(255) default NULL,
+	`did` varchar(255) default NULL,
+	`clid` varchar(255) default NULL,
+	`trclid` varchar(255) default NULL,
+	`maxchans` int(11) default 10,
+	`numagents` int(11) default 10,
+	PRIMARY KEY(`id`)
+        );";
+	$result = mysql_query($sql,$link);
+}
+
+/*======================================================================
+                            Campaign Message
+  ======================================================================*/
+if (!mysql_is_table($db_host,$db_user,$db_pass,"SineDialer","campaignconfig")){
+  include "admin/db_config.php";
+        $sql = "Create table `campaignmessage` (
+        `id` int(10) unsigned not null auto_increment,
+	`filename` varchar(250) not null,
+	`name` varchar(200) not null,
+	`description` varchar(250) not null,
+	`customer_id` int(11),
+	primary key(`id`)
+        );";
+        $result = mysql_query($sql,$link);
+}
 
 
+/*======================================================================
+                            CDR
+  ======================================================================*/
+if (!mysql_is_table($db_host,$db_user,$db_pass,"SineDialer","cdr")){
+  include "admin/db_config.php";
+        $sql = "Create table `cdr` (
+  `calldate` datetime NOT NULL default '0000-00-00 00:00:00',
+  `clid` varchar(80) NOT NULL default '',
+  `src` varchar(80) NOT NULL default '',
+  `dst` varchar(80) NOT NULL default '',
+  `dcontext` varchar(80) NOT NULL default '',
+  `channel` varchar(80) NOT NULL default '',
+  `dstchannel` varchar(80) NOT NULL default '',
+  `lastapp` varchar(80) NOT NULL default '',
+  `lastdata` varchar(80) NOT NULL default '',
+  `duration` int(11) NOT NULL default '0',
+  `billsec` int(11) NOT NULL default '0',
+  `disposition` varchar(45) NOT NULL default '',
+  `amaflags` int(11) NOT NULL default '0',
+  `accountcode` varchar(20) NOT NULL default '',
+  `userfield` varchar(255) NOT NULL default '',
+  `userfield2` varchar(255) NOT NULL default '',
+  `userfield3` varchar(255) NOT NULL default '',
+  `userfield4` varchar(255) NOT NULL default '',
+  `userfield5` varchar(255) NOT NULL default '',
+  KEY `calldate` (`calldate`),
+  KEY `dst` (`dst`),
+  KEY `accountcode` (`accountcode`)
+        );";
+        $result = mysql_query($sql,$link);
+}
+
+
+/*======================================================================
+                            Config
+  ======================================================================*/
+if (!mysql_is_table($db_host,$db_user,$db_pass,"SineDialer","config")){
+  include "admin/db_config.php";
+        $sql = "Create table `config` (
+`parameter` varchar(11) NOT NULL default '0',
+  `value` varchar(110) NOT NULL
+        );";
+        $result = mysql_query($sql,$link);
+}
+
+/*======================================================================
+                            Customer
+  ======================================================================*/
+if (!mysql_is_table($db_host,$db_user,$db_pass,"SineDialer","customer")){
+  include "admin/db_config.php";
+        $sql = "Create table `customer` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `username` varchar(30) NOT NULL default '',
+  `password` varchar(200) NOT NULL default '',
+  `campaigngroupid` int(10) unsigned NOT NULL default '0',
+  `address1` varchar(250) default NULL,
+  `address2` varchar(250) default NULL,
+  `city` varchar(250) default NULL,
+  `country` varchar(250) default NULL,
+  `phone` varchar(250) default NULL,
+  `email` varchar(250) default NULL,
+  `fax` varchar(250) default NULL,
+  `website` varchar(250) default NULL,
+  `security` int(3) unsigned default '0',
+  `company` varchar(250) default NULL,
+  `trunkid` int(11) default '-1',
+  `zip` varchar(25) default NULL,
+  `state` varchar(250) default NULL,
+  `maxcps` int(11) default '10',
+  `maxchans` int(11) default '100',
+  `adminlists` varchar(2555) default NULL,
+  PRIMARY KEY  (`id`)
+        );";
+        $result = mysql_query($sql,$link);
+	$result = mysql_query("INSERT INTO customer (`username`,`password`,`security`) VALUES ('matt','starsock',100)",$link);
+}
+
+/*======================================================================
+                         DNC Number
+  ======================================================================*/
+if (!mysql_is_table($db_host,$db_user,$db_pass,"SineDialer","dncnumber")){
+  include "admin/db_config.php";
+        $sql = "Create table `dncnumber` (
+  `campaignid` int(200) NOT NULL default '0',
+  `phonenumber` varchar(50) NOT NULL default '',
+  `status` varchar(50) NOT NULL default '',
+  `type` int(5) NOT NULL default '0',
+  PRIMARY KEY  (`campaignid`,`phonenumber`),
+  KEY `test` (`phonenumber`,`campaignid`)
+        );";
+        $result = mysql_query($sql,$link);
+}
+
+/*======================================================================
+                         Number
+  ======================================================================*/
+if (!mysql_is_table($db_host,$db_user,$db_pass,"SineDialer","number")){
+  include "admin/db_config.php";
+        $sql = "Create table `number` (
+  `campaignid` int(200) NOT NULL default '0',
+  `phonenumber` varchar(50) NOT NULL default '',
+  `status` varchar(50) NOT NULL default '',
+  `type` int(5) NOT NULL default '0',
+  `datetime` timestamp NULL default NULL on update CURRENT_TIMESTAMP,
+  PRIMARY KEY  (`campaignid`,`phonenumber`),
+  KEY `test` (`phonenumber`,`campaignid`)
+  KEY `status` (`campaignid`,`status`),
+  KEY `status2` (`status`)
+        );";
+        $result = mysql_query($sql,$link);
+}
+
+/*======================================================================
+                         Queue
+  ======================================================================*/
+if (!mysql_is_table($db_host,$db_user,$db_pass,"SineDialer","queue")){
+  include "admin/db_config.php";
+        $sql = "Create table `queue` (
+  `queueID` int(11) NOT NULL auto_increment,
+  `queuename` varchar(100) default NULL,
+  `status` tinyint(4) NOT NULL default '0',
+  `campaignID` int(11) NOT NULL default '0',
+  `details` varchar(250) default NULL,
+  `flags` int(11) NOT NULL default '0',
+  `transferclid` varchar(20) default '0',
+  `starttime` time default NULL,
+  `endtime` time default NULL,
+  `startdate` date default NULL,
+  `enddate` date default NULL,
+  `did` varchar(20) default NULL,
+  `clid` varchar(20) default NULL,
+  `context` int(1) NOT NULL default '0',
+  `maxcalls` int(11) default '100',
+  `maxchans` int(11) default '100',
+  `maxretries` int(11) default '0',
+  `retrytime` int(11) default '30',
+  `waittime` int(11) default '30',
+  `timespent` varchar(20) default '0',
+  `progress` varchar(20) default '0',
+  `expectedRate` float NOT NULL default '100',
+  `mode` varchar(120) default '0',
+  `astqueuename` varchar(20) default '',
+  `trunk` varchar(255) default 'Local/s@${EXTEN}',
+  `accountcode` varchar(255) default 'noaccount',
+  `trunkid` int(11) default '-1',
+  `customerID` int(11) default '-1',
+  `maxcps` int(11) default '31',
+  PRIMARY KEY  (`queueID`)
+        );";
+        $result = mysql_query($sql,$link);
 }
 
 
