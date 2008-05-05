f482038ec1fc348e38795787fd45a31f24442434
matt <MattRiddell@users.noreply.github.com>
2008-05-05 08:32:39 +0000

    Cleaner

--- view_system_bill.php
+++ view_system_bill.php
@@ -5,11 +5,8 @@ mysql_select_db("SineDialer", $link);
 $resultx = mysql_query("select distinct groupid from system_billing");
 $x = 0;
 while ($rowx = mysql_fetch_assoc($resultx)) {
-
-$result = mysql_query("select * from system_billing where groupid = ".$rowx[groupid]." order by timestamp desc LIMIT 100");
-$x = 0;
-echo "<b>Group ID: $rowx[groupid]</b><br />";
-echo '<a href="system_bill_graph.php?groupid='.$rowx[groupid].'"><img src="system_bill_graph.php?groupid='.$rowx[groupid].'" width="100" border="0"></a>';
-echo "<hr>";
+    echo "<b>Group ID: $rowx[groupid]</b><br />";
+    echo '<a href="system_bill_graph.php?groupid='.$rowx[groupid].'"><img src="system_bill_graph.php?groupid='.$rowx[groupid].'" width="100" border="0"></a>';
+    echo "<hr>";
 }
 ?>
