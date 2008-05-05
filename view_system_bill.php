af26597037c866e9d3b7541dcd5a56086883f22c
matt <MattRiddell@users.noreply.github.com>
2008-05-05 09:01:43 +0000

    Changes

--- view_system_bill.php
+++ view_system_bill.php
@@ -4,11 +4,22 @@ include "admin/db_config.php";
 mysql_select_db("SineDialer", $link);
 $resultx = mysql_query("select distinct system_billing.groupid, customer.* from system_billing left join customer on system_billing.groupid=customer.campaigngroupid");
 $x = 0;
+$highest = 0;
 while ($rowx = mysql_fetch_assoc($resultx)) {
     $result = mysql_query("select max(totalcost) from system_billing where groupid = ".$rowx[groupid]);
-    $totalcost = mysql_result($result,0,0);
-    echo "<b>Company: $rowx[company] ($totalcost)</b><br />";
-    echo '<a href="system_bill_graph.php?max=10000&groupid='.$rowx[groupid].'"><img src="system_bill_graph.php?max=10000&groupid='.$rowx[groupid].'" width="100" border="0"></a>';
+    $totalcost[$x] = mysql_result($result,0,0);
+    if ($totalcost[$x] > $highest) {
+        $highest = $totalcost[$x];
+    }
+    $company[$x] = $rowx[company];
+    $groupid[$x] = $rowx[groupid];
+    $x++;
+}
+for($i = 0;$i<$x;$i++) {
+    $totalcost_cr = $config_values['CURRENCY_SYMBOL']." ".number_format($totalcost[$i],2);
+    echo "<b>Company: $company[$i] ($totalcost_cr)</b><br />";
+    echo '<a href="system_bill_graph.php?size=300&max='.$highest.'&groupid='.$groupid[$i].'"><img src="system_bill_graph.php?size=300&max='.$highest.'&groupid='.$groupid[$i].'" width="100" border="0"></a>';
     echo "<br>";
+
 }
 ?>
