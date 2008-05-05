bbe2a1f0ee5d8ebaa25235d71721e543c1517a9f
matt <MattRiddell@users.noreply.github.com>
2008-05-05 08:36:42 +0000

    Cleaner

--- view_system_bill.php
+++ view_system_bill.php
@@ -2,11 +2,11 @@
 include "header.php";
 include "admin/db_config.php";
 mysql_select_db("SineDialer", $link);
-$resultx = mysql_query("select distinct groupid from system_billing");
+$resultx = mysql_query("select distinct system_billing.groupid, customer.* from system_billing left join customer on system_billing.groupid=customer.campaigngroupid");
 $x = 0;
 while ($rowx = mysql_fetch_assoc($resultx)) {
-    echo "<b>Group ID: $rowx[groupid]</b><br />";
+    echo "<b>Company: $rowx[company]</b><br />";
     echo '<a href="system_bill_graph.php?groupid='.$rowx[groupid].'"><img src="system_bill_graph.php?groupid='.$rowx[groupid].'" width="100" border="0"></a>';
-    echo "<hr>";
+    echo "<br>";
 }
 ?>
