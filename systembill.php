60ecc08e70e02d51c9ba738b0d61704cf38215a5
matt <MattRiddell@users.noreply.github.com>
2008-05-05 07:36:16 +0000

    Start an idea

---
+++ systembill.php
@@ -0,0 +1,15 @@
+<?
+require "header.php";
+$result = mysql_query("select cost, groupid from campaign where cost is not NULL");
+while ($row = mysql_fetch_assoc($result)) {
+echo $row[cost]." - ".$row[groupid];
+$test[$row[groupid]]+=$row[cost];
+echo "<br />";
+}
+foreach ($test as $key => $value) {
+    echo $value." ($key)<br />";
+    $sql = "INSERT INTO system_billing (groupid,totalcost) VALUES ($key, $value)";
+    echo $sql;
+    $result = mysql_query($sql);
+}
+?>
