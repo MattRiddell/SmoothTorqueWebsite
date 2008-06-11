dbea4798b3bc28a3ccd31ef3b82235ce29c9dab3
matt <MattRiddell@users.noreply.github.com>
2008-06-11 01:19:10 +0000

    Don't allow customers to delete trunks in use

--- deletetrunk.php
+++ deletetrunk.php
@@ -6,6 +6,20 @@ $_POST = array_map(mysql_real_escape_string,$_POST);
 $_GET = array_map(mysql_real_escape_string,$_GET);
 
 if (isset($_GET[id])){
+
+$count = 0;
+$sql="SELECT count(*) from campaign where trunkid=".($_GET[id]);
+$result=mysql_query($sql, $link) or die (mysql_error());
+$count+=mysql_result($result,0,0);
+
+if ($count > 0) {
+    require "header.php";
+    echo "<br /><br />Sorry this trunk is currently being used by a campaign";
+?>    <meta http-equiv="refresh" content="3;url=/trunks.php"><?
+} else {
+
+
+
     $id=($_GET[id]);
     $sql="delete from trunk where id=$_GET[id] limit 1";
     $result=mysql_query($sql, $link) or die (mysql_error());;
@@ -16,6 +30,7 @@ $result=mysql_query($sql, $link);
 
     include("trunks.php");
     exit;
+}
 }
 require "footer.php";
 ?>
