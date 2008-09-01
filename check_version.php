667aa902d6692a9c166a876cb7adc39bcdbe8d3a
matt <MattRiddell@users.noreply.github.com>
2008-09-01 06:55:29 +0000

    Changed

--- check_version.php
+++ check_version.php
@@ -6,5 +6,11 @@ if (!$handle = fopen("http://call.venturevoip.com/svn_version.php", 'r')) {
     $remote_version = fread($handle,4096);
     fclose($handle);
 }
-echo "Local: $local_version Remote: $remote_version<br />";
+$local_version = str_replace("M","",$local_version);
+$remote_version = str_replace("M","",$remote_version);
+if ($local_version != $remote_version) {
+    echo "Local: $local_version Remote: $remote_version<br />";
+} else {
+    echo "Your copy is up to date";
+}
 ?>
