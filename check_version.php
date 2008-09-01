d2c28a030796b348daa878c7ebc524023394c9e4
matt <MattRiddell@users.noreply.github.com>
2008-09-01 06:36:57 +0000

    Changed

--- check_version.php
+++ check_version.php
@@ -3,6 +3,6 @@ $result = exec("/usr/bin/which svn");
 print_r($result);
 //echo $result;
 //print_r(exec($result." info -r HEAD"));
-print_r(exec($result." status --username web --password \"\" --show-updates --verbose . |grep *",$output));
+print_r(exec($result." status --username web --password \"\" --no-auth-cache --show-updates --verbose .",$output));
 print_r($output);
 ?>
