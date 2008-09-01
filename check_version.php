33859929174eb6a4497434abd6b5d6778a1c6ba1
matt <MattRiddell@users.noreply.github.com>
2008-09-01 06:57:40 +0000

    Changed

--- check_version.php
+++ check_version.php
@@ -9,7 +9,9 @@ if (!$handle = fopen("http://call.venturevoip.com/svn_version.php", 'r')) {
 $local_version = str_replace("M","",$local_version);
 $remote_version = str_replace("M","",$remote_version);
 if ($local_version != $remote_version) {
+    echo "Your copy of SmoothTorque is not the latest version:<br />";
     echo "Local: $local_version Remote: $remote_version<br />";
+    echo "Please go to the root of your web server and type \"svn up\"<br />";
 } else {
     echo "Your copy is up to date";
 }
