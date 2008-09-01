95f9cdd9f74f09e6966eeab3f6a1f3d2da646685
matt <MattRiddell@users.noreply.github.com>
2008-09-01 11:51:22 +0000

    Betra

--- check_version.php
+++ check_version.php
@@ -20,7 +20,7 @@ if (trim($local_version) != trim($remote_version)) {
 }
 
 echo "<br /><b>";
-$local_version = exec("cat svn_version.php");
+//$local_version = exec("cat svn_version.php");
 if (!$handle = fopen("http://call.venturevoip.com/backend_version.php", 'r')) {
     echo "Can't read from remote";
 } else {
