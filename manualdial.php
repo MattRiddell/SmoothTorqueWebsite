f367aa8d49a27aa3a237149ebe5daaeecdcb6c5f
matt <MattRiddell@users.noreply.github.com>
2008-09-01 04:50:37 +0000

    Changes

--- manualdial.php
+++ manualdial.php
@@ -55,10 +55,10 @@ if (isset($_GET[campaignid])){
 }
 // Ok, so we have a campaign id to use numbers from
 // How many numbers has it got?
-$result = mysql_query("SELECT count(*) FROM number where (status='new' or status='manual_dial') and campaignid=$_POST[campaignid]");
+$result = mysql_query("SELECT count(*) FROM number where status='manual_dial' and campaignid=$_POST[campaignid]");
 $num_of_num = mysql_result($result,0,0);
 echo $num_of_num." numbers remaining<br />";
-$result = mysql_query("SELECT * from number where (status='new' or status='manual_dial') and campaignid=$_POST[campaignid] limit 100");
+$result = mysql_query("SELECT * from number where campaignid=$_POST[campaignid] and status='manual_dial' limit 100");
 //srand(time());
 $x = 0;
 while ($row = mysql_fetch_assoc($result)) {
@@ -66,17 +66,20 @@ while ($row = mysql_fetch_assoc($result)) {
     $rows[$x] = $row;
     $x++;
 }
-if(sizeof($numbers)){
-	$random = (rand()%sizeof($numbers));
-	//echo $random." entry is ".$numbers[$random]."<br />";
-	//$result = mysql_query("INSERT INTO number (campaignid, phonenumber, status) VALUES ($_POST[campaignid],'$numbers[$random]','new')") or die(mysql_error());
-	$result = mysql_query("SELECT * FROM campaign WHERE id = $_POST[campaignid]") or die(mysql_error());
-	$row = mysql_fetch_assoc($result);
-	?><a title="Start running this campaign" href="manual_startcampaign.php?phonenumber=<?=$numbers[$random];?>&id=<?=$_POST[campaignid]?>&astqueuename=<?echo $row[astqueuename];?>&clid=<?echo $row[clid];?>&trclid=<?echo $row[trclid];?>&agents=<?echo $row[maxagents];?>&did=<?echo $row[did];?>&context=<?echo $row[context];?>">
-	Dial <?=$numbers[$random]?></a>
-	<?
-	//print_r($rows[$x]);
-}else{
-//	echo "No more numbers to dial";
-}
+$random = (rand()%sizeof($numbers));
+//echo $random." entry is ".$numbers[$random]."<br />";
+//$result = mysql_query("INSERT INTO number (campaignid, phonenumber, status) VALUES ($_POST[campaignid],'$numbers[$random]','new')") or die(mysql_error());
+$result = mysql_query("SELECT * FROM campaign WHERE id = $_POST[campaignid]") or die(mysql_error());
+$row = mysql_fetch_assoc($result);
+?><a title="Start running this campaign" href="manual_startcampaign.php?phonenumber=<?=$numbers[$random];?>&id=<?=$_POST[campaignid]?>&astqueuename=<?echo $row[astqueuename];?>&clid=<?echo $row[clid];?>&trclid=<?echo $row[trclid];?>&agents=<?echo $row[maxagents];?>&did=<?echo $row[did];?>&context=<?echo $row[context];?>">
+Dial <?=$numbers[$random]?></a>
+<?
+//print_r($rows[$x]);
 }?>
+
+
+
+
+
+
+
