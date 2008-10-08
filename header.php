4074feaf011577f0f3cf29686d7eb1ea0ae9c84c
matt <MattRiddell@users.noreply.github.com>
2008-10-08 02:20:12 +0000

    changed it so that caller ID option will be displayed for the Immediate Message Playback type campaign

--- header.php
+++ header.php
@@ -944,14 +944,14 @@ if (myval == '0') {
         document.all['xx2'].style.display = "visible";
         document.all['xx3'].style.display = "none";
         document.all['xx4'].style.display = "none";
-        document.all['xx5'].style.display = "none";
+        document.all['xx5'].style.display = "visible";
         document.all['xx6'].style.display = "visible";
     } else {
         document.getElementById('xx1').style.display='none';
         document.getElementById('xx2').style.display='';
         document.getElementById('xx3').style.display='none';
         document.getElementById('xx4').style.display='none';
-        document.getElementById('xx5').style.display='none';
+        document.getElementById('xx5').style.display='';
         document.getElementById('xx6').style.display='';
     }
 } else if (myval == '8') {
