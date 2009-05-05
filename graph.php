db5be29304e278ce2bfedaedb37eac6004772940
matt <MattRiddell@users.noreply.github.com>
2009-05-05 04:31:34 +0000

    Changes

--- graph.php
+++ graph.php
@@ -114,90 +114,84 @@ if (mysql_num_rows($result) > 0) {
 }
 /* END OF CAMPAIGN PROPERTIES */
 
-
-
 $lines=file("/tmp/Sm".$id.".console");
 $lines2=file("/tmp/Sm".$id.".console2");
 $lines3=file("/tmp/Sm".$id.".console3");
 
+/* Initialise the arrays */
 for ($i=1;$i<721;$i++){
-        $chart [ 'chart_data' ][ 0 ][ $i ] = 720-$i;
-        $chart [ 'chart_data' ][ 1 ][ $i ] = 0;
-        $chart [ 'chart_data' ][ 2 ][ $i ] = 0;
-        $array1[$i] = 0;
-        $array3[$i] = 0;
-        $array2[$i] = $mrs/2000*100;
-        $array5[$i] = ($mrs/2000*100);
+    $chart [ 'chart_data' ][ 0 ][ $i ] = 720-$i;
+    $chart [ 'chart_data' ][ 1 ][ $i ] = 0;
+    $chart [ 'chart_data' ][ 2 ][ $i ] = 0;
+    $array1[$i] = 0;
+    $array3[$i] = 0;
+    $array2[$i] = $mrs/2000*100;
+    $array5[$i] = ($mrs/2000*100);
 }
 $count = 0;
 $highest = 0;
+
 foreach ($lines as $line_num => $line) {
-        $total_count_x++;
-        if ($count<720){
-                if (strlen(trim($line))>0){
-                        $count++;
-                        //if ($timespent > $count) {
-                            $avgPerc+=trim($line);
-                        //}
-                        if(trim($line)>$highest){
-                            $highest = trim($line);
-                        }
-                        $array1[ $count ] = trim($line);
-                        $lastPerc=trim($line);
-                }
+    $total_count_x++;
+    if ($count<720){
+        if (strlen(trim($line))>0){
+            $count++;
+            $avgPerc+=trim($line);
+            if(trim($line)>$highest){
+                $highest = trim($line);
+            }
+            $array1[ $count ] = trim($line);
+            $lastPerc=trim($line);
         }
+    }
 }
 $count = 0;
 foreach ($lines2 as $line_num2 => $line2) {
-        $total_count_x++;
-        if ($count<720){
-                if (strlen(trim($line2))>0){
-                        $count++;
-                       if (trim($line2) == "nan") {
-                               $line2 = "0";
-                       }
-                        $array3[ $count ] = trim($line2);
-                        $array4[ $count+1 ] = $array3[ $count ]-0.5;
-                        $lastSpeed=trim($line2);
-                        $xdata[$count] = $count;
-                }
+    $total_count_x++;
+    if ($count<720){
+        if (strlen(trim($line2))>0){
+            $count++;
+            if (trim($line2) == "nan") {
+               $line2 = "0";
+            }
+            $array3[ $count ] = trim($line2);
+            $array4[ $count+1 ] = $array3[ $count ]-0.5;
+            $lastSpeed=trim($line2);
+            $xdata[$count] = $count;
         }
+    }
 }
+
 $highest_ms = 1;
 if ($_GET[debug]>0){
     $count = 0;
     foreach ($lines3 as $line_num3 => $line3) {
-            //$total_count_x++;
-            if ($count<720){
-                    if (strlen(trim($line3))>0){
-                            $count++;
-                            if (trim($line3) > $highest_ms) {
-                                $highest_ms = trim($line3);
-                            }
-                    }
+        //$total_count_x++;
+        if ($count<720){
+            if (strlen(trim($line3))>0){
+                $count++;
+                if (trim($line3) > $highest_ms) {
+                    $highest_ms = trim($line3);
+                }
             }
+        }
     }
     $count = 0;
     foreach ($lines3 as $line_num3 => $line3) {
-            //$total_count_x++;
-            if ($count<720){
-                    if (strlen(trim($line3))>0){
-                            $count++;
-                            if (trim($line3) > 0) {
-                                //$newnumber = trim($line3)/1000;
-                                //$array_ms[ $count ] = 100-(20 * (log10($newnumber) + 2.7));
-                               if (!($highest_ms > 0)) {
-                                       $highest_ms = 1;
-                               }
-                                $array_ms[ $count ] = 101-(trim($line3)/($highest_ms)* 100);
-
-//                                $array_ms[ $count ] = 100-(20 * (log10($newnumber) + 2.7));
-                            } else {
-                                $array_ms[ $count ] = 0;
-                            }
-                            $lastSpeed_ms=trim($line3);
-                    }
+        if ($count<720){
+            if (strlen(trim($line3))>0){
+                $count++;
+                if (trim($line3) > 0) {
+                   if (!($highest_ms > 0)) {
+                           $highest_ms = 1;
+                   }
+                    $array_ms[ $count ] = 101-(trim($line3)/($highest_ms)* 100);
+                } else {
+                    $array_ms[ $count ] = 0;
+                }
+                $lastSpeed_ms=trim($line3);
             }
+        }
     }
 }
 
@@ -219,45 +213,23 @@ if ($total_count_x == 0) {
         $avgPerc/=$timespent;
     }
 }
-/*if ($timespent<$MAX_ENTRIES) {
-        $avgPerc=0;
-        for ($i=0;$i<$MAX_ENTRIES;$i++){
-                $avgPerc+=$chart [ 'chart_data' ][ 1 ][ $i ];
-        }
-        $avgPerc/=$timespent;
-}*/
+
 include ( "./jpgraph.php");
 include ("./jpgraph_line.php");
 include ("./jpgraph_bar.php");
 include "./jpgraph_regstat.php";
-//$res="300";
-//$spline  = new Spline($xdata, $array2);
-//$spline2  = new Spline($xdata, $array1);
-//$spline3  = new Spline($xdata, $array3);
-//list( $sdatax, $chart [ 'chart_data' ][ 2 ]) =  $spline->Get($res);
-//list( $sdatax, $chart [ 'chart_data' ][ 1 ]) =  $spline2->Get($res);
-//list( $sdatax, $chart [ 'chart_data' ][ 3 ]) =  $spline3->Get($res);
 $chart [ 'chart_data' ][ 2 ] = $array2;
 $chart [ 'chart_data' ][ 1 ] = $array1;
 $chart [ 'chart_data' ][ 3 ] = $array3;
-
 $graph2 = new Graph(1040, 400);
-//echo $highest;
 if ($highest>100){
 	$graph2->SetScale('linlin',0,$highest,1,719);
-//	$graph2->SetYScale(0,'lin',0,$highest);
-//	$graph2->SetXScale(0,'lin',1,719);
 } else {
-//	$graph2->SetScale('linlin',0,$highest,1,239);
 	$graph2->SetScale('linlin',0,100,1,719);
 }
 
-
-
-//$graph2->SetScale('linlin');
 $graph2->xaxis->SetTickLabels($aaa);
 $graph2->xaxis->SetTextLabelInterval(1);
-//$graph2->SetTickDensity(TICKD_SPARSE,TICKD_SPARSE); // Many Y-ticks
 $graph2->yaxis->SetWeight(1);
 $graph2->xgrid->SetColor('white@0.3','white@0.7');
 $graph2->ygrid->SetColor('white@0.3','white@0.7');
@@ -272,33 +244,32 @@ $graph2->xaxis->SetTickLabels($datax);
 $speedAtractorPlot[] = new LinePLot($chart [ 'chart_data' ][ 2 ]);
 $speedAtractorPlot2[] = new LinePLot($array5);
 $dplot2[] = new LinePlot($chart [ 'chart_data' ][ 1 ]);
-$runningSpeedPlot[] = new LinePLot($chart [ 'chart_data' ][ 3 ]);
-$runningSpeedPlot2[] = new LinePLot($array4);
-//$dplot[0]->SetFillColor("#ff0000@0.8");
+$dplot2[0]->SetLegend(' Staff on Phone');
 $dplot2[0]->SetFillGradient("#0000ff@0.99","#009933");
 $dplot2[0]->SetWeight(1);
-//$speedAtractorPlot[0]->SetFillGradient("#ff0000@0.5","#ff0000@0.5");
+
 $speedAtractorPlot[0]->SetWeight(1);
 $speedAtractorPlot[0]->SetColor("#00FF00");
+$speedAtractorPlot[0]->SetLegend(' Speed Attractor');
+
 $speedAtractorPlot2[0]->SetWeight(3);
 $speedAtractorPlot2[0]->SetColor("#000000");
-//$graph2->mark
+
+$runningSpeedPlot[] = new LinePLot($chart [ 'chart_data' ][ 3 ]);
+$runningSpeedPlot[0]->SetLegend(' Current Speed');
 $runningSpeedPlot[0]->SetColor("#ff0000");
 $runningSpeedPlot[0]->SetWeight(2);
 $runningSpeedPlot[0]->SetStepStyle();
+
+$runningSpeedPlot2[] = new LinePLot($array4);
 $runningSpeedPlot2[0]->SetColor("#000000@0.9");
 $runningSpeedPlot2[0]->SetWeight(2);
 $runningSpeedPlot2[0]->SetStepStyle(true);
+
 $graph2->ygrid->Show(true ,true);
 $graph2->xgrid->Show();
-//$graph2->title->Set('Filled Y-grid');
 $graph2->legend->SetLayout(LEGEND_HOR);
 $graph2->img->SetMargin(35,23,05,80);
-
-$speedAtractorPlot[0]->SetLegend(' Speed Attractor');
-$dplot2[0]->SetLegend(' Staff on Phone');
-$runningSpeedPlot[0]->SetLegend(' Current Speed');
-
 $graph2->legend->SetFrameWeight(1);
 $graph2->legend->SetShadow('black@0.8',4);
 $graph2->legend->SetFillColor('black@0.5');
@@ -306,32 +277,19 @@ $graph2->legend->SetColor('white');
 $graph2->legend->SetFont(FF_FONT2,FS_BOLD);
 $graph2->legend->SetPos(0.05,0.04,'left','top');
 $graph2->legend->SetMarkAbsSize(13);
-
-//$dplot[0]->SetStepStyle();
-//$dplot2[0]->SetStepStyle();
-//$dplot2[0]->SetStepStyle();
 $graph2->Add($dplot2[0]);
 $graph2->Add($runningSpeedPlot2[0]);
 $graph2->Add($runningSpeedPlot[0]);
 $graph2->Add($speedAtractorPlot2[0]);
 $graph2->Add($speedAtractorPlot[0]);
 if ($_GET[debug]>0){
-//$graph2->SetYScale(2,'lin',0,500,1,719);
-
-$msPlot[] = new LinePLot($array_ms);
+    $msPlot[] = new LinePLot($array_ms);
     $msPlot[0]->SetWeight(3);
     $msPlot[0]->SetColor("#ffff00");
-
     $graph2->Add($msPlot[0]);
 }
 $graph2->SetShadow();
 $graph2->img->SetAntiAliasing(true);
-
-/*$sql = 'SELECT progress from queue where campaignid='.$id;
-$resultx=mysql_query($sql, $link) or die (mysql_error());;
-$rowx = mysql_fetch_assoc($resultx);
-
-$progress=$rowx[progress];*/
 
 $sql = 'SELECT status from queue where campaignid='.$id;
 $resultx=mysql_query($sql, $link) or die (mysql_error());;
@@ -344,25 +302,21 @@ if ($dialed>0){
     $progress=$dialed;
 }
 
-
 if ($progress<0||$status < 0){
     $txt=new Text( "\n\n     This Campaign is Now Finished    \n\n");
     $txt->Pos( 500,122);
     $txt->SetAlign("center","","");
     $txt->SetFont(FF_FONT2,FS_BOLD);
     $txt->SetBox('#00ff88@0.2','navy@0.1','#000000@0.8',0,5);
-    //$txt->SetColor("red");
 } else {
-//$txt = new Text($ms);
     if (!($ms > 0)) {
        $ms = 1;
-       }
+    }
     $txt=new Text( "  Dialed: $progress   Busy Agents:$busy/$max   Average:".round($avgPerc)."%   Time Spent: $timespentM:$timespentS   Time between calls: ".round($ms/1000,3)." Seconds (".round(1/($ms/1000),3)." CPS)");
     $txt->Pos( 500,342);
     $txt->SetAlign("center","","");
     $txt->SetFont(FF_FONT2,FS_NORMAL);
     $txt->SetBox('#bbbbff','navy@0.1','#cccccc');
-    //$txt->SetColor("red");
 }
 $graph2->AddText( $txt);
 if ($_GET[debug]>0){
@@ -378,10 +332,6 @@ if ($_GET[debug]>0){
     $graph2->AddText( $txt2);
 }
 
-//$graph2->ygrid->SetFill(true,'#EFEFEF@0.9','#BBCCFF@0.9');
-//$graph2->xaxis->SetTextLabelInterval(1);
-//$graph2->xaxis->SetTextTickInterval(1,1);
-
-$graph2->SetBackgroundGradient('#CCCCFF@0.6','white@0.6',GRAD_HOR,BGRAD_PLOT);
+$graph2->SetBackgroundGradient('#CCCCFF@0.8','white@0.6',GRAD_HOR,BGRAD_PLOT);
 $graph2->Stroke();
 ?>
