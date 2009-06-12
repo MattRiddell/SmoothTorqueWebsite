36904210dccf4bfbc96e7e71d4a72b34fdcbdd76
matt <MattRiddell@users.noreply.github.com>
2009-06-12 15:00:56 +0000

    Remove newline

--- header_server.php
+++ header_server.php
@@ -3,5 +3,4 @@
     <TD class="subheader"><A HREF="servers.php"><img src="/images/server.png" border="0" align="left">View Servers</A>&nbsp;&nbsp;</TD>
     </TR></table><?box_end();?>
 
-    <BR>
 <?flush();?>
