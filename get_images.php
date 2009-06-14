62d717b68718120799bd9ef7cc3ab57a6e946ebd
matt <MattRiddell@users.noreply.github.com>
2009-06-14 11:01:12 +0000

    Survive a MySQL restart/crash

--- get_images.php
+++ get_images.php
@@ -1,12 +1,23 @@
 <?
 require "admin/db_config.php";
 while (1) {
+//	echo $link;
+	while (!@mysql_ping ($link)) {
+		echo "Link down\n";
+		@mysql_close($link);
+		$link = @mysql_connect($db_host, $db_user, $db_pass);
+		if (@mysql_ping($link)) {
+			@mysql_select_db("SineDialer") or die(mysql_error());
+		}
+		sleep(1);
+	}
 	$result = mysql_query("SELECT campaignid FROM queue WHERE status = 101");
 	echo "Image Caching Script\n";
 	echo "====================\n";
 	echo "Running Campaigns: ".mysql_num_rows($result)."\n";
 	if (mysql_num_rows($result) > 0) {
 		while ($row = mysql_fetch_assoc($result)) {
+			echo "Found campaign $row[campaignid]...\n";
 			if ($stream = fopen('http://italk.venturevoip.com/graph.php?id='.$row[campaignid], 'rb')) {
 				$contents = stream_get_contents($stream);
 				$output = fopen('./images/live/campaign_'.$row[campaignid].'.png', 'wb');
@@ -28,6 +39,10 @@ while (1) {
 	echo "=====================\n";
 	echo "Sleeping For Next Run\n";
 	echo "=====================\n";
-	sleep(5);
+	sleep(1);
+	echo "=====================\n";
+	echo "Sleeping For Next Run\n";
+	echo "=====================\n";
+	sleep(1);
 }
 ?>
