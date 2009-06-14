bd8a0137fb0c08ee8d3d69db15017a013216ed3a
matt <MattRiddell@users.noreply.github.com>
2009-06-14 09:33:33 +0000

    Changes to include debug mode

--- get_images.php
+++ get_images.php
@@ -2,6 +2,9 @@
 require "admin/db_config.php";
 while (1) {
 	$result = mysql_query("SELECT campaignid FROM queue WHERE status = 101");
+	echo "Image Caching Script\n";
+	echo "====================\n";
+	echo "Running Campaigns: ".mysql_num_rows($result)."\n";
 	if (mysql_num_rows($result) > 0) {
 		while ($row = mysql_fetch_assoc($result)) {
 			if ($stream = fopen('http://italk.venturevoip.com/graph.php?id='.$row[campaignid], 'rb')) {
@@ -12,8 +15,19 @@ while (1) {
 				fclose($output);
 				echo "Saved campaign $row[campaignid]\n";
 			}
+			if ($stream = fopen('http://italk.venturevoip.com/graph.php?id='.$row[campaignid].'&debug=1', 'rb')) {
+				$contents = stream_get_contents($stream);
+				$output = fopen('./images/live/debug_'.$row[campaignid].'.png', 'wb');
+				fwrite($output, $contents);
+				fclose($stream);
+				fclose($output);
+				echo "Saved debug campaign $row[campaignid]\n";
+			}
 		}
 	}
+	echo "=====================\n";
+	echo "Sleeping For Next Run\n";
+	echo "=====================\n";
 	sleep(5);
 }
 ?>
