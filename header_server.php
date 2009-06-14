9370cbf612da95b8e0720a3f9b8b3808c7a338cc
matt <MattRiddell@users.noreply.github.com>
2009-06-14 08:14:49 +0000

    Add the link to the new mysql stats

--- header_server.php
+++ header_server.php
@@ -1,6 +1,7 @@
-<?box_start(270);?><table align="center" width="100%" border="0" cellpadding="0" cellspacing="3"><TR>
+<?box_start(480);?><table align="center" width="100%" border="0" cellpadding="0" cellspacing="3"><TR>
     <TD class="subheader"><A HREF="addserver.php"><img src="/images/server_add.png" border="0" align="left">Add Server</A>&nbsp;&nbsp;</TD>
-    <TD class="subheader"><A HREF="servers.php"><img src="/images/server.png" border="0" align="left">View Servers</A>&nbsp;&nbsp;</TD>
+    <TD class="subheader"><A HREF="servers.php"><img src="/images/server.png" border="0" align="left">View Asterisk Servers</A>&nbsp;&nbsp;</TD>
+    <TD class="subheader"><A HREF="mysql_stats.php"><img src="/images/database.png" border="0" align="left">View MySQL Status</A>&nbsp;&nbsp;</TD>
     </TR></table><?box_end();?>
 
 <?flush();?>
