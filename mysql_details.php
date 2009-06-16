422da37129130fb6543b9b85605069dd8870c6ca
matt <MattRiddell@users.noreply.github.com>
2009-06-16 13:47:31 +0000

    Bring the MySQL logging to a new level - show what locks are held etc so you can see which queries create problems

---
+++ mysql_details.php
@@ -0,0 +1,71 @@
+<?
+$level=$_COOKIE[level];
+/* Find out what the base directory name is for two reasons:
+    1. So we can include files
+    2. So we can explain how to set up things that are missing */
+$current_directory = dirname(__FILE__);
+
+/* What page we are currently on - this is used to highlight the menu
+   system as well as to not cache certain pages like the graphs */
+$self=$_SERVER['PHP_SELF'];
+
+/* Load in the functions we may need - these are the list of available
+   custom functions - for more information, read the comments in the
+   functions.php file - most functions are in their own file in the
+   functions subdirectory */
+require "/".$current_directory."/functions/functions.php";
+
+$url = "default";
+include "admin/db_config.php";
+mysql_select_db("SineDialer", $link) or die("Unable to connect: ".mysql_error());
+echo "<b>MySQL Statistics:<br /></b>";
+$result = mysql_query("SHOW STATUS");
+while ($row = mysql_fetch_assoc($result)){
+        if ($row[Variable_name] == 'Slow_queries') {
+                $slow_queries = $row['Value'];
+        }
+        if ($row[Variable_name] == 'Questions') {
+                $questions = $row['Value'];
+        }
+        if ($row[Variable_name] == 'Uptime') {
+                $uptime = sec2hms($row['Value']);
+        }
+        if ($row[Variable_name] == 'Connections') {
+                $connections = number_format($row['Value']);
+        }
+}
+$result = mysql_query("SELECT value FROM SineDialer.config WHERE parameter = 'mysql_queue'") or die(mysql_error());
+if (mysql_num_rows($result) > 0) {
+        $pending = "Pending MySQL Writes: ".mysql_result($result,0,0)." ";
+}
+echo "Connections: $connections Queries: ".number_format($questions)." ".$pending."Uptime: $uptime<br /><br />";
+
+echo "<b>Long Running MySQL Threads:</b><br />";
+$result = mysql_query("SHOW PROCESSLIST");
+echo "<br /><center><table border=\"0\" class=\"tborder\">";
+while ($row = mysql_fetch_assoc($result)) {
+	if ($row[Command] != 'Sleep' && $row[Time] > 1) {
+		echo "<tr>";
+		if ($row[State] == 'Locked') {
+			echo "<td><p align=\"left\"><b>Locked:</b></font> $row[Info]<br /><b>Time:</b> ".sec2hms($row[Time])." <b>Host:</b> ".$row[User]."@".$row[Host]."</td>";
+		} else if ($row[State] == 'updating' ||$row[State] == 'Updating' ||$row[State] == 'update') {
+			echo "<td><p align=\"left\"><b>Updating:</b> $row[Info]<br /><b>Time:</b> ".sec2hms($row[Time])." <b>Host:</b> ".$row[User]."@".$row[Host]."</td>";
+		} else if ($row[State] == 'Sorting result') {
+			echo "<td><p align=\"left\"><b>Sorting result:</b> $row[Info]<br /> <b>Time:</b> ".sec2hms($row[Time])." <b>Host:</b> ".$row[User]."@".$row[Host]."</td>";
+		} else if ($row[State] == 'Sending data') {
+			echo "<td><p align=\"left\"><b>Sending data:</b> $row[Info]<br /> <b>Time:</b> ".sec2hms($row[Time])." <b>Host:</b> ".$row[User]."@".$row[Host]."</td>";
+		} else {
+			if ($row[Info] != "SHOW PROCESSLIST") {
+				echo "<td><p align=\"left\">";
+				foreach ($row as $key=>$value) {
+					echo "<b>Key:</b> $key <b>Value:</b> $value<br />";
+				}
+				echo "</p></td>";
+			}
+		}
+		echo "</tr>";
+		echo "<tr><td><br /></td></tr>";
+	}
+}
+echo "</table>";
+?>
--- mysql_stats.php
+++ mysql_stats.php
@@ -1,29 +1,71 @@
 <?
 require "header.php";
 require "header_server.php";
-$result = mysql_query("SHOW STATUS");
-while ($row = mysql_fetch_assoc($result)){
-	if ($row[Variable_name] == 'Slow_queries') {
-		$slow_queries = $row['Value'];
-	}
-	if ($row[Variable_name] == 'Questions') {
-		$questions = $row['Value'];
-	}
-	if ($row[Variable_name] == 'Uptime') {
-		$uptime = sec2hms($row['Value']);
-	}
-	if ($row[Variable_name] == 'Connections') {
-		$connections = number_format($row['Value']);
-	}
+ function _get_browser()
+{
+  $browser = array ( //reversed array
+   "OPERA",
+   "MSIE",            // parent
+   "NETSCAPE",
+   "FIREFOX",
+   "SAFARI",
+   "KONQUEROR",
+   "MOZILLA"        // parent
+  );
+
+  $info[browser] = "OTHER";
+
+  foreach ($browser as $parent)
+  {
+   if ( ($s = strpos(strtoupper($_SERVER['HTTP_USER_AGENT']), $parent)) !== FALSE )
+   {
+     $f = $s + strlen($parent);
+     $version = substr($_SERVER['HTTP_USER_AGENT'], $f, 5);
+     $version = preg_replace('/[^0-9,.]/','',$version);
+
+     $info[browser] = $parent;
+     $info[version] = $version;
+     break; // first match wins
+   }
+  }
+
+  return $info;
 }
-box_start(600);
+$level=$_COOKIE[level];
+$out=_get_browser();
+if ($out[browser]=="MSIE"){
+?>
+<script type="text/javascript" src="/ajax/jquery.js"></script>
+<script type="text/javascript">
+        $(function(){ // jquery onload
+                window.setInterval(function(){
+                        $('#ajaxDiv').loadIfModified('mysql_details.php');  // jquery ajax load into div
+                },2000);
+        });
+
+</script>
+<?} else {?>
+<script type="text/javascript" src="/ajax/jquery.js"></script>
+<script type="text/javascript">
+
+        $(function(){ // jquery onload
+                window.setInterval(
+                function(){
+                        $('#ajaxDiv').load('mysql_details.php');  // jquery ajax load into div
+                }
+                ,2000);
+        });
+</script>
+<?}
+box_start(700);
 echo "<center>";
-echo "<b>MySQL Statistics:<br /></b>";
-$result = mysql_query("SELECT value FROM SineDialer.config WHERE parameter = 'mysql_queue'");
-if (mysql_num_rows($result) > 0) {
-	$pending = "Pending MySQL Writes: ".mysql_result($result,0,0)." ";
-}
-echo "Connections: $connections Queries: ".number_format($questions)." ".$pending."Uptime: $uptime";
+?>
+<div id = "ajaxDiv">
+<?
+include "mysql_details.php";
+?>
+</div>
+<?
 box_end();
 $result = mysql_query("SHOW TABLE STATUS");
 ?>
@@ -34,6 +76,7 @@ $result = mysql_query("SHOW TABLE STATUS");
 <td CLASS="thead">Total Size</td>
 <td CLASS="thead">Last Update</td>
 <td CLASS="thead">Last Check</td>
+<td CLASS="thead">Engine</td>
 </tr>
 <?
 while ($row = mysql_fetch_assoc($result)) {
@@ -70,13 +113,28 @@ while ($row = mysql_fetch_assoc($result)) {
 	                $size_text = "Gb";
 			$digits = 4;
 		}
-		echo "<td".$tdstyle.">$row[Name]</td>";
-		echo "<td".$tdstyle.">".number_format($rows)."</td>";
-		echo "<td".$tdstyle."><b>".number_format($size,$digits)." $size_text</b></td>";
-		$update_time = strtotime($row[Update_time]);
-		$check_time = strtotime($row[Check_time]);
-		echo "<td".$tdstyle.">".date("D M j G:i:s Y", $update_time)."</td>";
-		echo "<td".$tdstyle.">".date("D M j G:i:s Y", $check_time)."</td>";
+		if ($row[Engine] == "InnoDB") {
+			if ($size_text != "Gb") {
+				$tdstyle = " style=\"background: #8888ff url('images/grad_blue.png')\"";
+			}
+			echo "<td".$tdstyle.">$row[Name]</td>";
+			echo "<td".$tdstyle.">".number_format($rows)." (approx)</td>";
+			echo "<td".$tdstyle."><b>".number_format($size,$digits)." $size_text</b></td>";
+			echo "<td".$tdstyle." colspan=\"2\">$row[Comment]</td>";
+			echo "<td".$tdstyle.">";
+			echo "<b>".$row[Engine]."</b>";
+		} else {
+			echo "<td".$tdstyle.">$row[Name]</td>";
+			echo "<td".$tdstyle.">".number_format($rows)."</td>";
+			echo "<td".$tdstyle."><b>".number_format($size,$digits)." $size_text</b></td>";
+			$update_time = strtotime($row[Update_time]);
+			$check_time = strtotime($row[Check_time]);
+			echo "<td".$tdstyle.">".date("D M j G:i:s Y", $update_time)."</td>";
+			echo "<td".$tdstyle.">".date("D M j G:i:s Y", $check_time)."</td>";
+			echo "<td".$tdstyle.">";
+			echo $row[Engine];
+		}
+		echo "</td>";
 		echo "</tr>";
 	}
 }
