baed81e11b69966dc35140a439565e92282c00b3
matt <MattRiddell@users.noreply.github.com>
2009-06-19 02:29:12 +0000

    Cleanup

--- processcdr.php
+++ processcdr.php
@@ -24,239 +24,160 @@ while ($header_row = mysql_fetch_assoc($result_config) ) {
     }
 }
 echo "Config loaded\n";
-
 $currency = $config_values['CURRENCY_SYMBOL'];
 $db_host=$config_values['CDR_HOST'];
 $db_user=$config_values['CDR_USER'];
 $db_pass=$config_values['CDR_PASS'];
+
+/* ==================================================================== */
+/* End of config information */
+/* ==================================================================== */
+
 $sql = "Select accountcode from billing";
 $result_accounts = mysql_query($sql, $link);
+/* Loop through the account codes which are defined in billing */
 while ($accounts = mysql_fetch_assoc($result_accounts)) {
-
-$accountcode_in = $accounts['accountcode'];
-$cdrlink = mysql_connect($db_host, $db_user, $db_pass) OR die("Error connecting to CDR database using $db_user:$db_pass@$db_host because: \n".mysql_error());
-mysql_select_db($config_values['CDR_DB'], $cdrlink);
-//$sql = "SELECT count(*) from ".$config_values['CDR_TABLE']." WHERE accountcode='$accountcode_in' and userfield2!='1' and dcontext!='default' and dcontext!='load-simulation'
-//	and dcontext!='staff' and dcontext!='ls3' and userfield!=''";
-//$result = mysql_query($sql,$cdrlink);
-//$count = mysql_result($result,0,0);
-//echo $count." Total Records for $accountcode_in\n";
-
-//$sql = "SELECT * from ".$config_values['CDR_TABLE']." order by calldate DESC LIMIT $start,100";
-$sql = "SELECT        * from ".$config_values['CDR_TABLE']." WHERE userfield2 != '1' and accountcode='$accountcode_in' and dcontext!='default' and dcontext!='load-simulation'
-        and dcontext!='staff' and dcontext!='ls3' and userfield!=''
-        order by calldate DESC limit 5000";
-//echo $sql;
-$result = mysql_query($sql,$cdrlink);
-$count = mysql_num_rows($result);
-echo $count." Total Records for $accountcode_in\n";
-
-$i = 0;
-$titletd = "<td bgcolor=\"#000000\"><font color=\"#CCCCFF\"><b>&nbsp;&nbsp;";
-$titletdc = "&nbsp;&nbsp;</td>";
-echo mysql_num_rows($result)." Records this run for $accountcode_in\n";
-while ($row = mysql_fetch_assoc($result)) {
-    $calldate[$i] = $row[calldate];
-    $dcontext[$i] = $row[dcontext];
-    $dst[$i] = $row[dst];
-    $src[$i] = $row[src];
-    $clid[$i] = $row[clid];
-    $channel[$i] = $row[channel];
-    $dstchannel[$i] = $row[dstchannel];
-    $lastapp[$i] = $row[lastapp];
-    $lastdata[$i] = $row[lastdata];
-    $duration[$i] = $row[duration];
-    $billsec[$i] = $row[billsec];
-    $disposition[$i] = $row[disposition];
-    $amaflags[$i] = $row[amaflags];
-    $accountcode[$i] = $row[accountcode];
-    $userfield[$i] = $row[userfield];
-    $userfield2[$i] = $row[userfield2];
-    if ($userfield2[$i] != 1) {
-        $userfield2[$i] = 0;
-        $paid[$i] = '<td bgcolor="#FFEEEE"><img src="/images/cross.png" border="0" align="center">';
-    } else {
-        $paid[$i] = '<td bgcolor="#EEFFEE"><img src="/images/tick.png" border="0" align="center">';
-    }
-    $display = true;
-    if ($disposition[$i] == "ANSWERED") {
-        $td = "<td bgcolor=\"#CCffCC\">";
-    } else if ($disposition[$i] == "NO ANSWER") {
-        $td = "<td bgcolor=\"#DDDDDD\">";
-    } else if ($disposition[$i] == "FAILED") {
-        $td = "<td bgcolor=\"#999999\">";
-        $display = false;
-    } else if ($disposition[$i] == "BUSY") {
-        $td = "<td bgcolor=\"#99ffff\">";
-    } else {
-        $td = "<td>";
-    }
-    if ($dst[$i] != "s") {
-        /* Failed */
-        $td = "<td bgcolor=\"#ff9999\">";
-    }
-    if ($dst[$i] == "timeout") {
-        $td = "<td bgcolor=\"#DDDDDD\">";
-        $dst[$i] = "no answer";
-    }
-    if ($dst[$i] == "busy") {
-        $td = "<td bgcolor=\"#FF99FF\">";
-        $dst[$i] = "busy";
-    }
-    if ($dst[$i] == "1") {
-        $td = "<td bgcolor=\"#9999ff\">";
-        $dst[$i] = "pressed 1";
-    }
-
-    if ($dst[$i] == "s") {
-        if ($disposition[$i]=="FAILED"){
-            $dst[$i] = "";
-        } else {
-            if ($dcontext[$i] == "do-amd") {
-                $dst[$i] = "answer machine";
-            } else if ($dcontext[$i] == "do-live") {
-                $dst[$i] = "human";
-            } /*else {
-                $dst[$i] = "answered";
-            }*/
+    $accountcode_in = $accounts['accountcode'];
+    $cdrlink = mysql_connect($db_host, $db_user, $db_pass) OR die("Error connecting to CDR database using $db_user:$db_pass@$db_host because: \n".mysql_error());
+    mysql_select_db($config_values['CDR_DB'], $cdrlink);
+    $sql = "SELECT * from ".$config_values['CDR_TABLE']." WHERE userfield2 != '1' and accountcode='$accountcode_in' and dcontext!='default' and dcontext!='load-simulation'
+        and dcontext!='staff' and dcontext!='ls3' and userfield!='' order by calldate DESC limit 5000";
+    $result = mysql_query($sql,$cdrlink);
+    $count = mysql_num_rows($result);
+    echo $count." Total Records for $accountcode_in\n";
+    $i = 0;
+    echo mysql_num_rows($result)." Records this run for $accountcode_in\n";
+    while ($row = mysql_fetch_assoc($result)) {
+        $calldate[$i] = $row[calldate];
+        $dcontext[$i] = $row[dcontext];
+        $dst[$i] = $row[dst];
+        $src[$i] = $row[src];
+        $clid[$i] = $row[clid];
+        $channel[$i] = $row[channel];
+        $dstchannel[$i] = $row[dstchannel];
+        $lastapp[$i] = $row[lastapp];
+        $lastdata[$i] = $row[lastdata];
+        $duration[$i] = $row[duration];
+        $billsec[$i] = $row[billsec];
+        $disposition[$i] = $row[disposition];
+        $amaflags[$i] = $row[amaflags];
+        $accountcode[$i] = $row[accountcode];
+        $userfield[$i] = $row[userfield];
+        $userfield2[$i] = $row[userfield2];
+        if ($userfield2[$i] != 1) {
+            $userfield2[$i] = 0;
         }
+        $display = true;
+        if (!($customerid[$accountcode[$i]]>0)) {
+            mysql_select_db("SineDialer", $link);
 
-    }
-    //$billtype[$i] = "Per Minute";
-    if (!($customerid[$accountcode[$i]]>0)) {
-        mysql_select_db("SineDialer", $link);
+            $sqlx = "SELECT * from SineDialer.billing where accountcode = '".$accountcode[$i]."'";
+            //echo $sqlx;
 
-        $sqlx = "SELECT * from SineDialer.billing where accountcode = '".$accountcode[$i]."'";
-        //echo $sqlx;
-
-        $resultx = mysql_query($sqlx,$link);
-        $priceperminute[$accountcode[$i]] = mysql_result($resultx, 0, 'priceperminute');
-        //echo mysql_result($resultx, 0, 'priceperminute');
-        $customerid[$accountcode[$i]] = mysql_result($resultx, 0, 'customerid');
-        $firstperiod[$accountcode[$i]] = mysql_result($resultx, 0, 'firstperiod');
-        $increment[$accountcode[$i]] = mysql_result($resultx, 0, 'increment');
-        $firstperiod[$accountcode[$i]] = mysql_result($resultx, 0, 'firstperiod');
-        $credit[$accountcode[$i]] = mysql_result($resultx, 0, 'credit');
-        $credit_limit[$accountcode[$i]] = mysql_result($resultx, 0, 'creditlimit');
-        $pricepercall[$accountcode[$i]] = mysql_result($resultx, 0, 'pricepercall');
-        $priceperconnectedcall[$accountcode[$i]] = mysql_result($resultx, 0, 'priceperconnectedcall');
-        $priceperpress1[$accountcode[$i]] = mysql_result($resultx, 0, 'priceperpress1');
-    }
-    //$cost[$i] = round((1/60)*$billsec[$i],2);
-    $cost[$i] = 0;
-    $costperpress1[$i] = 0;
-    $costpercall[$i] = 0;
-    $costperminute[$i] = 0;
-    $costperconnect[$i] = 0;
-
-    if ($pricepercall[$accountcode[$i]] > 0) {
-        if ($display) {
-            $costpercall[$i] = round($pricepercall[$accountcode[$i]],2);
-            $cost[$i] += $costpercall[$i];
+            $resultx = mysql_query($sqlx,$link);
+            $priceperminute[$accountcode[$i]] = mysql_result($resultx, 0, 'priceperminute');
+            //echo mysql_result($resultx, 0, 'priceperminute');
+            $customerid[$accountcode[$i]] = mysql_result($resultx, 0, 'customerid');
+            $firstperiod[$accountcode[$i]] = mysql_result($resultx, 0, 'firstperiod');
+            $increment[$accountcode[$i]] = mysql_result($resultx, 0, 'increment');
+            $firstperiod[$accountcode[$i]] = mysql_result($resultx, 0, 'firstperiod');
+            $credit[$accountcode[$i]] = mysql_result($resultx, 0, 'credit');
+            $credit_limit[$accountcode[$i]] = mysql_result($resultx, 0, 'creditlimit');
+            $pricepercall[$accountcode[$i]] = mysql_result($resultx, 0, 'pricepercall');
+            $priceperconnectedcall[$accountcode[$i]] = mysql_result($resultx, 0, 'priceperconnectedcall');
+            $priceperpress1[$accountcode[$i]] = mysql_result($resultx, 0, 'priceperpress1');
         }
-    }
-    if ($disposition[$i] == "ANSWERED") {
-        if ($billsec[$i] > $firstperiod[$accountcode[$i]]) {
-            if ($increment[$accountcode[$i]] == 1) {
-                $costperminute[$i] = round(($priceperminute[$accountcode[$i]]/60) * $billsec[$i],2);
-            } else {
-                /*30
-                27
-                if the increment is 30 seconds and the call is 73 seconds they should be
-                charged for 73/30 = 2.4 blocks - round up to 3 = 3*30 = 90*/
-                //echo "Billsec: $billsec[$i] Increment: $increment[$accountcode[$i]]";
-                $blocks = ceil($billsec[$i]/$increment[$accountcode[$i]]);
-                //echo "Blocks: $blocks";
-                $newsecs = $blocks * $increment[$accountcode[$i]];
-                //echo "Newsecs: $newsecs";
-                $costperminute[$i] = round(($priceperminute[$accountcode[$i]]/60) * $newsecs,2);
-                //echo "costperminute: $costperminute[$i]";
+        $cost[$i] = 0;
+        $costperpress1[$i] = 0;
+        $costpercall[$i] = 0;
+        $costperminute[$i] = 0;
+        $costperconnect[$i] = 0;
+
+        if ($pricepercall[$accountcode[$i]] > 0) {
+            if ($display) {
+                $costpercall[$i] = round($pricepercall[$accountcode[$i]],2);
+                $cost[$i] += $costpercall[$i];
             }
-            $cost[$i]+=$costperminute[$i];
-        } else {
-            $costperminute[$i] = round(($priceperminute[$accountcode[$i]]/60) * $firstperiod[$accountcode[$i]],2);
-            $cost[$i]+=$costperminute[$i];
         }
-        $costperconnect[$i] = round($priceperconnectedcall[$accountcode[$i]],2);
-        $cost[$i]+=$costperconnect[$i];
-        if ($dst[$i] == "pressed 1") {
-            $costperpress1[$i] = round($priceperpress1[$accountcode[$i]],2);
-            $cost[$i] += $costperpress1[$i];
+        if ($disposition[$i] == "ANSWERED") {
+            if ($billsec[$i] > $firstperiod[$accountcode[$i]]) {
+                if ($increment[$accountcode[$i]] == 1) {
+                    $costperminute[$i] = round(($priceperminute[$accountcode[$i]]/60) * $billsec[$i],2);
+                } else {
+                    /* if the increment is 30 seconds and the call is 73 seconds they should be
+                     * charged for 73/30 = 2.4 blocks - round up to 3 = 3*30 = 90*/
+                    $blocks = ceil($billsec[$i]/$increment[$accountcode[$i]]);
+                    $newsecs = $blocks * $increment[$accountcode[$i]];
+                    $costperminute[$i] = round(($priceperminute[$accountcode[$i]]/60) * $newsecs,2);
+                }
+                $cost[$i]+=$costperminute[$i];
+            } else {
+                $costperminute[$i] = round(($priceperminute[$accountcode[$i]]/60) * $firstperiod[$accountcode[$i]],2);
+                $cost[$i]+=$costperminute[$i];
+            }
+            $costperconnect[$i] = round($priceperconnectedcall[$accountcode[$i]],2);
+            $cost[$i]+=$costperconnect[$i];
+            if ($dst[$i] == "1") {
+                $costperpress1[$i] = round($priceperpress1[$accountcode[$i]],2);
+                $cost[$i] += $costperpress1[$i];
+            }
         }
-    }
-    if ($display) {
-        $totalcost[$accountcode[$i]]+=$cost[$i];
-        $pos = strpos($userfield[$i], '-');
-        if ($pos === false) {
-            // This is not a split
-        } else {
+        if ($display) {
+            $totalcost[$accountcode[$i]]+=$cost[$i];
+            $pos = strpos($userfield[$i], '-');
+            if ($pos === false) {
+                // This is not a split
+            } else {
+                mysql_select_db("SineDialer", $link);
+                $campaignid = substr($userfield[$i], $pos + 1);
+                $sql = "SELECT cost FROM SineDialer.campaign WHERE id = ".$campaignid;
+                $result_campaign_cost = mysql_query($sql,$link);
+                if (mysql_num_rows($result_campaign_cost) > 0) {
+                    $campaign_cost = mysql_result($result_campaign_cost,0,0);
+                } else {
+    	            $campaign_cost = 0;
+    	        }
+    	        $sql = "UPDATE SineDialer.campaign set cost = '".($campaign_cost+$cost[$i])."' WHERE id = ".$campaignid;
+    	        mysql_query($sql,$link);
+    	    }
+    	    $sql = "update cdr set userfield2 = '1' where calldate = '$calldate[$i]' and duration = '$duration[$i]' and accountcode = '$accountcode[$i]' and userfield = '$userfield[$i]'";
+    	    echo $i."/5000\r";
+    	    $result_update = mysql_query($sql,$cdrlink);
+    	}
+    	$i++;
+    } /* end of while on records */
+
+    $sqlx = "select credit,creditlimit from billing where accountcode = '$accountcode_in'";
+
+    mysql_select_db("SineDialer", $link);
+    $result_credit = mysql_query($sqlx,$link)  or die (mysql_error());
+    if (mysql_num_rows($result_credit) > 0) {
+        $credit = mysql_result($result_credit,0,'credit') or die (mysql_error());
+        $credit_limit = mysql_result($result_credit,0,'creditlimit');
+        if (($credit - $totalcost[$accountcode_in]) != $credit) {
+            echo "[".$accountcode_in."] Credit was $credit and will now be ".($credit - $totalcost[$accountcode_in])."\n";
+            $sql = "update billing set credit = ".($credit - $totalcost[$accountcode_in])." where accountcode = '$accountcode_in'";
             mysql_select_db("SineDialer", $link);
 
-            $campaignid = substr($userfield[$i], $pos + 1);
-            $sql = "SELECT cost FROM SineDialer.campaign WHERE id = ".$campaignid;
-            $result_campaign_cost = mysql_query($sql,$link);
-	    if (mysql_num_rows($result_campaign_cost) > 0) {
-            $campaign_cost = mysql_result($result_campaign_cost,0,0);
-	    } else {
-	         $campaign_cost = 0;
-	    }
-            $sql = "UPDATE SineDialer.campaign set cost = '".($campaign_cost+$cost[$i])."' WHERE id = ".$campaignid;
-            mysql_query($sql,$link);
-        }
-        $sql = "update cdr set userfield2 = '1' where calldate = '$calldate[$i]' and duration = '$duration[$i]' and accountcode = '$accountcode[$i]' and userfield = '$userfield[$i]'";
-	echo $i."/5000\r";
-//	echo $sql."\n";
-//        mysql_select_db("SineDialer", $link);
+            $result_update=mysql_query($sql, $link);
+            if ($credit - $totalcost[$accountcode_in] - $totalcost[$accountcode_in] - $totalcost[$accountcode_in] < 0 - $credit_limit[$accountcode_in]) {
+                //This person will run out of money if they do this again
+                mysql_select_db("SineDialer", $link);
 
-        $result_update = mysql_query($sql,$cdrlink);
-//	echo "result: ".$result_update."\n";
-    }
-    $i++;
-}
-//echo "</table>";
-//echo "About to print out totals for $accountcode_in<br />";
-//foreach ($totalcost as $key => $value) {
-//    echo "Key: $key Value: $value";
-//}
-//echo $totalcost[$accountcode_in];
-$sqlx = "select credit,creditlimit from billing where accountcode = '$accountcode_in'";
-//echo $sqlx;
-mysql_select_db("SineDialer", $link);
-
-$result_credit = mysql_query($sqlx,$link)  or die (mysql_error());
-if (mysql_num_rows($result_credit) > 0) {
-    //echo "More than 0 results";
-    $credit = mysql_result($result_credit,0,'credit') or die (mysql_error());
-    $credit_limit = mysql_result($result_credit,0,'creditlimit');
-    if (($credit - $totalcost[$accountcode_in]) != $credit) {
-        echo "[".$accountcode_in."] Credit was $credit and will now be ".($credit - $totalcost[$accountcode_in])."
-";
-        $sql = "update billing set credit = ".($credit - $totalcost[$accountcode_in])." where accountcode = '$accountcode_in'";
-        mysql_select_db("SineDialer", $link);
-
-        $result_update=mysql_query($sql, $link);
-        if ($credit - $totalcost[$accountcode_in] - $totalcost[$accountcode_in] - $totalcost[$accountcode_in] < 0 - $credit_limit[$accountcode_in]) {
-            //This person will run out of money if they do this again
-            mysql_select_db("SineDialer", $link);
-
-            $sql1="delete from queue where campaignid=".$campaignid;
-            $sql2="INSERT INTO queue (campaignid,queuename,status,details,flags,transferclid,
-                starttime,endtime,startdate,enddate,did,clid,context,maxcalls,maxchans,maxretries
-                ,retrytime,waittime) VALUES
-                ('$campaignid','creditstop-$campaignid','2','No details','0','0',
-                '00:00:00','23:59:00','2005-01-01','2020-01-01','123','000',
-                '0','10','500','0'
-                ,'0','30') ";
-            $resultx=mysql_query($sql1, $link) or die (mysql_error());;
-            $resultx=mysql_query($sql2, $link) or die (mysql_error());;
-            $sql = "INSERT INTO log (timestamp, username, activity) VALUES (NOW(), 'System', 'Stopped campaign id $campaignid because credit of $accountcode_in was low (Credit is now $credit and they spent $totalcost[$accountcode_in] in the last minute. Credit Limit is $credit_limit[$accountcode_in])')";
-$result=mysql_query($sql, $link) or die(mysql_error());
-
-
+                $sql1="delete from queue where campaignid=".$campaignid;
+                $sql2="INSERT INTO queue (campaignid,queuename,status,details,flags,transferclid,
+                    starttime,endtime,startdate,enddate,did,clid,context,maxcalls,maxchans,maxretries
+                    ,retrytime,waittime) VALUES
+                    ('$campaignid','creditstop-$campaignid','2','No details','0','0',
+                    '00:00:00','23:59:00','2005-01-01','2020-01-01','123','000',
+                    '0','10','500','0'
+                    ,'0','30') ";
+                $resultx=mysql_query($sql1, $link) or die (mysql_error());;
+                $resultx=mysql_query($sql2, $link) or die (mysql_error());;
+                $sql = "INSERT INTO log (timestamp, username, activity) VALUES (NOW(), 'System', 'Stopped campaign id $campaignid because credit of $accountcode_in was low (Credit is now $credit and they spent $totalcost[$accountcode_in] in the last minute. Credit Limit is $credit_limit[$accountcode_in])')";
+                $result=mysql_query($sql, $link) or die(mysql_error());
+            }
         }
     }
-}
-//echo "<hr>";
-}
-//require "footer.php";
+} /* End of while on customers */
 ?>
