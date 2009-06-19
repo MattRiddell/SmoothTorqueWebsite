1b9e00af4455a06489b85d8f56cbd40d6a710127
matt <MattRiddell@users.noreply.github.com>
2009-06-19 04:21:44 +0000

    Lotsa errors

--- login.php
+++ login.php
@@ -422,20 +422,20 @@ $sql = "CREATE TABLE `number` (
 
   if (!in_array('random_sort', $field_array))
   {
-      echo "Please wait, updating system...this may take a while - please don't stop it";
+      echo "Please wait, updating system...this may take a while - please don't stop it<br />";
       flush();
       sleep(1);
-      echo "Starting with adding the random sort field to the numbers table";
+      echo "Starting with adding the random sort field to the numbers table<br />";
       flush();
       sleep(1);
       $result = mysql_query('ALTER TABLE number ADD random_sort int(10)') or die(mysql_error());
       $sql = "INSERT INTO log (timestamp, username, activity) VALUES (NOW(), '$_POST[user]', 'Added number random_sort field')";
       $result=mysql_query($sql, $link);
-      echo "Added field - now updating the numbers to give them each a random value";
+      echo "Added field - now updating the numbers to give them each a random value<br />";
       flush();
       sleep(1);
       $result = mysql_query('UPDATE number SET random_sort = ROUND(RAND() * 999999999)') or die(mysql_error());
-      $result = mysql_query("ALTER TABLE config ADD INDEX randomize (random_sort, campaignid, status)") or die(mysql_error());;
+      $result = mysql_query("ALTER TABLE number ADD INDEX randomize (random_sort, campaignid, status)") or die(mysql_error());;
       $sql = "INSERT INTO log (timestamp, username, activity) VALUES (NOW(), '$_POST[user]', 'randomized existing number field')";
       echo "Update complete - please log back in";
       ?><META HTTP-EQUIV=REFRESH CONTENT="0; URL=/index.php"><?
