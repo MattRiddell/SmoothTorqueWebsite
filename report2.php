87bedde13f252cf15af4cf00cbe836f5c540c63f
matt <MattRiddell@users.noreply.github.com>
2009-07-03 13:54:25 +0000

    Change report2 because ie is actually same as ff

--- report2.php
+++ report2.php
@@ -46,11 +46,7 @@ swfobject.embedSWF(
 </script>
 <script>
 function findSWF(movieName) {
-  if (navigator.appName.indexOf("Microsoft")!= -1) {
-    return window["ie_" + movieName];
-  } else {
-    return document[movieName];
-  }
+  return document[movieName];
 }
 
 function count()
