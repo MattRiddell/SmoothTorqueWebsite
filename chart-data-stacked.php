c0ab597052386309855aaffdb2f93b8b4719e9cf
matt <MattRiddell@users.noreply.github.com>
2009-07-04 00:00:11 +0000

    Changes

--- chart-data-stacked.php
+++ chart-data-stacked.php
@@ -6,9 +6,6 @@ include 'ofc-library/open-flash-chart.php';
 require_once "admin/db_config.php";
         header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
         header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
-<?php
-
-include_once '../php-ofc-library/open-flash-chart.php';
 
 $title = new title( 'Stuff I\'m thinking about, '.date("D M d Y") );
 $title->set_style( "{font-size: 20px; color: #F24062; text-align: center;}" );
