923490d49c9db1e25ccd9a822c79ca5f76b27cf4
matt <MattRiddell@users.noreply.github.com>
2009-07-13 22:39:44 +0000

    Changes for the new interface

--- footer.php
+++ footer.php
@@ -1,3 +1,152 @@
+<?
+if ($interface_type == "broadcast") {
+?>
+</font>
+
+		
+		</td>
+		<td colspan="2" rowspan="13">
+			<img src="/modules/broadcast/images/broadcast_14.png" width="234" height="482" alt=""></td>
+		<td>
+			<img src="/modules/broadcast/images/spacer.gif" width="1" height="10" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="3" rowspan="10">
+			<img src="/modules/broadcast/images/broadcast_15.png" width="261" height="337" alt=""></td>
+		<td>
+			<a href="/modules/broadcast/upload_list.php">
+				<img src="/modules/broadcast/images/Upload-List.gif" width="192" height="43" border="0" alt="Upload List"></a></td>
+		<td colspan="3" rowspan="10">
+			<img src="/modules/broadcast/images/broadcast_17.png" width="55" height="337" alt=""></td>
+		<td>
+			<img src="/modules/broadcast/images/spacer.gif" width="1" height="43" alt=""></td>
+	</tr>
+	<tr>
+		<td>
+			<img src="/modules/broadcast/images/broadcast_18.png" width="192" height="16" alt=""></td>
+		<td>
+			<img src="/modules/broadcast/images/spacer.gif" width="1" height="16" alt=""></td>
+	</tr>
+	<tr>
+		<td>
+			<a href="/modules/broadcast/upload_messages.php">
+				<img src="/modules/broadcast/images/Upload-Messages.gif" width="192" height="44" border="0" alt="Upload Messages"></a></td>
+		<td>
+			<img src="/modules/broadcast/images/spacer.gif" width="1" height="44" alt=""></td>
+	</tr>
+	<tr>
+		<td>
+			<img src="/modules/broadcast/images/broadcast_20.png" width="192" height="23" alt=""></td>
+		<td>
+			<img src="/modules/broadcast/images/spacer.gif" width="1" height="23" alt=""></td>
+	</tr>
+	<tr>
+		<td>
+			<a href="/modules/broadcast/start_campaign.php">
+				<img src="/modules/broadcast/images/Start-Campaign.gif" width="192" height="42" border="0" alt="Start Campaign"></a></td>
+		<td>
+			<img src="/modules/broadcast/images/spacer.gif" width="1" height="42" alt=""></td>
+	</tr>
+	<tr>
+		<td>
+			<img src="/modules/broadcast/images/broadcast_22.png" width="192" height="18" alt=""></td>
+		<td>
+			<img src="/modules/broadcast/images/spacer.gif" width="1" height="18" alt=""></td>
+	</tr>
+	<tr>
+		<td>
+			<a href="/modules/broadcast/reports.php">
+				<img src="/modules/broadcast/images/View-Reports.gif" width="192" height="42" border="0" alt="View Reports"></a></td>
+		<td>
+			<img src="/modules/broadcast/images/spacer.gif" width="1" height="42" alt=""></td>
+	</tr>
+	<tr>
+		<td>
+			<img src="/modules/broadcast/images/broadcast_24.png" width="192" height="19" alt=""></td>
+		<td>
+			<img src="/modules/broadcast/images/spacer.gif" width="1" height="19" alt=""></td>
+	</tr>
+	<tr>
+		<td>
+			<a href="/modules/broadcast/../../logout.php">
+				<img src="/modules/broadcast/images/Logout.gif" width="192" height="43" border="0" alt="Logout"></a></td>
+		<td>
+			<img src="/modules/broadcast/images/spacer.gif" width="1" height="43" alt=""></td>
+	</tr>
+	<tr>
+		<td>
+			<img src="/modules/broadcast/images/broadcast_26.png" width="192" height="47" alt=""></td>
+		<td>
+			<img src="/modules/broadcast/images/spacer.gif" width="1" height="47" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="2" rowspan="2">
+			<img src="/modules/broadcast/images/broadcast_27.png" width="250" height="135" alt=""></td>
+		<td colspan="3" rowspan="2">
+			<img src="/modules/broadcast/images/broadcast_28.png" width="215" height="135" alt=""></td>
+		<td colspan="2" rowspan="2">
+			<img src="/modules/broadcast/images/broadcast_29.png" width="43" height="135" alt=""></td>
+		<td>
+			<img src="/modules/broadcast/images/spacer.gif" width="1" height="131" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="6">
+			<img src="/modules/broadcast/images/broadcast_30.png" width="658" height="4" alt=""></td>
+		<td>
+			<img src="/modules/broadcast/images/spacer.gif" width="1" height="4" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="15">
+			<img src="/modules/broadcast/images/broadcast_31.png" width="1400" height="20" alt=""></td>
+		<td>
+			<img src="/modules/broadcast/images/spacer.gif" width="1" height="20" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="15">
+			<img src="/modules/broadcast/images/broadcast_32.jpg" width="1400" height="102" alt=""></td>
+		<td>
+			<img src="/modules/broadcast/images/spacer.gif" width="1" height="102" alt=""></td>
+	</tr>
+	<tr>
+		<td>
+			<img src="/modules/broadcast/images/spacer.gif" width="215" height="1" alt=""></td>
+		<td>
+			<img src="/modules/broadcast/images/spacer.gif" width="35" height="1" alt=""></td>
+		<td>
+			<img src="/modules/broadcast/images/spacer.gif" width="11" height="1" alt=""></td>
+		<td>
+			<img src="/modules/broadcast/images/spacer.gif" width="192" height="1" alt=""></td>
+		<td>
+			<img src="/modules/broadcast/images/spacer.gif" width="12" height="1" alt=""></td>
+		<td>
+			<img src="/modules/broadcast/images/spacer.gif" width="37" height="1" alt=""></td>
+		<td>
+			<img src="/modules/broadcast/images/spacer.gif" width="6" height="1" alt=""></td>
+		<td>
+			<img src="/modules/broadcast/images/spacer.gif" width="63" height="1" alt=""></td>
+		<td>
+			<img src="/modules/broadcast/images/spacer.gif" width="114" height="1" alt=""></td>
+		<td>
+			<img src="/modules/broadcast/images/spacer.gif" width="95" height="1" alt=""></td>
+		<td>
+			<img src="/modules/broadcast/images/spacer.gif" width="152" height="1" alt=""></td>
+		<td>
+			<img src="/modules/broadcast/images/spacer.gif" width="117" height="1" alt=""></td>
+		<td>
+			<img src="/modules/broadcast/images/spacer.gif" width="117" height="1" alt=""></td>
+		<td>
+			<img src="/modules/broadcast/images/spacer.gif" width="4" height="1" alt=""></td>
+		<td>
+			<img src="/modules/broadcast/images/spacer.gif" width="230" height="1" alt=""></td>
+		<td></td>
+	</tr>
+</table>
+<!-- End Save for Web Slices -->
+</body>
+</html>
+<?
+} else {
+?>
 <BR><BR></TD></TR></TABLE>
 </BODY>
 <HEAD>
@@ -12,3 +161,5 @@ if ($self == "/test.php" || $self == "/report.php" || $self == "/mysql_stats.php
 ?>
 </HEAD>
 </HTML>
+<?
+}?>
