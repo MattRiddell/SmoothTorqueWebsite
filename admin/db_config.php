dd6edbd2945fe55937b46149fd088a6c11a3fa5e
matt <MattRiddell@users.noreply.github.com>
2009-08-06 03:58:41 +0000

    Changes

--- admin/db_config.php
+++ admin/db_config.php
@@ -1,29 +1,13 @@
 <?
-$config_file = "/stweb.conf";
-$comment = "#";
-
-if (file_exists($config_file)) {
-$fp = fopen($config_file, "r");
-while (!feof($fp)) {
-  $line = trim(fgets($fp));
-  if ($line && substr($line,0,1)!=$comment) {
-    $pieces = explode("=", $line);
-    $option = trim($pieces[0]);
-    $value = trim($pieces[1]);
-    $config_values_temp[$option] = $value;
-  }
-}
-fclose($fp);
-}
-if ($config_values_temp['ST_MYSQL_HOST'] == "") {
+if ($config_values['ST_MYSQL_HOST'] == "") {
     $db_host="localhost";
     $db_user="root";
     $db_pass="";
     $link = mysql_connect($db_host, $db_user, $db_pass) OR die(mysql_error());
 } else {
-    $db_host=$config_values_temp['ST_MYSQL_HOST'];
-    $db_user=$config_values_temp['ST_MYSQL_USER'];
-    $db_pass=$config_values_temp['ST_MYSQL_PASS'];
+    $db_host=$config_values['ST_MYSQL_HOST'];
+    $db_user=$config_values['ST_MYSQL_USER'];
+    $db_pass=$config_values['ST_MYSQL_PASS'];
     $link = mysql_connect($db_host, $db_user, $db_pass) OR die(mysql_error());
 }
 if (!mysql_select_db("SineDialer")) {
