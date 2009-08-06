a8febf3e655ddff875cb0ee401abe399878a168a
matt <MattRiddell@users.noreply.github.com>
2009-08-06 00:04:14 +0000

    Create db if missing

--- admin/db_config.php
+++ admin/db_config.php
@@ -25,5 +25,8 @@ if ($config_values_temp['ST_MYSQL_HOST'] == "") {
     $db_pass=$config_values_temp['ST_MYSQL_PASS'];
     $link = mysql_connect($db_host, $db_user, $db_pass) OR die(mysql_error());
 }
-mysql_select_db("SineDialer") or die(mysql_error());
+if (!mysql_select_db("SineDialer")) {
+	$result = mysql_query("CREATE DATABASE SineDialer");
+	mysql_select_db("SineDialer");
+}
 ?>
