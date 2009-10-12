b29082c91d55c835bcd2b6093120428f4605733b
matt <MattRiddell@users.noreply.github.com>
2009-10-12 22:03:24 +0000

    Config is now stored in MySQL

--- functions/read_config_file.php
+++ functions/read_config_file.php
@@ -1,7 +1,7 @@
 <?
 
 
-if (file_exists($config_file)) {
+/*if (file_exists($config_file)) {
     $fp = fopen($config_file, "r");
     while (!feof($fp)) {
       $line = trim(fgets($fp));
@@ -18,5 +18,5 @@ if (file_exists($config_file)) {
 if ($config_values['FILL_STYLE'] == "") {
     $config_values['FILL_STYLE'] = "gradient";
 }
-
+*/
 ?>
