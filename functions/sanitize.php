29d72556f3693fb4bc340de92f5d0bf905ed3fa7
matt <MattRiddell@users.noreply.github.com>
2009-11-11 23:48:34 +0000

    Indentation fixes

--- functions/sanitize.php
+++ functions/sanitize.php
@@ -1,6 +1,6 @@
 <?
-        if (!function_exists('sanitize') ) {
-        function sanitize($var, $quotes = true) {
+if (!function_exists('sanitize') ) {
+    function sanitize($var, $quotes = true) {
         if (is_array($var)) {   //run each array item through this function (by reference)
             foreach ($var as &$val) {
                 $val = $this->sanitize($val);
