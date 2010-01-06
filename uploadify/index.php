34983a538f880435caac2e3e4de9bc2e16097c75
matt <MattRiddell@users.noreply.github.com>
2010-01-06 01:46:13 +0000

    Changes

--- uploadify/index.php
+++ uploadify/index.php
@@ -1,8 +1,38 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Uploadify Example Script</title>
+<?
+require "../header.php";
+?>
+<style>
+.uploadifyProgressBar {
+	background-color: #0099FF;
+	width: 1px;
+	height: 3px;
+}
+
+.uploadifyQueueItem {
+	font: 11px Verdana, Geneva, sans-serif;
+	border: 2px solid #E5E5E5;
+	background-color: #F5F5F5;
+	margin-top: 5px;
+	padding: 10px;
+	width: 350px;
+}
+.uploadifyError {
+	border: 2px solid #FBCBBC !important;
+	background-color: #FDE5DD !important;
+}
+.uploadifyQueueItem .cancel {
+	float: right;
+}
+.uploadifyProgress {
+	background-color: #FFFFFF;
+	border-top: 1px solid #808080;
+	border-left: 1px solid #808080;
+	border-right: 1px solid #C5C5C5;
+	border-bottom: 1px solid #C5C5C5;
+	margin-top: 10px;
+	width: 100%;
+}
+</style>
 <script type="text/javascript" src="jquery-1.3.2.min.js"></script>
 <script type="text/javascript" src="swfobject.js"></script>
 <script type="text/javascript" src="jquery.uploadify.v2.1.0.min.js"></script>
@@ -10,13 +40,14 @@
 $(document).ready(function() {
 	$("#uploadify").uploadify({
 		'uploader'       : 'uploadify.swf',
-		'script'         : 'uploadify.php?campaignid=<?=$_GET[',
+		'script'         : 'uploadify.php?campaignid=<?=$_POST[campaignid]?>',
 		'cancelImg'      : 'cancel.png',
 		'folder'         : 'uploads',
 		'queueID'        : 'fileQueue',
 		'auto'           : true,
 		'multi'          : false,
-		'onComplete'	 : function(event, queueID, fileObj, reposnse, data) {$('#filesUploaded').append('Uploaded: '+fileObj.name+'<br>');}
+		'onError'	 : function() {alert('error');},
+		'onComplete'	 : function(event, queueID, fileObj, response, data) {$('#filesUploaded').append('Uploaded: '+fileObj.name+' '+response+'<br>');}
 	});
 });
 
--- uploadify/uploadify.php
+++ uploadify/uploadify.php
@@ -23,24 +23,87 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
+//echo "xx";exit(0);
+//require "../header.php";
+require "../header_numbers.php";
+require "../admin/db_config.php";
+
 if (!empty($_FILES)) {
-	$tempFile = $_FILES['Filedata']['tmp_name'];
-	$targetPath = $_SERVER['DOCUMENT_ROOT'] . $_REQUEST['folder'] . '/';
-	$targetFile =  str_replace('//','/',$targetPath) . $_FILES['Filedata']['name'];
-	
-	// $fileTypes  = str_replace('*.','',$_REQUEST['fileext']);
-	// $fileTypes  = str_replace(';','|',$fileTypes);
-	// $typesArray = split('\|',$fileTypes);
-	// $fileParts  = pathinfo($_FILES['Filedata']['name']);
-	
-	// if (in_array($fileParts['extension'],$typesArray)) {
-		// Uncomment the following line if you want to make the directory if it doesn't exist
-		// mkdir(str_replace('//','/',$targetPath), 0755, true);
-		
-//		move_uploaded_file($tempFile,$targetFile);
-		echo "nla1111";
-	// } else {
-	// 	echo 'Invalid file type.';
-	// }
+
+	$filename = $_FILES['Filedata']['tmp_name'];
+	$row = 0;
+        $display2 = 0;
+        $handle = fopen($filename, "r");
+        echo "<br />Importing numbers, please wait<br /><br />";
+        //print_r($_POST);
+        $campaignid = $_GET['campaignid'];
+        //$sql2 = "LOCK TABLES number WRITE";
+        //mysql_query($sql2, $link) or die (mysql_error());;
+        $sql = "INSERT IGNORE INTO number (campaignid,phonenumber,status,type, random_sort) VALUES";
+        $isfirst=true;
+        while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+            //echo "Inside Loop: ".$data[0]."<br />";
+            $data[0] = str_replace("(","",$data[0]);
+            $data[0] = str_replace(")","",$data[0]);
+            $data[0] = str_replace("-","",$data[0]);
+            $data[0] = str_replace(" ","",$data[0]);
+            $data[0] = str_replace("\r","",$data[0]);
+            if ($isfirst) {
+                $sql.="(".$campaignid.",'".$data[0]."','new',0, ROUND(RAND() * 999999999))";
+
+//                $sql2 = "SET AUTOCOMMIT=0;";//BEGIN";
+//                mysql_query($sql2, $link) or die(mysql_error());
+//                echo mysql_error();
+
+                $isfirst=false;
+            }
+            $row++;
+            $display++;
+            $display2++;
+            /*if ($display2>500){
+                //echo "<!-- -->";
+                //ob_flush();flush();
+
+                $display2=0;
+            }*/
+            if ($display > 17347) { /* Just so the chances of doing nothing  */
+                                   /* in the last write is low.  It doesn't */
+                                   /* really matter but makes it cleaner */
+                echo "".$row." numbers imported<br />\n";
+                ob_flush();flush();
+                //echo "saving $sql";
+                mysql_query($sql, $link) or die (mysql_error());;
+                //$sql2="COMMIT";
+                //mysql_query($sql2, $link) or die (mysql_error());;
+                //$sql2="UNLOCK TABLES";
+                //mysql_query($sql2, $link) or die (mysql_error());;
+
+
+				$display = 0;
+                //$sq2 = "LOCK TABLES number WRITE";
+                //mysql_query($sql2, $link) or die (mysql_error());;
+                $sql = "INSERT IGNORE INTO number (campaignid,phonenumber,status,type,random_sort)  VALUES";
+                $sql.="(".$campaignid.",'".$data[0]."','new',0,ROUND(RAND() * 999999999))";
+            } else {
+				$sql.=",(".$campaignid.",'".$data[0]."','new',0, ROUND(RAND() * 999999999))";
+			}
+        }
+        //echo "Saving Records to the Database <br />";
+        echo "[".$row." numbers inserted]<br />\n";
+        ob_flush();flush();
+        mysql_query($sql, $link) or die (mysql_error());;
+                //$sql2="COMMIT";
+                //mysql_query($sql2, $link) or die (mysql_error());;
+                //$sql2="UNLOCK TABLES";
+                //mysql_query($sql2, $link) or die (mysql_error());;
+
+			/*$sql2 = "SET AUTOCOMMIT=1;";
+		mysql_query($sql2, $link) or die (mysql_error());;*/
+//$row--;
+echo "<br />";
+echo "<br />";
+fclose($handle);
+echo "<b>A total of $row numbers were inserted into the database</b><br /><br /><br />";
+
 }
 ?>
