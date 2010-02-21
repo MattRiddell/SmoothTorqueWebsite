4ddbcabd7c4b69da447a25ca5c61e5e863156664
matt <MattRiddell@users.noreply.github.com>
2010-02-21 02:05:58 +0000

    Hmmm

--- json.php
+++ json.php
@@ -3,7 +3,7 @@ require "admin/db_config.php";
 $result = mysql_query("SELECT * FROM SineDialer.servers");
 while ($row = mysql_fetch_assoc($result)) {
 	$server_names[] = $row['name'];
-	$escaped_name = str_replace(" ","_",$row['name']);
+	$escaped_name = str_replace(" "," ",$row['name']);
 	$result2 = mysql_query("SELECT value FROM SineDialer.config WHERE parameter = 's_".$escaped_name."_calls'") or die(mysql_error());
 	if (mysql_num_rows($result2) > 0) {
 		$server_chans[] = mysql_result($result2,0,0);
