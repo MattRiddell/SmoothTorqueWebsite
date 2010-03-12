748005cace1f83c52391340a56f88aeab8c324dd
matt <MattRiddell@users.noreply.github.com>
2010-03-12 13:06:00 +0000

    Few more changes

--- pull_records.php
+++ pull_records.php
@@ -409,6 +409,7 @@ foreach ($numbers as $tier=>$values) {
                     break;
                 }
             }
+            $number = pregreplace('[\D]', '', $number);
             $sql = "INSERT INTO SineDialer.number (campaignid, phonenumber, random_sort) VALUES ('$campaignid','$number','1000')";
             echo $sql."<br />";
         } else {
