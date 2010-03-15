570ae7c02234de90a6e3a72b76fc2261b6b434e6
matt <MattRiddell@users.noreply.github.com>
2010-03-15 21:03:57 +0000

    GMT vs PST

--- pull_records.php
+++ pull_records.php
@@ -342,8 +342,11 @@ if ($tz_count == 0) {
                             // Not urgent Source
                         }
                         
+                        date_default_timezone_set("UTC");
+                        
                         $entered = strtotime($row['date_entered']);
                         $now = time();
+                        date_default_timezone_set("US/Pacific");
                         //echo "Now: $now Enterred: $entered Seconds ago: ".($now-$entered)."<br />";
                         
                         $random_sort[$number] = $now - $entered;
