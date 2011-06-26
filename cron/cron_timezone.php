4a8b599e8a8621192dec5fcfb4396bee22ab90bd
matt <MattRiddell@users.noreply.github.com>
2011-06-26 23:28:31 +0000

    Use comments to describe what the code will do

--- cron/cron_timezone.php
+++ cron/cron_timezone.php
@@ -1,4 +1,15 @@
 #!/usr/bin/php
 <?
 /* Cron Job for changing the status of numbers between "new" and "new_nodial" */
+
+/* Find out what the current time is in UTC */
+
+/* Get a list of all number prefixes that should not be running right now */
+
+/* Get a list of all number prefixes that should be running right now */
+
+/* Update the status of all numbers */
+
+/* Add a log entry to say what we've done */
+
 ?>
