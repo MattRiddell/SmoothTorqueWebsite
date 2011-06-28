ec4454e8bbc7c1d9290a1a9090c3b8dc75231fb8
matt <MattRiddell@users.noreply.github.com>
2011-06-28 02:20:22 +0000

    Sox version changes

--- addmessage.php
+++ addmessage.php
@@ -7,35 +7,35 @@ $result=mysql_query($sql, $link) or die (mysql_error());;
 $campaigngroupid=mysql_result($result,0,'campaigngroupid');
 
 if (isset($_POST[name])){
-/*    require_once "PHPTelnet.php";
-
-    $telnet = new PHPTelnet();
-    $result = $telnet->Connect();
-    $telnet->DoCommand('selectcg', $result);
-    $telnet->DoCommand($_COOKIE[user], $result);
-    if (substr(trim($result),0,7)=="GroupID") {
-        $campaigngroupid=substr(trim($result),8);
-    }
-    $telnet->Disconnect();
-
- */
+    /*    require_once "PHPTelnet.php";
+     
+     $telnet = new PHPTelnet();
+     $result = $telnet->Connect();
+     $telnet->DoCommand('selectcg', $result);
+     $telnet->DoCommand($_COOKIE[user], $result);
+     if (substr(trim($result),0,7)=="GroupID") {
+     $campaigngroupid=substr(trim($result),8);
+     }
+     $telnet->Disconnect();
+     
+     */
     $id=$_POST[id];
     $name=$_POST[name];
     $description=$_POST[description];
     $filename=$_POST[filename];
     $length=$_POST[length];
-
-//    $sql="INSERT INTO campaign (groupid,name,description,messageid,messageid2,messageid3) VALUES ('customerid','$name', '$description', '$filename')";
-//    echo $sql;
-
-/*    require_once "sql.php";
-    $SMDB2=new SmDB();*/
+    
+    //    $sql="INSERT INTO campaign (groupid,name,description,messageid,messageid2,messageid3) VALUES ('customerid','$name', '$description', '$filename')";
+    //    echo $sql;
+    
+    /*    require_once "sql.php";
+     $SMDB2=new SmDB();*/
     $sql="insert  into campaignmessage (filename,name,description,customer_id,length) values".
-         "('$filename', '$name', '$description',$campaigngroupid,'$length')";
+    "('$filename', '$name', '$description',$campaigngroupid,'$length')";
     $result=mysql_query($sql, $link) or die (mysql_error());;
-/*    $SMDB2->executeUpdate($sql);*/
-
-
+    /*    $SMDB2->executeUpdate($sql);*/
+    
+    
     header("Location: /messages.php");
     exit;
 }
@@ -61,7 +61,23 @@ $campaigngroupid=$groupid;
 <INPUT TYPE="TEXT" NAME="description" VALUE="<?echo $row[description];?>" size="60">
 <INPUT TYPE="HIDDEN" NAME="filename" VALUE="<?echo $row[filename];?><?if(isset($_GET[filename])){echo $_GET[filename];}?>" size="60">
 <?
-$command = $config_values['SOX']." uploads/".str_replace("sln","wav",str_replace(" ","\ ",$_GET[filename])).' -e stat';
+
+
+
+exec($config_values['SOX']." --version",$retval);
+$split = explode(":",$retval[0]);
+$sox_version = substr(trim($split[1]), 5);
+/* $sox_major is now 14, $sox_minor is 1 and $sox_point is 0 */
+list($sox_major, $sox_minor, $sox_point) = explode(".",$sox_version);	
+
+if ($sox_major <= 14) {
+    /* Use -n option */
+    $command = $config_values['SOX']." uploads/".str_replace("sln","wav",str_replace(" ","\ ",$_GET[filename])).' -n stat';
+} else {
+    /* Use -e option */
+    $command = $config_values['SOX']." uploads/".str_replace("sln","wav",str_replace(" ","\ ",$_GET[filename])).' -e stat';
+}
+
 //$command = "sox";
 //echo $command;
 $x = exec($command." 2>&1",$y);
