5e1a891ecf246eb306f6c7e971fa0a2ec5ba47bb
matt <MattRiddell@users.noreply.github.com>
2011-09-07 07:52:44 +0000

    Remove unneeded file

--- home.php
+++
@@ -1,7 +0,0 @@
-<?
-$pagenum=0;
-require "header.php";?>
-
-Welcome to VentureVoIP's flagship predictive Dialer Product, SmoothTorque<br \>
-<br \>
-Chose one of the menu options from above to continue.
