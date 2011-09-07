7750a403ccf534ef69e2c00ae8685ff929cef8bb
matt <MattRiddell@users.noreply.github.com>
2011-09-07 07:40:55 +0000

    reindent and add new_nodial for timezones

--- chart-data.php
+++ chart-data.php
@@ -4,8 +4,8 @@ error_reporting(0);
 
 include 'ofc-library/open-flash-chart.php';
 require_once "admin/db_config.php";
-        header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
-        header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 
 $result = mysql_query("SELECT count(*) FROM SineDialer.number where campaignid = $_GET[campaignid]") or die(mysql_error());
 $absolute_total = mysql_result($result,0,0);
@@ -15,63 +15,66 @@ $result = mysql_query("SELECT distinct(status), count(*) FROM SineDialer.number 
 $max = 0;
 $count = 0;
 /*
-amd
-answered
-engaged
-congested
-hungup
-indnc
-pressed1
-noanswer
-*/
+ amd
+ answered
+ engaged
+ congested
+ hungup
+ indnc
+ pressed1
+ noanswer
+ */
 $colors = array('#0000FF','#888888','#00FF00','#FF0000','#004444', '#FF0000', '#000000','#666600') ;
 $size = sizeof($colors);
 $total = 0;
 while ($row = mysql_fetch_assoc($result)) {
-        if ($row['count(*)'] > 0) {
-                $status_name = $row['status'];
-                $total+= $row['count(*)'];
-                switch ($status_name) {
-                        case "amd":
-                                $status_name = "Answer Machine";
-                                break;
-                        case "indnc":
-                                $status_name = "Do Not Call";
-                                break;
-                        case "hungup":
-                                $status_name = "Hung Up";
-                                break;
-                        case "pressed1":
-                                $status_name = "Transferred";
-                                break;
-                        case "busy":
-                                $status_name = "Engaged";
-                                break;
-                        case "timeout":
-                                $status_name = "No Answer";
-                                break;
-                        default:
-                                $status_name = ucfirst($status_name);
-                                break;
-                }
-                $perc = round((intval($row['count(*)'])/$total_done) *100,2);
-                $bar = new pie_value(intval($row['count(*)']), $status_name." (".$perc."%)");
-                if ($count > $size) {
-                        $count = 0;
-                }
-                $bar->set_tooltip ($status_name." (".$row['count(*)'].")");
-                $x_labels[] = $status_name;
-                $status[] = $bar;
-                $count++;
+    if ($row['count(*)'] > 0) {
+        $status_name = $row['status'];
+        $total+= $row['count(*)'];
+        switch ($status_name) {
+            case "amd":
+                $status_name = "Answer Machine";
+                break;
+            case "indnc":
+                $status_name = "Do Not Call";
+                break;
+            case "hungup":
+                $status_name = "Hung Up";
+                break;
+            case "pressed1":
+                $status_name = "Transferred";
+                break;
+            case "busy":
+                $status_name = "Engaged";
+                break;
+            case "timeout":
+                $status_name = "No Answer";
+                break;
+            case "new_nodial":
+                $status_name = "Timezone Not Dialling";
+                break;
+            default:
+                $status_name = ucfirst($status_name);
+                break;
         }
-        if ($row['count(*)'] > $max) {
-                $max = $row['count(*)'];
+        $perc = round((intval($row['count(*)'])/$total_done) *100,2);
+        $bar = new pie_value(intval($row['count(*)']), $status_name." (".$perc."%)");
+        if ($count > $size) {
+            $count = 0;
         }
+        $bar->set_tooltip ($status_name." (".$row['count(*)'].")");
+        $x_labels[] = $status_name;
+        $status[] = $bar;
+        $count++;
+    }
+    if ($row['count(*)'] > $max) {
+        $max = $row['count(*)'];
+    }
 }
 $bar = new pie();
 $bar->set_values($status);
 $bar->set_colours( $colors );
-                
+
 $chart = new open_flash_chart();
 $chart->set_title( null );
 $chart->add_element( $bar );
