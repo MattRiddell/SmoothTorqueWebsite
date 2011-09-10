dd35e0b68574a7d67dc632d61cdbc1769208adca
matt <MattRiddell@users.noreply.github.com>
2011-09-10 08:13:50 +0000

    Remove unused links

--- header_surveys.php
+++ header_surveys.php
@@ -1,7 +1,5 @@
-<?box_start(400);?>
+<?box_start(150);?>
 <table align="center" border="0" width="100%" cellpadding="0" cellspacing="3"><TR>
-<TD><A HREF="surveys.php?add=1"><img src="images/add.png" border="0" align="left">Add Survey</A>&nbsp;&nbsp;</TD>
-<TD><A HREF="surveys.php?add=1"><img src="images/add.png" border="0" align="left">Add Survey</A>&nbsp;&nbsp;</TD>
 <TD><A HREF="surveys.php?add=1"><img src="images/add.png" border="0" align="left">Add Survey</A>&nbsp;&nbsp;</TD>
 </TR></table><?box_end();?>
 
