90b15865c0bb9bc82aba7b01ce5cbeedcf402a7d
matt <MattRiddell@users.noreply.github.com>
2011-09-10 23:57:49 +0000

    lower case html

--- header_surveys.php
+++ header_surveys.php
@@ -1,7 +1,7 @@
 <?box_start(150);?>
-<table align="center" border="0" width="100%" cellpadding="0" cellspacing="3"><TR>
-<TD><A HREF="surveys.php?add=1"><img src="images/table_add.png" border="0" align="left">Add Survey</A>&nbsp;&nbsp;</TD>
-</TR></table><?box_end();?>
+<table align="center" border="0" width="100%" cellpadding="0" cellspacing="3"><tr>
+<td><a href="surveys.php?add=1"><img src="images/table_add.png" border="0" align="left">Add Survey</a>&nbsp;&nbsp;</td>
+</tr></table><?box_end();?>
 
-<BR>
+<br />
 <?flush();?>
