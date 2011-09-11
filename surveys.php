e79be4c5f3becae9966259f9f92fbb90350fa82d
matt <MattRiddell@users.noreply.github.com>
2011-09-11 02:00:47 +0000

    Start providing the choices section

--- surveys.php
+++ surveys.php
@@ -3,6 +3,8 @@ require "header.php";
 require "header_surveys.php";
 
 if (isset($_GET['edit'])) {
+    ?><script type="text/javascript" src="<?=$http_dir_name?>ajax/jquery.js"></script><?
+
     $result = mysql_query("SELECT * FROM surveys WHERE id = ".sanitize($_GET['edit']));
     if (mysql_num_rows($result) == 0) {
         echo "Incorrect ID";
@@ -10,8 +12,7 @@ if (isset($_GET['edit'])) {
         $row = mysql_fetch_assoc($result);
         $name = $row['name'];
         $description = $row['description'];
-        $result_choices = mysql_query("SELECT count(*) FROM survey_choices WHERE survey_id = ".$row['id']);
-        box_start(400);
+        box_start(600);
         ?>
         <center>
         <form action="surveys.php?save_edit=1" method="post">
@@ -26,11 +27,29 @@ if (isset($_GET['edit'])) {
         </tr>
         </table>
         </form>
-        <br />
-        <br />
-        <h3>Choices</h3>
-        
+        <h3>Choices</h3>        
+        <a href="#"><img src="images/add.png" alt="Add Choice">&nbsp;Add Choice</a><br />
+        <table width="540" border="1">
         <?
+        $result_choices = mysql_query("SELECT * FROM survey_choices WHERE survey_id = ".$row['id']." order by question_number");
+        if (mysql_num_rows($result_choices) > 0) {
+            while ($row_choices = mysql_fetch_assoc($result_choices)) {
+                echo "<tr>";
+                if ($row_choices['question_number'] == 0) {
+                    // Invalid choice message
+                    echo '<td>Invalid Choice Message</td>';
+                    echo '<td>'.$row_choices['soundfile'].'</td>';
+                    echo '<td>'.$row_choices['choices'].'</td>';
+                } else {
+                    // Valid choice message
+                    echo '<td>'.$row_choices['question_number'].'</td>';
+                    echo '<td>'.$row_choices['soundfile'].'</td>';
+                    echo '<td>'.$row_choices['choices'].'</td>';
+                }
+                echo "</tr>";
+            }
+        }
+        echo "</table>";
         box_end();
     }
     require "footer.php";
