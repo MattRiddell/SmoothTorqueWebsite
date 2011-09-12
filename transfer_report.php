c0d8fad9c95a6591c56de14d29552881dcddacda
matt <MattRiddell@users.noreply.github.com>
2011-09-12 08:33:03 +0000

    Add links

--- transfer_report.php
+++ transfer_report.php
@@ -1,8 +1,14 @@
 <?
 require "header.php";
 require "header_surveys.php";
-box_start(500);
-echo "<center><br />Coming Soon<br /><br />";
+box_start(250);
+echo "<center>";
+?>
+<h3>Transfer Reports</h3>
+<a href="transfer_report.php?all_campaigns=1"><img src="images/folder.png">&nbsp;All Campaigns</a><br /><br />
+<a href="transfer_report.php?historical=1"><img src="images/calendar.png">&nbsp;Historical Transfers</a><br />
+<br />
+<?
 box_end();
 require "footer.php";
 ?>
