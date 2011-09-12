4fda4057f12519d9bd720b4c8277b8c642211a77
matt <MattRiddell@users.noreply.github.com>
2011-09-12 08:12:38 +0000

    Download report as CSV

--- survey_responses.php
+++ survey_responses.php
@@ -1,23 +1,27 @@
 <?
-require "header.php";
-require "header_surveys.php";
 if (isset($_GET['campaign_id'])) {
+    require "admin/db_config.php";
+    require "functions/sanitize.php";
     $campaign_name = mysql_result(mysql_query("SELECT name FROM campaign WHERE id = ".sanitize($_GET['campaign_id'])),0,0);
-    box_start(400);
-    echo "<center>";
-    echo "<h3>".$campaign_name."</h3>";
+    header("Content-type: application/csv"); 
+    header("Content-Disposition: attachment; filename=".$campaign_name."_".@date('l jS \of F Y h:i:s A').".csv");
+    header("Pragma: no-cache");
+    header("Expires: 0");
+    echo "Phone Number, Question, Choice, Date/Time\n";
     $result = mysql_query("SELECT * FROM survey_results WHERE campaign_id = ".sanitize($_GET['campaign_id'])." ORDER by phonenumber, question") or die(mysql_error());
     if (mysql_num_rows($result) == 0) {
         echo "There are no responses";
     } else {
         while ($row = mysql_fetch_assoc($result)) {
-            print_pre($row);
+//            print_r($row);
+            echo $row['phonenumber'].",".$row['question'].",".$row['choice'].",".$row['datetime']."\n";
         }
     }
-    box_end();
-    require "footer.php";
+//    require "footer.php";
     exit(0);
 }
+require "header.php";
+require "header_surveys.php";
 box_start(400);
 echo "<center>";
 $result = mysql_query("SELECT campaign_id, count(*) as count FROM survey_results group by campaign_id");
