64c6cc41aa02689bb89383d1f004c692178505e8
matt <MattRiddell@users.noreply.github.com>
2011-09-12 08:41:50 +0000

    Start fleshing out transfer reports

--- transfer_report.php
+++ transfer_report.php
@@ -1,6 +1,38 @@
 <?
 require "header.php";
 require "header_surveys.php";
+if (isset($_GET['all_campaigns'])) {
+    box_start();
+    echo "<center><br />Coming Soon";
+    require "footer.php";
+    exit(0);
+}
+if (isset($_GET['historical_campaign'])) {
+    box_start();
+    echo "<center><br />Coming Soon";
+    require "footer.php";
+    exit(0);
+}
+if (isset($_GET['historical'])) {
+    box_start();
+    echo "<center><h3>Select Campaign:</h3>";
+    //TODO: split by user/admin
+    $result = mysql_query("SELECT * from campaign");
+    ?>
+    <form action="transfer_report.php?historical_campaign=1" method="post">
+    <select name="campaign_id">
+    <?
+    while ($row = mysqL_fetch_assoc($result)) {
+        echo '<option value="'.$row['id'].'">'.$row['name'].'</option>';
+    }
+    ?>
+    </select>
+    <input type="submit" value="Display Report">
+    </form>
+    <?
+    require "footer.php";
+    exit(0);
+}
 box_start(250);
 echo "<center>";
 ?>
