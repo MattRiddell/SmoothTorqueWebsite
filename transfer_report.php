38005c779b9eb786740ddacad1e07c98a18ebb76
matt <MattRiddell@users.noreply.github.com>
2011-09-13 09:13:30 +0000

    Provide transfer reports across the board

--- transfer_report.php
+++ transfer_report.php
@@ -2,10 +2,77 @@
 require "header.php";
 require "header_surveys.php";
 if (isset($_GET['all_campaigns'])) {
-    box_start();
-    echo "<center><br />Coming Soon";
+    box_start(800);
+    echo "<center><br /><h3>All Campaigns</h3>";
+    
+    $totals = array();
+    $billables = array();
+    $group_0_to_29 = array();
+    $group_30_to_119 = array();
+    $group_120_to_299 = array();
+    $group_300_to_600 = array();
+    $group_600_to_900 = array();
+    $group_900_plus = array();
+    
+    if (isset($_GET['span'])) {
+        $result = mysql_query("SELECT userfield, billsec FROM cdr WHERE amaflags = '-1' AND calldate >= DATE_SUB(CURDATE(), INTERVAL ".sanitize($_GET['span'])." DAY)") or die(mysql_error());
+    } else {
+        $result = mysql_query("SELECT userfield, billsec FROM cdr WHERE amaflags = '-1'");
+    }
+    
+    if (mysql_num_rows($result) > 0) {
+        while ($row = mysqL_fetch_assoc($result)) {
+            $userfield = split("-",$row['userfield']);
+            $campaign_id = $userfield[1];
+            //echo $campaign_id."<br />";;
+            //echo "CallDate: ".$row['calldate']." Length: ".$row['billsec']."<br />";
+            $totals[$campaign_id][] = $row['billsec'];
+            if ($row['billsec'] < 30) {
+                $group_0_to_29[$campaign_id][] = $row['billsec'];
+            } else if ($row['billsec'] < 120) {
+                $group_30_to_119[$campaign_id][] = $row['billsec'];
+                $billables[$campaign_id][] = $row['billsec'];
+            } else if ($row['billsec'] < 300) {
+                $group_120_to_299[$campaign_id][] = $row['billsec'];
+                $billables[$campaign_id][] = $row['billsec'];
+            } else if ($row['billsec'] < 600) {
+                $group_300_to_600[$campaign_id][] = $row['billsec'];
+                $billables[$campaign_id][] = $row['billsec'];
+            } else if ($row['billsec'] < 900) {
+                $group_600_to_900[$campaign_id][] = $row['billsec'];
+                $billables[$campaign_id][] = $row['billsec'];
+            } else {
+                $group_900_plus[$campaign_id][] = $row['billsec'];
+                $billables[$campaign_id][] = $row['billsec'];
+            }
+        }
+    }
+    //print_pre($totals);
+    ?>
+    <table class="transfer_history">
+    <tr>
+    <th class="transfer_history">Campaign</th><th class="transfer_history">Total Xfers</th><th class="transfer_history">Billable Xfers</th><th class="transfer_history">0-29 secs</th><th class="transfer_history">30-119 secs</th><th class="transfer_history">2-5 mins</th><th class="transfer_history">5-10 mins</th><th class="transfer_history">10-15 mins</th><th class="transfer_history">15+ mins</th></th>
+    </tr>
+    <?
+    foreach ($totals as $name=>$entry) {
+        $campaign_name = mysql_result(mysql_query("SELECT name FROM campaign WHERE id = ".sanitize($name)),0,0);
+        echo "<tr>";
+        echo "<td class=\"transfer_history\">$campaign_name</td>";
+        echo "<td class=\"transfer_history\">".count($entry)."</td>";
+        echo "<td class=\"transfer_history\">".count($billables[$name])."</td>";
+        echo "<td class=\"transfer_history\">".count($group_0_to_29[$name])."</td>";
+        echo "<td class=\"transfer_history\">".count($group_30_to_119[$name])."</td>";
+        echo "<td class=\"transfer_history\">".count($group_120_to_299[$name])."</td>";
+        echo "<td class=\"transfer_history\">".count($group_300_to_600[$name])."</td>";
+        echo "<td class=\"transfer_history\">".count($group_600_to_900[$name])."</td>";
+        echo "<td class=\"transfer_history\">".count($group_900_plus[$name])."</td>";
+        echo "</tr>";
+        //print_pre($entry);
+    }
+    echo "</table>";
     require "footer.php";
     exit(0);
+
 }
 if (isset($_GET['historical_campaign'])) {
     box_start(800);
@@ -95,7 +162,9 @@ box_start(250);
 echo "<center>";
 ?>
 <h3>Transfer Reports</h3>
-<a href="transfer_report.php?all_campaigns=1"><img src="images/folder.png">&nbsp;All Campaigns</a><br /><br />
+<a href="transfer_report.php?all_campaigns=1"><img src="images/folder.png">&nbsp;All Campaigns All Time</a><br /><br />
+<a href="transfer_report.php?all_campaigns=1&span=0"><img src="images/folder.png">&nbsp;All Campaigns Today</a><br /><br />
+<a href="transfer_report.php?all_campaigns=1&span=7"><img src="images/folder.png">&nbsp;All Campaigns Last 7 Days</a><br /><br />
 <a href="transfer_report.php?historical=1"><img src="images/calendar.png">&nbsp;Historical Transfers</a><br />
 <br />
 <?
