6f5aab71b56dd09eb86ee36b159475503ff3bbc2
matt <MattRiddell@users.noreply.github.com>
2011-09-14 01:37:06 +0000

    Update every 5 seconds

--- transfer_report.php
+++ transfer_report.php
@@ -1,76 +1,7 @@
 <?
-require "header.php";
-require "header_surveys.php";
-if (isset($_GET['all_campaigns'])) {
-    box_start(800);
-    echo "<center><br /><h3>All Campaigns</h3>";
-    
-    $totals = array();
-    $billables = array();
-    $group_0_to_29 = array();
-    $group_30_to_119 = array();
-    $group_120_to_299 = array();
-    $group_300_to_600 = array();
-    $group_600_to_900 = array();
-    $group_900_plus = array();
-    
-    if (isset($_GET['span'])) {
-        $result = mysql_query("SELECT userfield, billsec FROM cdr WHERE amaflags = '-1' AND calldate >= DATE_SUB(CURDATE(), INTERVAL ".sanitize($_GET['span'])." DAY)") or die(mysql_error());
-    } else {
-        $result = mysql_query("SELECT userfield, billsec FROM cdr WHERE amaflags = '-1'");
-    }
-    
-    if (mysql_num_rows($result) > 0) {
-        while ($row = mysqL_fetch_assoc($result)) {
-            $userfield = split("-",$row['userfield']);
-            $campaign_id = $userfield[1];
-            //echo $campaign_id."<br />";;
-            //echo "CallDate: ".$row['calldate']." Length: ".$row['billsec']."<br />";
-            $totals[$campaign_id][] = $row['billsec'];
-            if ($row['billsec'] < 30) {
-                $group_0_to_29[$campaign_id][] = $row['billsec'];
-            } else if ($row['billsec'] < 120) {
-                $group_30_to_119[$campaign_id][] = $row['billsec'];
-                $billables[$campaign_id][] = $row['billsec'];
-            } else if ($row['billsec'] < 300) {
-                $group_120_to_299[$campaign_id][] = $row['billsec'];
-                $billables[$campaign_id][] = $row['billsec'];
-            } else if ($row['billsec'] < 600) {
-                $group_300_to_600[$campaign_id][] = $row['billsec'];
-                $billables[$campaign_id][] = $row['billsec'];
-            } else if ($row['billsec'] < 900) {
-                $group_600_to_900[$campaign_id][] = $row['billsec'];
-                $billables[$campaign_id][] = $row['billsec'];
-            } else {
-                $group_900_plus[$campaign_id][] = $row['billsec'];
-                $billables[$campaign_id][] = $row['billsec'];
-            }
-        }
-    }
-    //print_pre($totals);
-    ?>
-    <table class="transfer_history">
-    <tr>
-    <th class="transfer_history">Campaign</th><th class="transfer_history">Total Xfers</th><th class="transfer_history">Billable Xfers</th><th class="transfer_history">0-29 secs</th><th class="transfer_history">30-119 secs</th><th class="transfer_history">2-5 mins</th><th class="transfer_history">5-10 mins</th><th class="transfer_history">10-15 mins</th><th class="transfer_history">15+ mins</th></th>
-    </tr>
-    <?
-    foreach ($totals as $name=>$entry) {
-        $campaign_name = mysql_result(mysql_query("SELECT name FROM campaign WHERE id = ".sanitize($name)),0,0);
-        echo "<tr>";
-        echo "<td class=\"transfer_history\">$campaign_name</td>";
-        echo "<td class=\"transfer_history\">".count($entry)."</td>";
-        echo "<td class=\"transfer_history\">".count($billables[$name])."</td>";
-        echo "<td class=\"transfer_history\">".count($group_0_to_29[$name])."</td>";
-        echo "<td class=\"transfer_history\">".count($group_30_to_119[$name])."</td>";
-        echo "<td class=\"transfer_history\">".count($group_120_to_299[$name])."</td>";
-        echo "<td class=\"transfer_history\">".count($group_300_to_600[$name])."</td>";
-        echo "<td class=\"transfer_history\">".count($group_600_to_900[$name])."</td>";
-        echo "<td class=\"transfer_history\">".count($group_900_plus[$name])."</td>";
-        echo "</tr>";
-        //print_pre($entry);
-    }
-    echo "</table>";
-    
+
+if (isset($_GET['live_calls'])) {
+    require "admin/db_config.php";
     $field[0] = "channel";
     $field[1] = "context";
     $field[2] = "exten";
@@ -144,23 +75,112 @@ if (isset($_GET['all_campaigns'])) {
     </tr>
     <?
     if (count($channels) > 0) {
-    foreach ($channels as $chan=>$values) {
-        if ($values['context'] == "dial-cc") {
-        echo "<tr>";
-        echo "<td class=\"transfer_history\">$values[callerid]</td>";
-        echo "<td class=\"transfer_history\">$values[data]</td>";
-        $start = @mktime(@date("h"),@date("i"), @date("s")-$values['duration'], @date("m")  , @date("d"), @date("Y"));
-        $created = @Date("m/d/Y H:i:s",$start);
-        echo "<td class=\"transfer_history\">$created</td>";
-        
-        echo "<td class=\"transfer_history\">".$channels[$values['bridged']]['duration']."</td>";
-        echo "<td class=\"transfer_history\">".round($channels[$values['bridged']]['duration']/60,2)."</td>";
-        echo "<td class=\"transfer_history\">".$values['duration']."</td>";
-        echo "<tr>";
+        foreach ($channels as $chan=>$values) {
+            if (1 || $values['context'] == "dial-cc") {
+                echo "<tr>";
+                echo "<td class=\"transfer_history\">$values[callerid]</td>";
+                echo "<td class=\"transfer_history\">$values[data]</td>";
+                $start = @mktime(@date("h"),@date("i"), @date("s")-$values['duration'], @date("m")  , @date("d"), @date("Y"));
+                $created = @Date("m/d/Y H:i:s",$start);
+                echo "<td class=\"transfer_history\">$created</td>";
+                
+                echo "<td class=\"transfer_history\">".$channels[$values['bridged']]['duration']."</td>";
+                echo "<td class=\"transfer_history\">".round($channels[$values['bridged']]['duration']/60,2)."</td>";
+                echo "<td class=\"transfer_history\">".$values['duration']."</td>";
+                echo "<tr>";
+            }
         }
     }
+    echo "</table>";
+    exit(0);
+}
+
+require "header.php";
+require "header_surveys.php";
+
+if (isset($_GET['all_campaigns'])) {
+    ?>
+    <script src="js/jquery.min.1.6.3.js" type="text/javascript"></script>
+    
+    <?
+    box_start(800);
+    echo "<center><br /><h3>All Campaigns</h3>";
+    
+    $totals = array();
+    $billables = array();
+    $group_0_to_29 = array();
+    $group_30_to_119 = array();
+    $group_120_to_299 = array();
+    $group_300_to_600 = array();
+    $group_600_to_900 = array();
+    $group_900_plus = array();
+    
+    if (isset($_GET['span'])) {
+        $result = mysql_query("SELECT userfield, billsec FROM cdr WHERE amaflags = '-1' AND calldate >= DATE_SUB(CURDATE(), INTERVAL ".sanitize($_GET['span'])." DAY)") or die(mysql_error());
+    } else {
+        $result = mysql_query("SELECT userfield, billsec FROM cdr WHERE amaflags = '-1'");
+    }
+    
+    if (mysql_num_rows($result) > 0) {
+        while ($row = mysqL_fetch_assoc($result)) {
+            $userfield = split("-",$row['userfield']);
+            $campaign_id = $userfield[1];
+            //echo $campaign_id."<br />";;
+            //echo "CallDate: ".$row['calldate']." Length: ".$row['billsec']."<br />";
+            $totals[$campaign_id][] = $row['billsec'];
+            if ($row['billsec'] < 30) {
+                $group_0_to_29[$campaign_id][] = $row['billsec'];
+            } else if ($row['billsec'] < 120) {
+                $group_30_to_119[$campaign_id][] = $row['billsec'];
+                $billables[$campaign_id][] = $row['billsec'];
+            } else if ($row['billsec'] < 300) {
+                $group_120_to_299[$campaign_id][] = $row['billsec'];
+                $billables[$campaign_id][] = $row['billsec'];
+            } else if ($row['billsec'] < 600) {
+                $group_300_to_600[$campaign_id][] = $row['billsec'];
+                $billables[$campaign_id][] = $row['billsec'];
+            } else if ($row['billsec'] < 900) {
+                $group_600_to_900[$campaign_id][] = $row['billsec'];
+                $billables[$campaign_id][] = $row['billsec'];
+            } else {
+                $group_900_plus[$campaign_id][] = $row['billsec'];
+                $billables[$campaign_id][] = $row['billsec'];
+            }
+        }
+    }
+    //print_pre($totals);
+    ?>
+    <table class="transfer_history">
+    <tr>
+    <th class="transfer_history">Campaign</th><th class="transfer_history">Total Xfers</th><th class="transfer_history">Billable Xfers</th><th class="transfer_history">0-29 secs</th><th class="transfer_history">30-119 secs</th><th class="transfer_history">2-5 mins</th><th class="transfer_history">5-10 mins</th><th class="transfer_history">10-15 mins</th><th class="transfer_history">15+ mins</th></th>
+    </tr>
+    <?
+    foreach ($totals as $name=>$entry) {
+        $campaign_name = mysql_result(mysql_query("SELECT name FROM campaign WHERE id = ".sanitize($name)),0,0);
+        echo "<tr>";
+        echo "<td class=\"transfer_history\">$campaign_name</td>";
+        echo "<td class=\"transfer_history\">".count($entry)."</td>";
+        echo "<td class=\"transfer_history\">".count($billables[$name])."</td>";
+        echo "<td class=\"transfer_history\">".count($group_0_to_29[$name])."</td>";
+        echo "<td class=\"transfer_history\">".count($group_30_to_119[$name])."</td>";
+        echo "<td class=\"transfer_history\">".count($group_120_to_299[$name])."</td>";
+        echo "<td class=\"transfer_history\">".count($group_300_to_600[$name])."</td>";
+        echo "<td class=\"transfer_history\">".count($group_600_to_900[$name])."</td>";
+        echo "<td class=\"transfer_history\">".count($group_900_plus[$name])."</td>";
+        echo "</tr>";
+        //print_pre($entry);
     }
     echo "</table>";
+    
+    ?>
+    <div id="live_calls">
+    </div>
+    <script>
+    jQuery('#live_calls').load('transfer_report.php?live_calls=1');
+    setInterval(function(){ jQuery('#live_calls').load('transfer_report.php?live_calls=1'); }, 5000);
+    
+    </script>
+    <?
     
     require "footer.php";
     exit(0);
