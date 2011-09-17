01bf89a5768c610388f0a0c522af923ddf5a6111
matt <MattRiddell@users.noreply.github.com>
2011-09-17 11:41:41 +0000

    Enough to create an entry

--- ddis.php
+++ ddis.php
@@ -2,20 +2,22 @@
 /* DDI Management
  * ==============
  * 
- * Works in conjunction with the SineDialer.extensions table to provide 
+ * Works in conjunction with the SineDialer.ddis table to provide 
  * realtime Asterisk extensions for setting up DDI numbers.
  *
  * A DDI contains three components:
  * 1. A DDI Number
  * 2. A Message to play when it is answered
  * 3. A Campaign it is associated with
- * 
- * Rather than create a separate table for DDI info and extensions we will
- * just parse the extensions_table for the info we're looking for.
  */
 
 require "header.php";
 require "header_surveys.php";
+if (isset($_GET['save_new'])) {
+    $sql = "INSERT INTO dids (number, message_id, campaign_id) VALUES (".sanitize($_POST['ddi_number']).",".sanitize($_POST['message_id']).",".sanitize($_POST['campaign_id']).")";
+    require "footer.php";
+    exit(0);
+}
 if (isset($_GET['add'])) {
     box_start(400);
     ?>
@@ -41,7 +43,7 @@ if (isset($_GET['add'])) {
     require "footer.php";
     exit(0);
 }
-$result = mysql_query("SELECT * FROM extensions_table");
+$result = mysql_query("SELECT * FROM dids");
 if (mysql_num_rows($result) > 0) {
     while ($row = mysql_fetch_assoc($result)) {
         print_pre($row);
