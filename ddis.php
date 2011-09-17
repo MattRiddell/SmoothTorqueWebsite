f673a1beaf6d48ae9fa74c1108a5fb208b5adae4
matt <MattRiddell@users.noreply.github.com>
2011-09-17 10:55:29 +0000

    Start adding DDI code

---
+++ ddis.php
@@ -0,0 +1,12 @@
+<?
+/* DDI Management
+ * ==============
+ * 
+ * Works in conjunction with the SineDialer.extensions table to provide 
+ * realtime Asterisk extensions for setting up DDI numbers.
+ */
+
+require "header.php";
+require "header_surveys.php";
+require "footer.php";
+?>
--- header_surveys.php
+++ header_surveys.php
@@ -1,8 +1,9 @@
-<?box_start(520);?>
+<?box_start(580);?>
 <table align="center" border="0" width="100%" cellpadding="0" cellspacing="3"><tr>
 <td><a href="surveys.php"><img src="images/table.png" border="0" align="left">View Surveys</a>&nbsp;&nbsp;</td>
 <td><a href="surveys.php?add=1"><img src="images/table_add.png" border="0" align="left">Add Survey</a>&nbsp;&nbsp;</td>
 <td><a href="survey_responses.php"><img src="images/user_comment.png" border="0" align="left">Survey Responses</a>&nbsp;&nbsp;</td>
+<td><a href="ddis.php"><img src="images/phone.png" border="0" align="left">DDIs</a>&nbsp;&nbsp;</td>
 <td><a href="transfer_report.php"><img src="images/user_go.png" border="0" align="left">Transfer Reports</a>&nbsp;&nbsp;</td>
 </tr></table><?box_end();?>
 
