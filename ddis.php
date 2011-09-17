54e608eb4795ce67830bc3c36fd388d4eca5fc9c
matt <MattRiddell@users.noreply.github.com>
2011-09-17 11:06:21 +0000

    Start fleshing out the logic

--- ddis.php
+++ ddis.php
@@ -4,9 +4,23 @@
  * 
  * Works in conjunction with the SineDialer.extensions table to provide 
  * realtime Asterisk extensions for setting up DDI numbers.
+ *
+ * A DDI contains three components:
+ * 1. A DDI Number
+ * 2. A Message to play when it is answered
+ * 3. A Campaign it is associated with
+ * 
+ * Rather than create a separate table for DDI info and extensions we will
+ * just parse the extensions_table for the info we're looking for.
  */
 
 require "header.php";
 require "header_surveys.php";
+$result = mysql_query("SELECT * FROM extensions_table");
+if (mysql_num_rows($result) > 0) {
+    while ($row = mysql_fetch_assoc($result)) {
+        print_pre($row);
+    }
+}
 require "footer.php";
 ?>
--- header_surveys.php
+++ header_surveys.php
@@ -1,9 +1,10 @@
-<?box_start(580);?>
+<?box_start(780);?>
 <table align="center" border="0" width="100%" cellpadding="0" cellspacing="3"><tr>
 <td><a href="surveys.php"><img src="images/table.png" border="0" align="left">View Surveys</a>&nbsp;&nbsp;</td>
 <td><a href="surveys.php?add=1"><img src="images/table_add.png" border="0" align="left">Add Survey</a>&nbsp;&nbsp;</td>
 <td><a href="survey_responses.php"><img src="images/user_comment.png" border="0" align="left">Survey Responses</a>&nbsp;&nbsp;</td>
 <td><a href="ddis.php"><img src="images/phone.png" border="0" align="left">DDIs</a>&nbsp;&nbsp;</td>
+<td><a href="ddis.php?add=1"><img src="images/phone_add.png" border="0" align="left">Add a DDI</a>&nbsp;&nbsp;</td>
 <td><a href="transfer_report.php"><img src="images/user_go.png" border="0" align="left">Transfer Reports</a>&nbsp;&nbsp;</td>
 </tr></table><?box_end();?>
 
