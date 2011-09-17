0934dbb5595b869045ee95c092cf242e58add162
matt <MattRiddell@users.noreply.github.com>
2011-09-17 11:12:22 +0000

    Add logic to add DDI

--- ddis.php
+++ ddis.php
@@ -16,6 +16,31 @@
 
 require "header.php";
 require "header_surveys.php";
+if (isset($_GET['add'])) {
+    box_start(400);
+    ?>
+    <center>
+    <form action="ddis.php?save_new=1" method="post">
+    DDI Number: <input type="text" name="ddi_number"><br />
+    Message To Play on Answer: <select name="message_id"><?
+    $result = mysql_query("SELECT * FROM campaignmessage where filename like 'x-%'");
+    while ($row = mysql_fetch_assoc($result)) {
+        echo '<option value="'.$row['id'].'">'.substr($row['name'],0,20).'</option>';
+    }
+    ?></select><br />
+    Campaign To Associate With:<select name="campaign_id"><?
+    $result = mysql_query("SELECT * FROM campaign");
+    while ($row = mysql_fetch_assoc($result)) {
+        echo '<option value="'.$row['id'].'">'.substr($row['name'],0,20).'</option>';
+    }    
+    ?></select><br />
+    <input type="submit" value="Add DDI">
+    </form>
+    <?
+    box_end();
+    require "footer.php";
+    exit(0);
+}
 $result = mysql_query("SELECT * FROM extensions_table");
 if (mysql_num_rows($result) > 0) {
     while ($row = mysql_fetch_assoc($result)) {
