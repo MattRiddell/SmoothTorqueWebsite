4e9f51ac4927e389eeb03b1194d8b2c79d11356c
matt <MattRiddell@users.noreply.github.com>
2011-09-19 02:22:49 +0000

    Clean up a couple of errors based on lack of apostrophes

--- mysql_details.php
+++ mysql_details.php
@@ -1,22 +1,22 @@
 <?
 $level=$_COOKIE[level];
 /* Find out what the base directory name is for two reasons:
-    1. So we can include files
-    2. So we can explain how to set up things that are missing */
+ 1. So we can include files
+ 2. So we can explain how to set up things that are missing */
 $current_directory = dirname(__FILE__);
 
 /* What page we are currently on - this is used to highlight the menu
-   system as well as to not cache certain pages like the graphs */
+ system as well as to not cache certain pages like the graphs */
 $self=$_SERVER['PHP_SELF'];
 
 /* Load in the functions we may need - these are the list of available
-   custom functions - for more information, read the comments in the
-   functions.php file - most functions are in their own file in the
-   functions subdirectory */
+ custom functions - for more information, read the comments in the
+ functions.php file - most functions are in their own file in the
+ functions subdirectory */
 if ($_GET['ajax'] == 1) {
 	header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 	header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
-        /* Functions will not have been included if there is no header*/
+    /* Functions will not have been included if there is no header*/
 	$url = "default";
 	require "/".$current_directory."/functions/functions.php";
 }
@@ -28,23 +28,23 @@ mysql_select_db("SineDialer", $link) or die("Unable to connect: ".mysql_error())
 echo "<b>Database Statistics:<br /></b>";
 $result = mysql_query("SHOW STATUS");
 while ($row = mysql_fetch_assoc($result)){
-        if ($row[Variable_name] == 'Slow_queries') {
-                $slow_queries = $row['Value'];
-        }
-        if ($row[Variable_name] == 'Questions') {
-                $questions = $row['Value'];
-        }
-        if ($row[Variable_name] == 'Uptime') {
-                $uptime = sec2hms($row['Value']);
+    if ($row['Variable_name'] == 'Slow_queries') {
+        $slow_queries = $row['Value'];
+    }
+    if ($row['Variable_name'] == 'Questions') {
+        $questions = $row['Value'];
+    }
+    if ($row['Variable_name'] == 'Uptime') {
+        $uptime = sec2hms($row['Value']);
 		$uptime_s = $row['Value'];
-        }
-        if ($row[Variable_name] == 'Connections') {
-                $connections = number_format($row['Value']);
-        }
+    }
+    if ($row[Variable_name] == 'Connections') {
+        $connections = number_format($row['Value']);
+    }
 }
 $result = mysql_query("SELECT value FROM SineDialer.config WHERE parameter = 'mysql_queue'") or die(mysql_error());
 if (mysql_num_rows($result) > 0 && mysql_result($result,0,0) > 0) {
-        $pending = "<font color=\"red\"><b>Queued Writes: ".mysql_result($result,0,0)."</b></font> ";
+    $pending = "<font color=\"red\"><b>Queued Writes: ".mysql_result($result,0,0)."</b></font> ";
 }
 $result = mysql_query("select count(*) from SineDialer.number where random_sort = 0 or random_sort = NULL") or die(mysql_error());
 if (mysql_result($result,0,0)>0) {
@@ -60,22 +60,22 @@ $output = "";
 if (mysql_num_rows($result) > 0) {
     $count = 0;
     while ($row = mysql_fetch_assoc($result)) {
-    	if ($row[Command] != 'Sleep' && $row[Time] > 2) {
+    	if ($row['Command'] != 'Sleep' && $row['Time'] > 2) {
             $count++;
     		$output.= "<tr>";
     		$link = '<td><a href="mysql_stats.php?kill='.$row['Id'].'"><img src="images/delete.png" border="0">&nbsp;Kill this process</a></td>';
-    		if ($row[State] == 'Locked') {
-    			$output.= "<td><p align=\"left\" style=\"color:#ff0000\"><b>Locked:</b></font> $row[Info]<br /><b>Time:</b> ".sec2hms($row[Time])." <b>Host:</b> ".$row[User]."@".$row[Host]."$link</td>";
-    		} else if ($row[State] == 'copy to tmp table') {
-    			$output.= "<td><p align=\"left\" style=\"color:#880088\"><b>Copying to temp table:</b></font> $row[Info]<br /><b>Time:</b> ".sec2hms($row[Time])." <b>Host:</b> ".$row[User]."@".$row[Host]."$link</td>";
-    		} else if ($row[State] == 'updating' ||$row[State] == 'Updating' ||$row[State] == 'update') {
-    			$output.= "<td><p align=\"left\" style=\"color:#000000\"><b>Updating:</b> $row[Info]<br /><b>Time:</b> ".sec2hms($row[Time])." <b>Host:</b> ".$row[User]."@".$row[Host]."$link</td>";
-    		} else if ($row[State] == 'Sorting result') {
-    			$output.= "<td><p align=\"left\" style=\"color:#000000\"><b>Sorting result:</b> $row[Info]<br /> <b>Time:</b> ".sec2hms($row[Time])." <b>Host:</b> ".$row[User]."@".$row[Host]."$link</td>";
-    		} else if ($row[State] == 'Sending data') {
-    			$output .= "<td><p align=\"left\" style=\"color:#008800\"><b>Sending data:</b> $row[Info]<br /> <b>Time:</b> ".sec2hms($row[Time])." <b>Host:</b> ".$row[User]."@".$row[Host]."$link</td>";
+    		if ($row['State'] == 'Locked') {
+    			$output.= "<td><p align=\"left\" style=\"color:#ff0000\"><b>Locked:</b></font> $row[Info]<br /><b>Time:</b> ".sec2hms($row['Time'])." <b>Host:</b> ".$row['User']."@".$row['Host']."$link</td>";
+    		} else if ($row['State'] == 'copy to tmp table') {
+    			$output.= "<td><p align=\"left\" style=\"color:#880088\"><b>Copying to temp table:</b></font> $row[Info]<br /><b>Time:</b> ".sec2hms($row['Time'])." <b>Host:</b> ".$row['User']."@".$row['Host']."$link</td>";
+    		} else if ($row['State'] == 'updating' ||$row['State'] == 'Updating' ||$row['State'] == 'update') {
+    			$output.= "<td><p align=\"left\" style=\"color:#000000\"><b>Updating:</b> $row[Info]<br /><b>Time:</b> ".sec2hms($row['Time'])." <b>Host:</b> ".$row['User']."@".$row['Host']."$link</td>";
+    		} else if ($row['State'] == 'Sorting result') {
+    			$output.= "<td><p align=\"left\" style=\"color:#000000\"><b>Sorting result:</b> $row[Info]<br /> <b>Time:</b> ".sec2hms($row['Time'])." <b>Host:</b> ".$row['User']."@".$row['Host']."$link</td>";
+    		} else if ($row['State'] == 'Sending data') {
+    			$output .= "<td><p align=\"left\" style=\"color:#008800\"><b>Sending data:</b> $row[Info]<br /> <b>Time:</b> ".sec2hms($row['Time'])." <b>Host:</b> ".$row['User']."@".$row['Host']."$link</td>";
     		} else {
-    			if ($row[Info] != "SHOW PROCESSLIST") {
+    			if ($row['Info'] != "SHOW PROCESSLIST") {
     				$output.= "<td><p align=\"left\">";
     				foreach ($row as $key=>$value) {
     					$output.= "<b>Key:</b> $key <b>Value:</b> $value<br />";
@@ -88,7 +88,7 @@ if (mysql_num_rows($result) > 0) {
     }
     if ($count > 0) {
         echo "<br /><b>Long Running MySQL Threads:</b><br />";
-	echo "<br /><center><table border=\"0\" class=\"tborder\">";
+        echo "<br /><center><table border=\"0\" class=\"tborder\">";
         echo $output;
         echo "</table>";
     }
