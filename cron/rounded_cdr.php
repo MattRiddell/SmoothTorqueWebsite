7c62ecbf3dc7aef8e509ee4a24a8d9dc77153ab8
matt <MattRiddell@users.noreply.github.com>
2011-09-21 05:14:20 +0000

    Ok wow that's fast

--- cron/rounded_cdr.php
+++ cron/rounded_cdr.php
@@ -4,5 +4,5 @@ $db_host = "localhost";
 $db_user = "root";
 $db_pass = "";
 mysql_connect($db_host, $db_user, $db_pass);
-$result = mysql_query("UPDATE SineDialer.cdr set rounded_billsec = (CEILING(billsec/6)*6) WHERE rounded_billsec is NULL limit 20000");
+$result = mysql_query("UPDATE SineDialer.cdr set rounded_billsec = (CEILING(billsec/6)*6) WHERE rounded_billsec is NULL");
 ?>
