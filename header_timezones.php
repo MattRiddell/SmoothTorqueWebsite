285cea8a774ccc573b0cc98b15c089a443876d89
matt <MattRiddell@users.noreply.github.com>
2011-09-21 06:45:27 +0000

    Add run timezone script

--- header_timezones.php
+++ header_timezones.php
@@ -4,6 +4,7 @@
 <?/*<TD class="subheader"><A HREF="timezones.php?view_prefixes=1"><img src="images/telephone.png" border="0" align="left">Timezone Prefixes</A>&nbsp;&nbsp;</TD>*/?>
 <TD class="subheader"><A HREF="timezones.php?add_prefixes=1"><img src="images/telephone_add.png" border="0" align="left">Add Single Timezone Prefix</A>&nbsp;&nbsp;</TD>
 <TD class="subheader"><A HREF="upload_prefixes.php"><img src="images/telephone_add.png" border="0" align="left">Upload Timezone Prefixes</A>&nbsp;&nbsp;</TD>
+<TD class="subheader"><A HREF="cron/cron_timezone.php"><img src="images/recycle.png" border="0" align="left">Run Timezone Script</A>&nbsp;&nbsp;</TD>
 </TR></table><?box_end();?>
 
 <BR>
