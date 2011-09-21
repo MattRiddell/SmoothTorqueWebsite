04b4fa699f7e70c2f2bb412953384d66c5b90c4b
matt <MattRiddell@users.noreply.github.com>
2011-09-21 06:47:18 +0000

    Open in new window

--- header_timezones.php
+++ header_timezones.php
@@ -1,10 +1,10 @@
-<?box_start(670);?><table align="center" width="100%" border="0" cellpadding="0" cellspacing="3"><TR>
+<?box_start(770);?><table align="center" width="100%" border="0" cellpadding="0" cellspacing="3"><TR>
 <TD class="subheader"><A HREF="timezones.php?add=1"><img src="images/world_add.png" border="0" align="left">Add Timezone</A>&nbsp;&nbsp;</TD>
 <TD class="subheader"><A HREF="timezones.php?view_timezones=1"><img src="images/world.png" border="0" align="left">Timezones</A>&nbsp;&nbsp;</TD>
 <?/*<TD class="subheader"><A HREF="timezones.php?view_prefixes=1"><img src="images/telephone.png" border="0" align="left">Timezone Prefixes</A>&nbsp;&nbsp;</TD>*/?>
 <TD class="subheader"><A HREF="timezones.php?add_prefixes=1"><img src="images/telephone_add.png" border="0" align="left">Add Single Timezone Prefix</A>&nbsp;&nbsp;</TD>
 <TD class="subheader"><A HREF="upload_prefixes.php"><img src="images/telephone_add.png" border="0" align="left">Upload Timezone Prefixes</A>&nbsp;&nbsp;</TD>
-<TD class="subheader"><A HREF="cron/cron_timezone.php"><img src="images/recycle.png" border="0" align="left">Run Timezone Script</A>&nbsp;&nbsp;</TD>
+<TD class="subheader"><A HREF="cron/cron_timezone.php" target="_blank"><img src="images/arrow_refresh.png" border="0" align="left">Run Timezone Script</A>&nbsp;&nbsp;</TD>
 </TR></table><?box_end();?>
 
 <BR>
