11956ba25d7694dd344e896aff900962ddd4205a
matt <MattRiddell@users.noreply.github.com>
2011-09-25 06:24:11 +0000

    A bit easier to see and reset the total

--- historic_cps.php
+++ historic_cps.php
@@ -6,15 +6,16 @@ while (1) {
     $sql = "select campaign.id, campaign.name, campaign_stats.* from campaign, campaign_stats, queue where campaign_stats.campaignid=queue.campaignID and queue.status = 101 and campaign_stats.campaignid = campaign.id";
     $result = mysql_query($sql) or die(mysql_error());
     if (mysql_num_rows($result) > 0) {
+        $total['cps'] = 0;
         while ($row = mysqL_fetch_assoc($result)) {
             echo $row['name']."=".(1000/$row['ms_sleep'])."\n";
             $total['cps']+=round(1000/$row['ms_sleep'],2);
             //$result_new2 = mysql_query("INSERT INTO historic_cps (campaign_id, cps) VALUES (".sanitize($row['id']).",".sanitize((1000/$row['ms_sleep'])).")");
         }
-        echo "Total: ".$total['cps']."\n";
+        echo " [ Total: ".$total['cps']." ]\n";
         $result_new = mysql_query("INSERT INTO historic_cps (campaign_id, cps) VALUES (0,".sanitize($total['cps']).")");
     } else {
-        echo "No campaigns running\n";
+        echo "-- No campaigns running --\n";
     }
     sleep(10);
 }
