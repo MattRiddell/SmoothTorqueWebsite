f8cc59b5d92c550c175d1dd3cb989d721ecfbb45
matt <MattRiddell@users.noreply.github.com>
2011-09-25 06:21:43 +0000

    Only really need total

--- historic_cps.php
+++ historic_cps.php
@@ -3,16 +3,18 @@
 require "admin/db_config.php";
 require "functions/sanitize.php";
 while (1) {
-    $sql = "select campaign.id, campaign_stats.* from campaign, campaign_stats, queue where campaign_stats.campaignid=queue.campaignID and queue.status = 101 and campaign_stats.campaignid = campaign.id";
+    $sql = "select campaign.id, campaign.name, campaign_stats.* from campaign, campaign_stats, queue where campaign_stats.campaignid=queue.campaignID and queue.status = 101 and campaign_stats.campaignid = campaign.id";
     $result = mysql_query($sql) or die(mysql_error());
     if (mysql_num_rows($result) > 0) {
         while ($row = mysqL_fetch_assoc($result)) {
-            echo $row['id']."=".(1000/$row['ms_sleep'])."\n";
+            echo $row['name']."=".(1000/$row['ms_sleep'])."\n";
             $total['cps']+=round(1000/$row['ms_sleep'],2);
-            $result_new2 = mysql_query("INSERT INTO historic_cps (campaign_id, cps) VALUES (".sanitize($row['id']).",".sanitize((1000/$row['ms_sleep'])).")");
+            //$result_new2 = mysql_query("INSERT INTO historic_cps (campaign_id, cps) VALUES (".sanitize($row['id']).",".sanitize((1000/$row['ms_sleep'])).")");
         }
         echo "Total: ".$total['cps']."\n";
         $result_new = mysql_query("INSERT INTO historic_cps (campaign_id, cps) VALUES (0,".sanitize($total['cps']).")");
+    } else {
+        echo "No campaigns running\n";
     }
     sleep(10);
 }
