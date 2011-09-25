971b8909d8619fa91dd1b3deab6683410b257b43
matt <MattRiddell@users.noreply.github.com>
2011-09-25 06:18:41 +0000

    Die if there is an error

--- historic_cps.php
+++ historic_cps.php
@@ -4,7 +4,7 @@ require "admin/db_config.php";
 require "functions/sanitize.php";
 while (1) {
     $sql = "select campaign.id, campaign_stats.* from campaign, campaign_stats, queue where campaign_stats.campaignid=queue.campaignID and queue.status = 101 and campaign_stats.campaignid = campaign.id";
-    $result = mysql_query($sql);
+    $result = mysql_query($sql) or die(mysql_error());
     if (mysql_num_rows($result) > 0) {
         while ($row = mysqL_fetch_assoc($result)) {
             echo $row['id']."=".(1000/$row['ms_sleep'])."\n";
