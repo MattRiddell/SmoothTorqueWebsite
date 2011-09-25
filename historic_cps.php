ac47cd2f9c2bd7ade7bb6f72a55582b2e7722080
matt <MattRiddell@users.noreply.github.com>
2011-09-25 06:19:43 +0000

    Same again

--- historic_cps.php
+++ historic_cps.php
@@ -9,7 +9,7 @@ while (1) {
         while ($row = mysqL_fetch_assoc($result)) {
             echo $row['id']."=".(1000/$row['ms_sleep'])."\n";
             $total['cps']+=round(1000/$row['ms_sleep'],2);
-            $result = mysql_query("INSERT INTO historic_cps (camapign_id, cps) VALUES (".sanitize($row['id']).",".sanitize((1000/$row['ms_sleep'])).")");
+            $result_new2 = mysql_query("INSERT INTO historic_cps (camapign_id, cps) VALUES (".sanitize($row['id']).",".sanitize((1000/$row['ms_sleep'])).")");
         }
         echo "Total: ".$total['cps']."\n";
         $result_new = mysql_query("INSERT INTO historic_cps (camapign_id, cps) VALUES (0,".sanitize($total['cps']).")");
