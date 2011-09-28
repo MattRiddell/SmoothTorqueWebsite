39bc5de2370c322d497020001b3cabcf7dba4107
matt <MattRiddell@users.noreply.github.com>
2011-09-28 22:47:31 +0000

    Not a cron job

--- update_numbers.php
+++ update_numbers.php
@@ -1,33 +1,12 @@
 #!/usr/bin/php
 <?
-/* Cron Job for changing the status of numbers between "new" and "new_nodial" 
- * 
- * This is used for timezone management.  It is designed to be run every 30
- * minutes and swap the statuses of numbers.  Code by Matt Riddell - development
- * funded by VentureVoIP Canada and TSOA International.
- */
-
 function print_pre($text) {
     echo "<pre>";
     print_r($text);
     echo "</pre>";
 }
 
-/* Change this to your time zone */
-date_default_timezone_set("EST");
-
-$current_time = date("H:i:s");
-
-/* MySQL Connection details */
-$db_host="127.0.0.1";
-$db_user="root";
-$db_pass="";
-
-/* Connect to MySQL */
-$link = mysql_connect($db_host, $db_user, $db_pass) OR die(mysql_error());
-
-/* Select the appropriate database */
-mysql_select_db("SineDialer");
+require "header.php";
 
 /* Get all of the timezone prefixes and times */
 $result = mysql_query("select time_zones.start, time_zones.end, prefix from time_zones, timezone_prefixes where timezone_prefixes.timezone = time_zones.id");
