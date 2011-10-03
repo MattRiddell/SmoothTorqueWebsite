67b36b0931ea0debce0675b9e3f034df35e3c0ab
matt <MattRiddell@users.noreply.github.com>
2011-10-03 17:53:52 +0000

    Change script name

--- header_timezones.php
+++ header_timezones.php
@@ -4,7 +4,7 @@
 <?/*<TD class="subheader"><A HREF="timezones.php?view_prefixes=1"><img src="images/telephone.png" border="0" align="left">Timezone Prefixes</A>&nbsp;&nbsp;</TD>*/?>
 <TD class="subheader"><A HREF="timezones.php?add_prefixes=1"><img src="images/telephone_add.png" border="0" align="left">Add Single Timezone Prefix</A>&nbsp;&nbsp;</TD>
 <TD class="subheader"><A HREF="upload_prefixes.php"><img src="images/telephone_add.png" border="0" align="left">Upload Timezone Prefixes</A>&nbsp;&nbsp;</TD>
-<TD class="subheader"><A HREF="cron/cron_timezone.php" target="_blank"><img src="images/arrow_refresh.png" border="0" align="left">Run Timezone Script</A>&nbsp;&nbsp;</TD>
+<TD class="subheader"><A HREF="update_numbers.php" target="_blank"><img src="images/arrow_refresh.png" border="0" align="left">Run Timezone Script</A>&nbsp;&nbsp;</TD>
 </TR></table><?box_end();?>
 
 <BR>
