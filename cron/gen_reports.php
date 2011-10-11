9fac28cf679f6d093ff9b4a957fc1e01d9f48bd0
matt <MattRiddell@users.noreply.github.com>
2011-10-11 00:23:24 +0000

    add sanitize function

--- cron/gen_reports.php
+++ cron/gen_reports.php
@@ -8,6 +8,32 @@ mysql_connect($db_host, $db_user, $db_pass);
 /* Select the appropriate database */
 mysql_select_db("SineDialer");
 
+/* Sanitize function */
+if (!function_exists('sanitize') ) {
+    function sanitize($var, $quotes = true) {
+        if (is_array($var)) {   //run each array item through this function (by reference)
+            foreach ($var as &$val) {
+                $val = $this->sanitize($val);
+            }
+        }
+        else if (is_string($var)) { //clean strings
+            $var = mysql_real_escape_string($var);
+            if ($quotes) {
+                $var = "'". $var ."'";
+            }
+        }
+        else if (is_null($var)) {   //convert null variables to SQL NULL
+            $var = "NULL";
+        }
+        else if (is_bool($var)) {   //convert boolean variables to binary boolean
+            $var = ($var) ? 1 : 0;
+        }
+        return $var;
+    }
+}
+
+$_GET['span'] = 0;
+
 if (isset($_GET['span'])) {
     $result = mysql_query("SELECT userfield, billsec FROM cdr WHERE amaflags = '-1' AND calldate >= DATE_SUB(CURDATE(), INTERVAL ".sanitize($_GET['span'])." DAY)") or die(mysql_error());
     
