6bd59991f93640fc5acafcb621b074f2c1a49e31
matt <MattRiddell@users.noreply.github.com>
2011-11-02 22:12:01 +0000

    Don't print error

--- survey_responses.php
+++ survey_responses.php
@@ -30,7 +30,12 @@ if (mysql_num_rows($result) == 0) {
 } else {
     while ($row = mysql_fetch_assoc($result)) {
         $campaign_names = mysql_query("SELECT name FROM campaign WHERE id = ".$row['campaign_id']);
-        echo '<a href="survey_responses.php?campaign_id='.$row['campaign_id'].'">'.mysql_result($campaign_names,0,0)." Count: ".$row['count']."</a><br />";
+        if (mysql_num_rows($result) == 0) {
+            $name = "Unknown (deleted)";
+        } else {
+            $name = mysql_result($campaign_names,0,0);
+        }
+        echo '<a href="survey_responses.php?campaign_id='.$row['campaign_id'].'">'.$name." Count: ".$row['count']."</a><br />";
     }
 }
 box_end();
