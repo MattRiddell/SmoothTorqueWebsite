868bf657404b0d46e7c1227cde4ab13bc2aaffb8
matt <MattRiddell@users.noreply.github.com>
2011-11-18 00:04:23 +0000

    Add the first part of the cron job

---
+++ cron/run_notifications.php
@@ -0,0 +1,38 @@
+#!/usr/bin/php
+<?
+/* This cron job is used to check what percentage of the numbers are remaining 
+ and send out appropriate notifications if the remaining percentage falls below 
+ the threshold for a particular notification */
+
+/* This cron job assumes that you are using timezone based dialling */
+
+$query_start = time();
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
+mysql_connect($db_host, $db_user, $db_pass);
+
+/* Select the appropriate database */
+mysql_select_db("SineDialer");
+
+/* First go through all the campaigns and check if there are any changes.  If 
+ there are changes make a note of them */
+
+$result1 = mysql_query("SELECT * FROM campaign");
+$count_array = array();
+if (mysql_num_rows($result1) > 0) {
+    while ($row = mysql_fetch_assoc($result1)) {
+        $sql = 'Select count(*) from number where status="new" and campaignid = '.$row['id']." and TIME(NOW()) between start_time and end_time";
+        $result2=mysql_query($sql, $link) or die (mysql_error());;
+        $count_array[$row['id']] == mysql_result($result2,0,'count(*)');
+    }
+}
+
+print_r($count_array);
+
+/*
+
+$result = mysql_query("SELECT campaign_id FROM survey_schedules WHERE leads_required > 0 AND start_hour = ".date("H"));
+if (mysql_num_rows($result) > 0) {
+    while ($row = mysql_fetch_assoc($result)) {
+*/
