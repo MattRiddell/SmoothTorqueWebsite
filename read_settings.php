08aa70bc5ad46eec708668e6d23e90785ad5b762
matt <MattRiddell@users.noreply.github.com>
2011-12-29 00:32:37 +0000

    Small change

--- read_settings.php
+++ read_settings.php
@@ -2,11 +2,11 @@
 $docroot = $_SERVER["DOCUMENT_ROOT"];
 /* Settings file should be placed one step above document root.
  If you can't place it there, change the row below to point to the actual location of the settings file */
-$settingsfile = $docroot."/../upload_settings.inc";
+$settingsfile = "../upload_settings.inc";
 
 if(file_exists($settingsfile)) {
   eval(file_get_contents($settingsfile));
 } else {
-  die("Could not find settings file. Please edit read_settings.php.");
+  die("Could not find settings file at $settingsfile. Please edit read_settings.php.");
 }
 ?>
