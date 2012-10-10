7dcd6bbad5e34e8f433c129bb0094ed1964661c1
matt <MattRiddell@users.noreply.github.com>
2012-10-10 19:47:37 +0000

    Changes

--- get_records.php
+++ get_records.php
@@ -7,13 +7,13 @@ $url = "http://someserver/getleads/leads.aspx?apikey=x&qty=";
 require "admin/db_config.php";
 $result = mysql_query("SELECT count(*) from number where status = 'new'");
 $count = mysql_result($result,0,0);
-echo $count;
+//echo $count;
 if ($count <= $min_new_records) {
-    echo "Not enough records";
-    // Copy existing data to backup table
-    // Delete existing data
-    // Import new data
-    // Scrub against DNC
-    // Run TimeZone script
+    echo "Not enough records ($count <= $min_new_records)\n";
+    echo "Copy existing data to backup table\n";
+    echo "Delete existing data\n";
+    echo "Import new data\n";
+    echo "Scrub against DNC\n";
+    echo "Run TimeZone script\n";
 }
 ?>
