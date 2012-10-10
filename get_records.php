c00edb4dbb26a8e954a80b370ffffd07b9da56de
matt <MattRiddell@users.noreply.github.com>
2012-10-10 19:48:47 +0000

    Show errors

--- get_records.php
+++ get_records.php
@@ -5,7 +5,7 @@ $min_new_records = 1000000;
 $campaignid = 63;
 $url = "http://someserver/getleads/leads.aspx?apikey=x&qty=";
 require "admin/db_config.php";
-$result = mysql_query("SELECT count(*) from number where status = 'new'");
+$result = mysql_query("SELECT count(*) from number where status = 'new'") or die(mysql_error());
 $count = mysql_result($result,0,0);
 //echo $count;
 if ($count <= $min_new_records) {
