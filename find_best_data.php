6b93835308eba5f6eac0dd33efb002cf323f8788
matt <MattRiddell@users.noreply.github.com>
2013-01-28 21:16:43 +0000

    Break it out

--- find_best_data.php
+++ find_best_data.php
@@ -62,14 +62,54 @@ foreach ($records as $number=>$name) {
         $full_name_reverse = $exploded[$source['last_name_field']]." ".$exploded[$source['first_name_field']];
         $full_name_init = $exploded[$source['last_name_field']]." ".substr($exploded[$source['first_name_field']],0,1);
         $full_name_init_reverse = substr($exploded[$source['first_name_field']],0,1)." ".$exploded[$source['last_name_field']];
-        similar_text($full_name, $name, $percentage_match_1);
-        similar_text($full_name_reverse, $name, $percentage_match_2);
-        similar_text($full_name_init, $name, $percentage_match_3);
-        similar_text($full_name_init_reverse, $name, $percentage_match_4);
-        echo $source['name'].": ".$full_name." Match 1: $name ".$percentage_match_1."\n";
+        similar_text($full_name, $name, $percentage_match[0]);
+        similar_text($full_name_reverse, $name, $percentage_match[1]);
+        similar_text($full_name_init, $name, $percentage_match[2]);
+        similar_text($full_name_init_reverse, $name, $percentage_match[3]);
+        
+        $highest = -1;
+        $highest_percentage = -1;
+        for ($i = 0;$i<4;$i++) {
+            if ($percentage_match > $highest_percentage) {
+                $highest = $i;
+                $highest_percentage = $percentage_match;
+            }
+        }
+        
+        /*echo $source['name'].": ".$full_name." Match 1: $name ".$percentage_match_1."\n";
         echo $source['name'].": ".$full_name_reverse." Match 2: $name ".$percentage_match_2."\n";
         echo $source['name'].": ".$full_name_init." Match 3: $name ".$percentage_match_3."\n";
-        echo $source['name'].": ".$full_name_init_reverse." Match 4: $name ".$percentage_match_4."\n";
+        echo $source['name'].": ".$full_name_init_reverse." Match 4: $name ".$percentage_match_4."\n";*/
+        switch ($highest) {
+            case 0:
+                $text = $full_name;
+                break;
+            case 1:
+                $text = $full_name_reverse;
+                break;
+            case 2:
+                $text = $full_name_init;
+                break;
+            case 3:
+                $text = $full_name_init_reverse;
+                break;
+            default:
+                break;
+        }
+        if ($highest > -1) {
+            if ($highest_percentage < 50) {
+                echo "No Match $highest_percentage $name with ".$text;
+            } else if ($highest_percentage < 75) {
+                echo "Spouse Match $highest_percentage $name with ".$text;
+            } else if ($highest_percentage < 100) {
+                echo "Almost exact Match $highest_percentage $name with ".$text;
+            } else {
+                echo "EXACT MATCH $name with ".$text;
+            }            
+        } else {
+            echo "No match";
+        }
+        
     }
 }
 
