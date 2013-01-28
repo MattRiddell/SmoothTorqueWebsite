d4ff5bbcdd3cc8f91694d79bcf14a6397c5b6478
matt <MattRiddell@users.noreply.github.com>
2013-01-28 18:55:35 +0000

    Add a script to lookup cnam records and populate the names table (requires a service that you are subscribed to)

---
+++ lookup_cnam.php
@@ -0,0 +1,42 @@
+<?
+$cnam_lookup = "http://x.x.x.x/api/query.php?mode=all&output=txt&dn=";
+$userpass = "user:pass";
+require "admin/db_config.php";
+require "functions/sanitize.php";
+mysql_select_db("ClusterControl");
+while (1) {
+    $result = mysql_query("select distinct survey_results.phonenumber from survey_results left join names on survey_results.phonenumber=names.phonenumber where survey_results.campaign_id = 64 and names.phonenumber is null");
+    while ($row = mysql_fetch_assoc($result)) {
+        $context = stream_context_create(array('http' => array('header'  => "Authorization: Basic " . base64_encode($userpass))));
+        $phonenumber = $row['phonenumber'];
+        $url = $cnam_lookup.$phonenumber;
+        $data = file_get_contents($url, false, $context);
+        echo "XXXXXXXXXXX: ".$phonenumber." returns ".$data."\n";
+        $skip = false;
+        if (strlen(trim($data)) > 0 && $data != "WIRELESS CALLER" && $data != "U.S. CELLULAR") {
+            if (substr($data,13,1) == " ") {
+                $test_string = substr($data,14,1);
+                if (preg_match('/[^A-Za-z]/',$test_string)) {
+                    $test_string = substr($data,15,1);
+                    if (preg_match('/[^A-Za-z]/',$test_string)) {
+                        echo "CITY: ".$data."\n";
+                        $skip = true;
+                    }
+                }
+            }
+        } else {
+            $skip = true;
+        }
+        if ($skip) {
+            $sql = "INSERT IGNORE INTO names (campaignid, phonenumber, name) VALUES ($campaign, '$phonenumber','XXX: ".$data."')";
+            echo $sql."\n";
+            mysql_query($sql);
+        } else {
+            $sql = "INSERT IGNORE INTO names (campaignid, phonenumber, name) VALUES ($campaign, '$phonenumber','".$data."')";
+            echo $sql."\n";
+            mysql_query($sql);
+        }
+        sleep(1);
+    }
+}
+?>
