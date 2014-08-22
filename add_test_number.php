04dda536d02434749449cf047213f23dc56c417f
matt <MattRiddell@users.noreply.github.com>
2014-08-22 15:23:28 +0000

    Heh

--- add_test_number.php
+++ add_test_number.php
@@ -1,6 +1,6 @@
 <?
 require "header.php";
-$sql = "REPLACE INTO SineDialer.number (campaignid, status, phonenumber, random_sort, start_time, end_time) VALUES (".sanitize($_GET['campaign']).",'new','".$config_values['test_number']."',1,'00:00:00','25:59:59')";
+$sql = "REPLACE INTO SineDialer.number (campaignid, status, phonenumber, random_sort, start_time, end_time) VALUES (".sanitize($_GET['id']).",'new','".$config_values['test_number']."',1,'00:00:00','25:59:59')";
 $result = mysql_query($sql) or die(mysql_error());
 redirect("campaigns.php?type=".$_GET['type'],"Inserted test number into campaign");
 require "footer.php";
