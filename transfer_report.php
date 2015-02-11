cf15556f20f14a05b48d52e4afa25da5b1e3db6e
matt <MattRiddell@users.noreply.github.com>
2015-02-11 18:03:32 +0000

    Bugfix

--- transfer_report.php
+++ transfer_report.php
@@ -271,7 +271,7 @@ if (isset($_GET['recordings_date'])) {
             audio[0].play();
             
             
-            jQuery("td").css("background-color","");
+            jQuery(".recordings").css("background-color","");
             //var foo = document.getElementById('player-<?=$x?>');
             //foo.play();
             <?
