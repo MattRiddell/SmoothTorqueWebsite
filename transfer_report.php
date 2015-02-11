e10e136f0c6077c0475a0ce78acb7e374cf74b1d
matt <MattRiddell@users.noreply.github.com>
2015-02-11 18:00:03 +0000

    Bugfix

--- transfer_report.php
+++ transfer_report.php
@@ -271,7 +271,7 @@ if (isset($_GET['recordings_date'])) {
             audio[0].play();
             
             
-            
+            jQuery("td").removeClass("recordings");
             //var foo = document.getElementById('player-<?=$x?>');
             //foo.play();
             <?
