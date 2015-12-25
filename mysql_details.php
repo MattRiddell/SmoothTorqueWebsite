56c0ccb75958c4c007ae7b7f62dedfb266b53e3d
matt <MattRiddell@users.noreply.github.com>
2015-12-25 15:52:58 +0000

    Clean up the database info pages

--- mysql_details.php
+++ mysql_details.php
@@ -1,5 +1,5 @@
 <?
-$level=$_COOKIE[level];
+$level = $_COOKIE[level];
 /* Find out what the base directory name is for two reasons:
  1. So we can include files
  2. So we can explain how to set up things that are missing */
@@ -7,29 +7,29 @@ $current_directory = dirname(__FILE__);
 
 /* What page we are currently on - this is used to highlight the menu
  system as well as to not cache certain pages like the graphs */
-$self=$_SERVER['PHP_SELF'];
+$self = $_SERVER['PHP_SELF'];
 
 /* Load in the functions we may need - these are the list of available
  custom functions - for more information, read the comments in the
  functions.php file - most functions are in their own file in the
  functions subdirectory */
 if ($_GET['ajax'] == 1) {
-	header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
-	header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+    header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+    header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
     /* Functions will not have been included if there is no header*/
-	$url = "default";
-	require "/".$current_directory."/functions/functions.php";
+    $url = "default";
+    require "/".$current_directory."/functions/functions.php";
 }
 include "admin/db_config.php";
 ?>
-<meta http-equiv="Pragma" content="no-cache" />
+<meta http-equiv="Pragma" content="no-cache"/>
 <?
 mysql_select_db("SineDialer", $link) or die("Unable to connect: ".mysql_error());
 echo "<b>Database Statistics:<br /></b>";
 $result = mysql_query("SHOW STATUS");
 $version = mysql_get_server_info();
-$version = substr($version,0,strpos($version,"-"));
-$exp_version = explode(".",$version);
+$version = substr($version, 0, strpos($version, "-"));
+$exp_version = explode(".", $version);
 $questions = "Questions";
 if ($exp_version[0] >= 5 && $exp_version[1] >= 0 && $exp_version[2] >= 72) {
     $questions = "Queries";
@@ -40,7 +40,7 @@ if ($exp_version[0] >= 5 && $exp_version[1] >= 1) {
 if ($exp_version[0] >= 5) {
     $questions = "Queries";
 }
-while ($row = mysql_fetch_assoc($result)){
+while ($row = mysql_fetch_assoc($result)) {
     if ($row['Variable_name'] == 'Slow_queries') {
         $slow_queries = $row['Value'];
     }
@@ -49,61 +49,61 @@ while ($row = mysql_fetch_assoc($result)){
     }
     if ($row['Variable_name'] == 'Uptime') {
         $uptime = sec2hms($row['Value']);
-		$uptime_s = $row['Value'];
+        $uptime_s = $row['Value'];
     }
     if ($row['Variable_name'] == 'Connections') {
         $connections = number_format($row['Value']);
     }
 }
 $result = mysql_query("SELECT value FROM SineDialer.config WHERE parameter = 'mysql_queue'") or die(mysql_error());
-if (mysql_num_rows($result) > 0 && mysql_result($result,0,0) > 0) {
-    $pending = "<font color=\"red\"><b>Queued Writes: ".mysql_result($result,0,0)."</b></font> ";
+if (mysql_num_rows($result) > 0 && mysql_result($result, 0, 0) > 0) {
+    $pending = "<font color=\"red\"><b>Queued Writes: ".mysql_result($result, 0, 0)."</b></font> ";
 }
 $result = mysql_query("select count(*) from SineDialer.number where random_sort = 0 or random_sort = NULL") or die(mysql_error());
-if (mysql_result($result,0,0)>0) {
-	$unrandomized = "<font color=\"red\"><b>Unrandomized Numbers: ".mysql_result($result,0,0)."</b></font>";
+if (mysql_result($result, 0, 0) > 0) {
+    $unrandomized = "<font color=\"red\"><b>Unrandomized Numbers: ".mysql_result($result, 0, 0)."</b></font>";
 } else {
-	$unrandomized = "";
+    $unrandomized = "";
 }
 
-echo "Queries: ".number_format($questions)." (".number_format($questions/$uptime_s,3)."/sec) ".$pending."Uptime: $uptime $unrandomized<br />";
+echo "Queries: ".number_format($questions)." (".number_format($questions / $uptime_s, 3)."/sec) ".$pending."Uptime: $uptime $unrandomized<br />";
 
 $result = mysql_query("SHOW FULL PROCESSLIST") or die(mysql_error());
 $output = "";
 if (mysql_num_rows($result) > 0) {
     $count = 0;
     while ($row = mysql_fetch_assoc($result)) {
-    	if ($row['Command'] != 'Sleep' && $row['Time'] > 2) {
+        if ($row['Command'] != 'Sleep' && $row['Time'] > 2) {
             $count++;
-    		$output.= "<tr>";
-    		$link = '<td><a href="mysql_stats.php?kill='.$row['Id'].'"><img src="images/delete.png" border="0">&nbsp;Kill this process</a></td>';
-    		if ($row['State'] == 'Locked') {
-    			$output.= "<td><p align=\"left\" style=\"color:#ff0000\"><b>Locked:</b></font> $row[Info]<br /><b>Time:</b> ".sec2hms($row['Time'])." <b>Host:</b> ".$row['User']."@".$row['Host']."$link</td>";
-    		} else if ($row['State'] == 'copy to tmp table') {
-    			$output.= "<td><p align=\"left\" style=\"color:#880088\"><b>Copying to temp table:</b></font> $row[Info]<br /><b>Time:</b> ".sec2hms($row['Time'])." <b>Host:</b> ".$row['User']."@".$row['Host']."$link</td>";
-    		} else if ($row['State'] == 'updating' ||$row['State'] == 'Updating' ||$row['State'] == 'update') {
-    			$output.= "<td><p align=\"left\" style=\"color:#000000\"><b>Updating:</b> $row[Info]<br /><b>Time:</b> ".sec2hms($row['Time'])." <b>Host:</b> ".$row['User']."@".$row['Host']."$link</td>";
-    		} else if ($row['State'] == 'Sorting result') {
-    			$output.= "<td><p align=\"left\" style=\"color:#000000\"><b>Sorting result:</b> $row[Info]<br /> <b>Time:</b> ".sec2hms($row['Time'])." <b>Host:</b> ".$row['User']."@".$row['Host']."$link</td>";
-    		} else if ($row['State'] == 'Sending data') {
-    			$output .= "<td><p align=\"left\" style=\"color:#008800\"><b>Sending data:</b> $row[Info]<br /> <b>Time:</b> ".sec2hms($row['Time'])." <b>Host:</b> ".$row['User']."@".$row['Host']."$link</td>";
-    		} else {
-    			if ($row['Info'] != "SHOW PROCESSLIST") {
-    				$output.= "<td><p align=\"left\">";
-    				foreach ($row as $key=>$value) {
-    					$output.= "<b>Key:</b> $key <b>Value:</b> $value<br />";
-    				}
-    				$output.= "</p>$link</td>";
-    			}
-    		}
-    		$output.= "</tr>";
-    	}
+            $output .= "<tr>";
+            $link = '<td><a href="mysql_stats.php?kill='.$row['Id'].'"><img src="images/delete.png" border="0">&nbsp;Kill this process</a></td>';
+            if ($row['State'] == 'Locked') {
+                $output .= "<td><p align=\"left\" style=\"color:#ff0000\"><b>Locked:</b></font> $row[Info]<br /><b>Time:</b> ".sec2hms($row['Time'])." <b>Host:</b> ".$row['User']."@".$row['Host']."$link</td>";
+            } else if ($row['State'] == 'copy to tmp table') {
+                $output .= "<td><p align=\"left\" style=\"color:#880088\"><b>Copying to temp table:</b></font> $row[Info]<br /><b>Time:</b> ".sec2hms($row['Time'])." <b>Host:</b> ".$row['User']."@".$row['Host']."$link</td>";
+            } else if ($row['State'] == 'updating' || $row['State'] == 'Updating' || $row['State'] == 'update') {
+                $output .= "<td><p align=\"left\" style=\"color:#000000\"><b>Updating:</b> $row[Info]<br /><b>Time:</b> ".sec2hms($row['Time'])." <b>Host:</b> ".$row['User']."@".$row['Host']."$link</td>";
+            } else if ($row['State'] == 'Sorting result') {
+                $output .= "<td><p align=\"left\" style=\"color:#000000\"><b>Sorting result:</b> $row[Info]<br /> <b>Time:</b> ".sec2hms($row['Time'])." <b>Host:</b> ".$row['User']."@".$row['Host']."$link</td>";
+            } else if ($row['State'] == 'Sending data') {
+                $output .= "<td><p align=\"left\" style=\"color:#008800\"><b>Sending data:</b> $row[Info]<br /> <b>Time:</b> ".sec2hms($row['Time'])." <b>Host:</b> ".$row['User']."@".$row['Host']."$link</td>";
+            } else {
+                if ($row['Info'] != "SHOW PROCESSLIST") {
+                    $output .= "<td><p align=\"left\">";
+                    foreach ($row as $key => $value) {
+                        $output .= "<b>Key:</b> $key <b>Value:</b> $value<br />";
+                    }
+                    $output .= "</p>$link</td>";
+                }
+            }
+            $output .= "</tr>";
+        }
     }
     if ($count > 0) {
         echo "<br /><b>Long Running MySQL Threads:</b><br />";
-        echo "<br /><center><table border=\"0\" class=\"tborder\">";
+        echo "<br /><div class='table-responsive'><table border=\"0\" class=\"table table-striped\">";
         echo $output;
-        echo "</table>";
+        echo "</table></div>";
     }
 }
 ?>
--- mysql_stats.php
+++ mysql_stats.php
@@ -4,163 +4,152 @@ require "header_server.php";
 if (isset($_GET[convert_table])) {
     echo "Please wait - converting to InnoDB - this may take a while<br />";
     flush();
-    $result = mysql_query("ALTER TABLE SineDialer.".sanitize($_GET[convert_table],false)." ENGINE = InnoDB") or die (mysql_error());
+    $result = mysql_query("ALTER TABLE SineDialer.".sanitize($_GET[convert_table], FALSE)." ENGINE = InnoDB") or die (mysql_error());
     echo "<br /><b>Conversion completed</b><br />";
-    ?><META HTTP-EQUIV=REFRESH CONTENT="0; URL=mysql_stats.php"><?
+    ?>
+    <META HTTP-EQUIV=REFRESH CONTENT="0; URL=mysql_stats.php"><?
     exit(0);
 }
 if (isset($_GET[convert_back])) {
     echo "Please wait - converting to MyISAM - this may take a while<br />";
     flush();
-    $result = mysql_query("ALTER TABLE SineDialer.".sanitize($_GET[convert_back],false)." ENGINE = MyISAM") or die (mysql_error());
+    $result = mysql_query("ALTER TABLE SineDialer.".sanitize($_GET[convert_back], FALSE)." ENGINE = MyISAM") or die (mysql_error());
     echo "<br /><b>Conversion completed</b><br />";
-    ?><META HTTP-EQUIV=REFRESH CONTENT="0; URL=mysql_stats.php"><?
+    ?>
+    <META HTTP-EQUIV=REFRESH CONTENT="0; URL=mysql_stats.php"><?
     exit(0);
 }
 if (isset($_GET[kill])) {
-	$result = mysql_query("KILL $_GET[kill]") or die (mysql_error());
+    $result = mysql_query("KILL $_GET[kill]") or die (mysql_error());
 }
- function _get_browser()
-{
-  $browser = array ( //reversed array
-   "OPERA",
-   "MSIE",            // parent
-   "NETSCAPE",
-   "FIREFOX",
-   "SAFARI",
-   "KONQUEROR",
-   "MOZILLA"        // parent
-  );
+function _get_browser() {
+    $browser = array( //reversed array
+        "OPERA",
+        "MSIE",            // parent
+        "NETSCAPE",
+        "FIREFOX",
+        "SAFARI",
+        "KONQUEROR",
+        "MOZILLA"        // parent
+    );
 
-  $info[browser] = "OTHER";
+    $info[browser] = "OTHER";
 
-  foreach ($browser as $parent)
-  {
-   if ( ($s = strpos(strtoupper($_SERVER['HTTP_USER_AGENT']), $parent)) !== FALSE )
-   {
-     $f = $s + strlen($parent);
-     $version = substr($_SERVER['HTTP_USER_AGENT'], $f, 5);
-     $version = preg_replace('/[^0-9,.]/','',$version);
+    foreach ($browser as $parent) {
+        if (($s = strpos(strtoupper($_SERVER['HTTP_USER_AGENT']), $parent)) !== FALSE) {
+            $f = $s + strlen($parent);
+            $version = substr($_SERVER['HTTP_USER_AGENT'], $f, 5);
+            $version = preg_replace('/[^0-9,.]/', '', $version);
 
-     $info[browser] = $parent;
-     $info[version] = $version;
-     break; // first match wins
-   }
-  }
+            $info[browser] = $parent;
+            $info[version] = $version;
+            break; // first match wins
+        }
+    }
 
-  return $info;
+    return $info;
 }
-$level=$_COOKIE[level];
-$out=_get_browser();
-if ($out[browser]=="MSIE"){
-?>
-<script type="text/javascript" src="ajax/jquery.js"></script>
-<script type="text/javascript">
-        $(function(){ // jquery onload
-                window.setInterval(function(){
-                        $('#ajaxDiv').loadIfModified('mysql_details.php?ajax=1');  // jquery ajax load into div
-                },2000);
+
+$level = $_COOKIE[level];
+$out = _get_browser();
+if ($out[browser] == "MSIE") {
+    ?>
+    <script type="text/javascript" src="ajax/jquery.js"></script>
+    <script type="text/javascript">
+        $(function () { // jquery onload
+            window.setInterval(function () {
+                $('#ajaxDiv').loadIfModified('mysql_details.php?ajax=1');  // jquery ajax load into div
+            }, 2000);
         });
 
-</script>
-<?} else {?>
-<script type="text/javascript" src="ajax/jquery.js"></script>
-<script type="text/javascript">
+    </script>
+<? } else { ?>
+    <script type="text/javascript" src="ajax/jquery.js"></script>
+    <script type="text/javascript">
 
-        $(function(){ // jquery onload
-                window.setInterval(
-                function(){
-                        $('#ajaxDiv').load('mysql_details.php?ajax=1');  // jquery ajax load into div
+        $(function () { // jquery onload
+            window.setInterval(
+                function () {
+                    $('#ajaxDiv').load('mysql_details.php?ajax=1');  // jquery ajax load into div
                 }
-                ,2000);
+                , 2000);
         });
-</script>
-<?}
+    </script>
+    <?
+}
 box_start(700);
 echo "<center>";
 ?>
 
-<div id = "ajaxDiv">
-<?
-include "mysql_details.php";
-?>
+<div id="ajaxDiv">
+    <?
+    include "mysql_details.php";
+    ?>
 </div>
 
 <?
 box_end();
 $result = mysql_query("SHOW TABLE STATUS");
 ?>
-<center><table class="table-border table table-striped table-hover" cellspacing="1" boreder="0" cellpadding="5">
+<div class="table-responsive">
+    <table class="table-border table table-striped table-hover" cellspacing="1" boreder="0" cellpadding="5">
         <thead>
-<tr>
-<th>Name</th>
-    <th>Rows</th>
-    <th>Total Size</th>
-    <th>Last Update</th>
-    <th>Last Check</th>
-    <th>Engine</th>
-</tr>
+        <tr>
+            <th>Name</th>
+            <th>Rows</th>
+            <th>Total Size</th>
+            <th>Last Update</th>
+            <th>Last Check</th>
+            <th>Engine</th>
+        </tr>
         </thead>
         <tbody>
-<?
-while ($row = mysql_fetch_assoc($result)) {
+        <?
+        while ($row = mysql_fetch_assoc($result)) {
 //	print_pre($row);
-	$size = ($row['Data_length']+$row['Index_length'])/1048576;
-	$rows = $row[Rows];
-	if ($size > 0 && $rows > 0) {
-//		if ($toggle){
-//			$toggle=false;
-//			$class=" class=\"tborder2\"  onmouseover=\"style.backgroundColor='#84DFC1';\" onmouseout=\"style.backgroundColor='#f8f8f8'\"   ";
-//		} else {
-//			$toggle=true;
-//			$class=" class=\"tborderx\"  onmouseover=\"style.backgroundColor='#84DFC1';\" onmouseout=\"style.backgroundColor='#f0f0f0'\" ";
-//		}
-		echo "<tr ".$class.">";
+            $size = ($row['Data_length'] + $row['Index_length']) / 1048576;
+            $rows = $row[Rows];
+            if ($size > 0 && $rows > 0) {
+                echo "<tr ".$class.">";
 
-		if ($size < 1) {
-			$size *= 1024;
-			//$tdstyle = " style=\"background: #eeffee \"";
-			$size_text = "Kb";
-			$digits = 0;
-		} else if ($size<1024) {
-			//$tdstyle = " style=\"background: #ffdd88 url('images/grad_green.gif');\"";
-			$size_text = "Mb";
-			$digits = 1;
-		} else if ($size <10240) {
-			//$tdstyle = " style=\"background: #ff8844 url('images/grad_orange.gif');\"";
-			$size/=1024;
-	                $size_text = "Gb";
-			$digits = 2;
-		} else {
-			//$tdstyle = " style=\"background: #ff4444\"";
-			$size/=1024;
-	                $size_text = "Gb";
-			$digits = 4;
-		}
-		if ($row[Engine] == "InnoDB") {
-			if ($size_text != "Gb") {
-//				$tdstyle = " style=\"background: #8888ff url('images/grad_blue.png')\"";
-			}
-			echo "<td".$tdstyle.">$row[Name]</td>";
-			echo "<td".$tdstyle.">".number_format($rows)." (approx)</td>";
-			echo "<td".$tdstyle."><b>".number_format($size,$digits)." $size_text</b></td>";
-			echo "<td".$tdstyle." colspan=\"2\">$row[Comment]</td>";
-			echo "<td".$tdstyle.">";
-			echo "<b>".$row[Engine]."</b> <a href=\"mysql_stats.php?convert_back=$row[Name]\">Convert</a>";
-		} else {
-			echo "<td".$tdstyle.">$row[Name]</td>";
-			echo "<td".$tdstyle.">".number_format($rows)."</td>";
-			echo "<td".$tdstyle."><b>".number_format($size,$digits)." $size_text</b></td>";
-			$update_time = @strtotime($row[Update_time]);
-			$check_time = @strtotime($row[Check_time]);
-			echo "<td".$tdstyle.">".@date("D M j G:i:s Y", $update_time)."</td>";
-			echo "<td".$tdstyle.">".@date("D M j G:i:s Y", $check_time)."</td>";
-			echo "<td".$tdstyle.">";
-			echo "$row[Engine] <a href=\"mysql_stats.php?convert_table=$row[Name]\">Convert</a>";
-		}
-		echo "</td>";
-		echo "</tr>";
-	}
-}
-require "footer.php";
-?>
+                if ($size < 1) {
+                    $size *= 1024;
+                    $size_text = "Kb";
+                    $digits = 0;
+                } else if ($size < 1024) {
+                    $size_text = "Mb";
+                    $digits = 1;
+                } else if ($size < 10240) {
+                    $size /= 1024;
+                    $size_text = "Gb";
+                    $digits = 2;
+                } else {
+                    $size /= 1024;
+                    $size_text = "Gb";
+                    $digits = 4;
+                }
+                if ($row[Engine] == "InnoDB") {
+                    echo "<td".$tdstyle.">$row[Name]</td>";
+                    echo "<td".$tdstyle.">".number_format($rows)." (approx)</td>";
+                    echo "<td".$tdstyle."><b>".number_format($size, $digits)." $size_text</b></td>";
+                    echo "<td".$tdstyle." colspan=\"2\">$row[Comment]</td>";
+                    echo "<td".$tdstyle.">";
+                    echo "<b>".$row[Engine]."</b> <a href=\"mysql_stats.php?convert_back=$row[Name]\">Convert</a>";
+                } else {
+                    echo "<td".$tdstyle.">$row[Name]</td>";
+                    echo "<td".$tdstyle.">".number_format($rows)."</td>";
+                    echo "<td".$tdstyle."><b>".number_format($size, $digits)." $size_text</b></td>";
+                    $update_time = @strtotime($row[Update_time]);
+                    $check_time = @strtotime($row[Check_time]);
+                    echo "<td".$tdstyle.">".@date("D M j G:i:s Y", $update_time)."</td>";
+                    echo "<td".$tdstyle.">".@date("D M j G:i:s Y", $check_time)."</td>";
+                    echo "<td".$tdstyle.">";
+                    echo "$row[Engine] <a href=\"mysql_stats.php?convert_table=$row[Name]\">Convert</a>";
+                }
+                echo "</td>";
+                echo "</tr>";
+            }
+        }
+        echo "</table></div>";
+        require "footer.php";
+        ?>
