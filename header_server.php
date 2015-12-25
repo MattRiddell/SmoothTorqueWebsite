00f77ed58187b7f3fd7c7b50ebd69a66077bacf4
matt <MattRiddell@users.noreply.github.com>
2015-12-25 15:49:55 +0000

    Why not

--- header_server.php
+++ header_server.php
@@ -1,27 +1,19 @@
 <? box_start(); ?>
     <A HREF="addserver.php" class="btn btn-primary"><i class="glyphicon glyphicon-plus"></i> Add Asterisk Server</A>
-
-
     <A HREF="servers.php" class="btn btn-primary"><i class="glyphicon glyphicon-asterisk"></i> Asterisk Servers</A>
-
 <?
 if (isset($_GET['debug'])) {
     ?>
-
     <A HREF="freeswitch_servers.php" class="btn btn-primary"><i class="glyphicon glyphicon-list"></i> FreeSwitch Servers</A>
-
     <?
 }
 ?>
 <?
 if (strlen($config_values['SUGAR_HOST']) > 0) {
     ?>
-
     <A HREF="sugar_servers.php" class="btn btn-primary"><i class="glyphicon glyphicon-list"></i> SugarCRM Servers</A>
-
     <?
 }
 ?>
-
     <A HREF="mysql_stats.php" class="btn btn-primary"><i class="glyphicon glyphicon-hdd"></i> MySQL Status</A>
 <? box_end(); ?>
