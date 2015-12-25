d25a09de8feae1c4f9fd1a143ff2047e0a792158
matt <MattRiddell@users.noreply.github.com>
2015-12-25 05:10:47 +0000

    Add viewport etc

--- header.php
+++ header.php
@@ -560,6 +560,10 @@ if ($loggedin) {
 <html>
 
 <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <? /*<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">*/ ?>
     <TITLE><? echo stripslashes(stripslashes($config_values['TITLE'])); ?></TITLE>
