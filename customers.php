218382e69f9e0a278710e9a8571cbebf1e6f266f
matt <MattRiddell@users.noreply.github.com>
2015-12-25 05:44:59 +0000

    Make customers responsive

--- customers.php
+++ customers.php
@@ -24,136 +24,137 @@ if ($level != sha1("level100")) {
     $campaigngroupid = mysql_result($result, 0, 'campaigngroupid');
 //shadow_start();
     ?>
-
-    <table class="table table-bordered" align="center" border="0" cellpadding="2" cellspacing="0">
-        <TR>
-            <TD CLASS="">
-                Name
-            </TD>
-            <TD CLASS="">
-                Username
-            </TD>
-            <TD CLASS="">
-                Call Details
-            </TD>
-            <TD CLASS="">
-                City
-            </TD>
-            <TD CLASS="">
-                Country
-            </TD>
-            <TD CLASS="">
-                Phone
-            </TD>
-            <? if ($config_values['USE_BILLING'] == "YES") { ?>
+    <
+    <div class="table-responsive">
+        <table class="table table-bordered" align="center" border="0" cellpadding="2" cellspacing="0">
+            <TR>
                 <TD CLASS="">
-                    Credit
+                    Name
                 </TD>
                 <TD CLASS="">
-                    Credit Limit
+                    Username
                 </TD>
-            <?
-            } ?>
-            <TD CLASS="">
-                Trunk
-            </TD>
-            <TD CLASS="">
-            </TD>
-        </TR>
-        <?
-
-        $sql = 'SELECT customer.*,trunk.name AS trunkname, billing.credit as credit, billing.creditlimit as creditlimit FROM customer LEFT JOIN trunk ON customer.trunkid=trunk.id LEFT JOIN billing on customer.id=billing.customerid order by customer.company';
-        $result = mysql_query($sql, $link) or die (mysql_error());;
-        //$campaigngroupid=mysql_result($result,0,'campaigngroupid');
-        $count = 0;
-        while ($row = mysql_fetch_assoc($result)) {
-
-
-            $count++;
-            if ($toggle) {
-                $toggle = FALSE;
-                $class = " class=\"tborder2\"  onmouseover=\"style.backgroundColor='#84DFC1';\" onmouseout=\"style.backgroundColor='#f8f8f8'\"   ";
-            } else {
-                $toggle = TRUE;
-                $class = " class=\"tborderx\"  onmouseover=\"style.backgroundColor='#84DFC1';\" onmouseout=\"style.backgroundColor='#f0f0f0'\" ";
-            }
-
-            ?>
-            <TR <? echo $class; ?> >
-                <TD>
-                    <?
-                    if (strlen($row[company]) < 15) {
-                        echo "<A HREF=\"editcustomer.php?id=".$row[id]."\"><img src=\"images/pencil.png\" border=\"0\" align=\"right\" title=\"Edit\">".$row[company]."</A>&nbsp;";
-                    } else {
-                        echo "<A HREF=\"editcustomer.php?id=".$row[id]."\"><img src=\"images/pencil.png\" border=\"0\" align=\"right\" title=\"Edit\">".trim(substr($row[company], 0, 15))."...</A>";
-                    }
-                    ?>
+                <TD CLASS="">
+                    Call Details
                 </TD>
-                <TD>
-                    <?
-                    ?>
-                    <? echo "<A HREF=\"changepassword.php?id=".$row[id]."\" title=\"Change Password\"><img src=\"images/lock_edit.png\" border=\"0\" align=\"right\" title=\"Change Password\"></A>"; ?>
-                    <?
-                    if (strlen($row[username]) < 15) {
-                        echo $row[username];
-                    } else {
-                        echo trim(substr($row[username], 0, 15))."...";
-                    }
-                    ?>
+                <TD CLASS="">
+                    City
                 </TD>
-                <TD>
-                    <?
-                    echo '<A HREF="viewcdr.php?accountcode=stl-'.$row[username].'" title="View CDR Information" class="btn btn-primary">View CDR</A>'; ?>
+                <TD CLASS="">
+                    Country
                 </TD>
-                <TD>
-                    <? echo $row[city]; ?>
-                </TD>
-                <TD>
-                    <? echo $row[country]; ?>
-                </TD>
-                <TD>
-                    <?
-                    if (strlen($row[phone]) < 15) {
-                        echo $row[phone];
-                    } else {
-                        echo substr($row[phone], 15)."...";
-                    }
-                    ?>
+                <TD CLASS="">
+                    Phone
                 </TD>
                 <? if ($config_values['USE_BILLING'] == "YES") { ?>
-                    <TD>
-                        <?
-                        echo "<A HREF=\"billing.php?id=".$row[id]."\" title=\"View Billing Information\"><img src=\"images/cart_edit.png\" border=\"0\" align=\"right\" title=\"View Billing Information\">";
-                        if (isset($row[credit])) {
-                            echo $config_values['CURRENCY_SYMBOL']." ".number_format($row[credit], 2)."</A>";
-                        } else {
-                            echo "Add Billing</A>";
-                        }
-                        ?>
+                    <TD CLASS="">
+                        Credit
                     </TD>
-                    <TD>
-                        <?
-                        echo "<A HREF=\"billing.php?id=".$row[id]."\" title=\"View Billing Information\"><img src=\"images/cart_edit.png\" border=\"0\" align=\"right\" title=\"View Billing Information\">";
-                        if (isset($row[credit])) {
-                            echo $config_values['CURRENCY_SYMBOL']." ".number_format($row[creditlimit], 2)."</A>";
-                        } else {
-                            echo "Add Billing</A>";
-                        }
-                        ?>
+                    <TD CLASS="">
+                        Credit Limit
                     </TD>
-                <?
-                } ?>
-                <TD>
-
                     <?
-                    if (strlen(trim($row[trunkname])) < 1) {
-                        echo "Default";
-                    } else {
-                        echo "<b>".$row[trunkname]."</b>";
-                    }
-                    ?>
+                } ?>
+                <TD CLASS="">
+                    Trunk
                 </TD>
-                <?/*<TD>
+                <TD CLASS="">
+                </TD>
+            </TR>
+            <?
+
+            $sql = 'SELECT customer.*,trunk.name AS trunkname, billing.credit as credit, billing.creditlimit as creditlimit FROM customer LEFT JOIN trunk ON customer.trunkid=trunk.id LEFT JOIN billing on customer.id=billing.customerid order by customer.company';
+            $result = mysql_query($sql, $link) or die (mysql_error());;
+            //$campaigngroupid=mysql_result($result,0,'campaigngroupid');
+            $count = 0;
+            while ($row = mysql_fetch_assoc($result)) {
+
+
+                $count++;
+                if ($toggle) {
+                    $toggle = FALSE;
+                    $class = " class=\"tborder2\"  onmouseover=\"style.backgroundColor='#84DFC1';\" onmouseout=\"style.backgroundColor='#f8f8f8'\"   ";
+                } else {
+                    $toggle = TRUE;
+                    $class = " class=\"tborderx\"  onmouseover=\"style.backgroundColor='#84DFC1';\" onmouseout=\"style.backgroundColor='#f0f0f0'\" ";
+                }
+
+                ?>
+                <TR <? echo $class; ?> >
+                    <TD>
+                        <?
+                        if (strlen($row[company]) < 15) {
+                            echo "<A HREF=\"editcustomer.php?id=".$row[id]."\"><img src=\"images/pencil.png\" border=\"0\" align=\"right\" title=\"Edit\">".$row[company]."</A>&nbsp;";
+                        } else {
+                            echo "<A HREF=\"editcustomer.php?id=".$row[id]."\"><img src=\"images/pencil.png\" border=\"0\" align=\"right\" title=\"Edit\">".trim(substr($row[company], 0, 15))."...</A>";
+                        }
+                        ?>
+                    </TD>
+                    <TD>
+                        <?
+                        ?>
+                        <? echo "<A HREF=\"changepassword.php?id=".$row[id]."\" title=\"Change Password\"><img src=\"images/lock_edit.png\" border=\"0\" align=\"right\" title=\"Change Password\"></A>"; ?>
+                        <?
+                        if (strlen($row[username]) < 15) {
+                            echo $row[username];
+                        } else {
+                            echo trim(substr($row[username], 0, 15))."...";
+                        }
+                        ?>
+                    </TD>
+                    <TD>
+                        <?
+                        echo '<A HREF="viewcdr.php?accountcode=stl-'.$row[username].'" title="View CDR Information" class="btn btn-primary">View CDR</A>'; ?>
+                    </TD>
+                    <TD>
+                        <? echo $row[city]; ?>
+                    </TD>
+                    <TD>
+                        <? echo $row[country]; ?>
+                    </TD>
+                    <TD>
+                        <?
+                        if (strlen($row[phone]) < 15) {
+                            echo $row[phone];
+                        } else {
+                            echo substr($row[phone], 15)."...";
+                        }
+                        ?>
+                    </TD>
+                    <? if ($config_values['USE_BILLING'] == "YES") { ?>
+                        <TD>
+                            <?
+                            echo "<A HREF=\"billing.php?id=".$row[id]."\" title=\"View Billing Information\"><img src=\"images/cart_edit.png\" border=\"0\" align=\"right\" title=\"View Billing Information\">";
+                            if (isset($row[credit])) {
+                                echo $config_values['CURRENCY_SYMBOL']." ".number_format($row[credit], 2)."</A>";
+                            } else {
+                                echo "Add Billing</A>";
+                            }
+                            ?>
+                        </TD>
+                        <TD>
+                            <?
+                            echo "<A HREF=\"billing.php?id=".$row[id]."\" title=\"View Billing Information\"><img src=\"images/cart_edit.png\" border=\"0\" align=\"right\" title=\"View Billing Information\">";
+                            if (isset($row[credit])) {
+                                echo $config_values['CURRENCY_SYMBOL']." ".number_format($row[creditlimit], 2)."</A>";
+                            } else {
+                                echo "Add Billing</A>";
+                            }
+                            ?>
+                        </TD>
+                        <?
+                    } ?>
+                    <TD>
+
+                        <?
+                        if (strlen(trim($row[trunkname])) < 1) {
+                            echo "Default";
+                        } else {
+                            echo "<b>".$row[trunkname]."</b>";
+                        }
+                        ?>
+                    </TD>
+                    <?/*<TD>
 
  this week
 $sql = 'select count(*) from cdr.cdr where date(calldate)<=curdate() and date(calldate)>=DATE_ADD(CURDATE(), INTERVAL -7
@@ -177,17 +178,17 @@ $today = mysql_result($resultaa,0,0);
 echo "today: $today <br />";
 
 </TD>  */ ?>
-                <TD>
-                    <a href="#" onclick="displaySmallMessage('includes/confirmDeleteCustomer.php?id=<? echo $row[id]; ?>');return false" class="btn btn-warning"><i class="glyphicon glyphicon-remove"></i> Delete</a><br>
-                </TD>
-            </TR>
+                    <TD>
+                        <a href="#" onclick="displaySmallMessage('includes/confirmDeleteCustomer.php?id=<? echo $row[id]; ?>');return false" class="btn btn-warning"><i class="glyphicon glyphicon-remove"></i> Delete</a><br>
+                    </TD>
+                </TR>
 
-            <?
-        }
-        ?>
+                <?
+            }
+            ?>
 
-    </TABLE>
-
+        </TABLE>
+    </div>
     <?
     shadow_end();
     require "footer.php";
