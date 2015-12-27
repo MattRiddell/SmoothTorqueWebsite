020ec0202f02162ec4d1e81ebe79c75c573a130e
matt <MattRiddell@users.noreply.github.com>
2015-12-27 22:58:42 +0000

    Make the billing log bootstraped

--- billinglog.php
+++ billinglog.php
@@ -1,43 +1,37 @@
 <?
-    require "header.php";
-if (!isset($_GET[startdate])){
-?>
-<br />
-Please select the dates you would like to view:<br />
-<br />
-<form action="billinglog.php">
-From: <input name="startdate">
-<input type=button value="select" onclick="displayDatePicker('startdate', false, 'ymd', '-');"><BR>
-To: <input name="enddate">
-<input type=button value="select" onclick="displayDatePicker('enddate', false, 'ymd', '-');"><BR>
-<?if (isset($_GET[accountcode])) {?>
-<input type="hidden" name="accountcode" value="<?echo $_GET[accountcode];?>">
-<?}?>
-<input class="btn btn-primary" type="submit" value="Select">
-</form>
-<?
-} else {
-$startdate = $_GET[startdate];
-$enddate = $_GET[startdate];
-/*================= Log Access ======================================*/
-$sql = "INSERT INTO log (timestamp, username, activity) VALUES (NOW(), '$_COOKIE[user]', 'Viewed the billing logs')";
-$result=mysql_query($sql, $link);
-/*================= Log Access ======================================*/
-    $sql = "select * from billinglog where timestamp between '".$_GET['startdate']." 00:00:00' and '".$_GET['enddate']." 23:59:59' order by timestamp desc";
-    $result=mysql_query($sql, $link);
-    echo "<br /><center><table border=\"0\">";
-    echo "<tr><td>TimeStamp</td><td>Amount</td><td>Receipt</td><td>Payment Mode</td><td>User Name</td><td>Added By</td></tr>";
-    while ($row = mysql_fetch_assoc($result)) {
-    $class_bold=" class=\"tborder2x\"  onmouseover=\"style.backgroundColor='#C10000';\" onmouseout=\"style.backgroundColor='#FF6666'\"   ";
-    if ($toggle){
-$toggle=false;
-$class=" class=\"tborder2\"  onmouseover=\"style.backgroundColor='#84DFC1';\" onmouseout=\"style.backgroundColor='#f8f8f8'\"   ";
-} else {
-$toggle=true;
-$class=" class=\"tborderx\"  onmouseover=\"style.backgroundColor='#84DFC1';\" onmouseout=\"style.backgroundColor='#f0f0f0'\" ";
-}
+require "header.php";
+if (!isset($_GET[startdate])) {
+    ?>
+    <div class="jumbotron">
+        <h3>Billing Log</h3>
 
-        echo "<tr $class>";
+        <p>Please select the dates you would like to view</p>
+        <form action="billinglog.php">
+            From: <input name="startdate">
+            <input type=button value="select" onclick="displayDatePicker('startdate', false, 'ymd', '-');"><BR><BR>
+            To: <input name="enddate">
+            <input type=button value="select" onclick="displayDatePicker('enddate', false, 'ymd', '-');"><BR><BR>
+            <? if (isset($_GET[accountcode])) { ?>
+                <input type="hidden" name="accountcode" value="<? echo $_GET[accountcode]; ?>">
+            <? } ?>
+            <input class="btn btn-primary" type="submit" value="Select">
+        </form>
+    </div>
+    <?
+} else {
+    $startdate = $_GET[startdate];
+    $enddate = $_GET[startdate];
+    /*================= Log Access ======================================*/
+    $sql = "INSERT INTO log (timestamp, username, activity) VALUES (NOW(), '$_COOKIE[user]', 'Viewed the billing logs')";
+    $result = mysql_query($sql, $link);
+    /*================= Log Access ======================================*/
+    $sql = "select * from billinglog where timestamp between '".$_GET['startdate']." 00:00:00' and '".$_GET['enddate']." 23:59:59' order by timestamp desc";
+    $result = mysql_query($sql, $link);
+    echo '<div class="table-responsive">';
+    echo '<table class="table table-striped table-hover">';
+    echo "<tr><th>TimeStamp</th><th>Amount</th><th>Receipt</th><th>Payment Mode</th><th>User Name</th><th>Added By</th></tr>";
+    while ($row = mysql_fetch_assoc($result)) {
+        echo "<tr>";
         echo "<td>$row[timestamp]</td>";
         echo "<td>$row[activity]</td>";
         echo "<td>$row[receipt]</td>";
@@ -47,5 +41,6 @@ $class=" class=\"tborderx\"  onmouseover=\"style.backgroundColor='#84DFC1';\" on
         echo "</tr>";
     }
     echo "</table>";
+    echo "</div>";
 }
 ?>
