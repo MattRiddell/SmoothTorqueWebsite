b3d2da2e4af9af380dd33a113699b3ed18e4bb21
matt <MattRiddell@users.noreply.github.com>
2016-01-07 22:20:38 +0000

    Few changes

--- header_message.php
+++ header_message.php
@@ -1,24 +1,35 @@
-<?box_start();?>
-                <?
-                if ($config_values['DISABLE_MESSAGE_UPLOAD'] == "NO") {
+<? box_start(); ?>
+<?
+if ($config_values['DISABLE_MESSAGE_UPLOAD'] == "NO") {
+    if ($config_values['disable_all_types'] == "YES") {
 
-                    ?>
-                    
-                        <A HREF="uploadmessage.php?type=audio" class="btn btn-primary "><i class="glyphicon glyphicon-upload"></i> Upload New Audio Message</A>
-                    
-                    <? if ($config_values['disable_all_types'] != "YES") {
+        ?>
+        <A HREF="uploadmessage.php?type=audio" class="btn btn-primary "><i class="glyphicon glyphicon-upload"></i> Upload New Voicemail Message
+        </A>
+        <? if ($config_values['disable_all_types'] != "YES") {
+            ?>
+            <A HREF="uploadmessage.php?type=fax" class="btn btn-primary "><i class="glyphicon glyphicon-upload"></i> Upload New Fax Message</A>
+            <?
+        }
+        ?>
+        <A HREF="messages.php" class="btn btn-primary "><i class="glyphicon glyphicon-list"></i> View All Voicemail Messages</A>
+    <? } else {
 
-                        ?>
-                        
-                            <A HREF="uploadmessage.php?type=fax" class="btn btn-primary "><i class="glyphicon glyphicon-upload"></i> Upload New Fax Message</A>
-                        
-                        <?
-                    }
-                    ?>
-                    <?
-                }
-                ?>
-                
-                    <A HREF="messages.php" class="btn btn-primary "><i class="glyphicon glyphicon-list"></i> View All Messages</A>
-                
-<?box_end();?>
+        ?>
+        <A HREF="uploadmessage.php?type=audio" class="btn btn-primary "><i class="glyphicon glyphicon-upload"></i> Upload New Audio Message
+        </A>
+        <? if ($config_values['disable_all_types'] != "YES") {
+            ?>
+            <A HREF="uploadmessage.php?type=fax" class="btn btn-primary "><i class="glyphicon glyphicon-upload"></i> Upload New Fax Message</A>
+            <?
+        }
+        ?>
+        <A HREF="messages.php" class="btn btn-primary "><i class="glyphicon glyphicon-list"></i> View All Messages</A>
+        <?
+    }
+    ?>
+
+
+    <?
+}
+box_end(); ?>
