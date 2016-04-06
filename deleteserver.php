15ffd1369d03055ccd35d1a15294059024148242
matt <MattRiddell@users.noreply.github.com>
2016-04-06 18:52:24 +0000

    Remove bad code

--- deleteserver.php
+++ deleteserver.php
@@ -1,46 +1,51 @@
 <?
-include "admin/db_config.php";//mysql_connect('localhost', 'root', '') OR die(mysql_error());
-mysql_select_db("SineDialer", $link);
+require "header.php";
 
-$_GET = array_map(mysql_real_escape_string,$_GET);
-$_POST = array_map(mysql_real_escape_string,$_POST);
 
-if (isset($_GET[sure])){
-    $id=($_GET[id]);
-    $sql="DELETE FROM servers where id=$id limit 1";
-    $result=mysql_query($sql, $link) or die (mysql_error());;
-/*================= Log Access ======================================*/
-$sql = "INSERT INTO log (timestamp, username, activity) VALUES (NOW(), '$_COOKIE[user]', 'Deleted a server')";
-$result=mysql_query($sql, $link);
-/*================= Log Access ======================================*/
+$_GET = array_map(mysql_real_escape_string, $_GET);
+$_POST = array_map(mysql_real_escape_string, $_POST);
 
-    include("servers.php");
+if (isset($_GET[sure])) {
+    $id = ($_GET[id]);
+    $sql = "DELETE FROM servers where id=$id limit 1";
+    $result = mysql_query($sql, $link) or die (mysql_error());;
+    /*================= Log Access ======================================*/
+    $sql = "INSERT INTO log (timestamp, username, activity) VALUES (NOW(), '$_COOKIE[user]', 'Deleted a server')";
+    $result = mysql_query($sql, $link);
+    /*================= Log Access ======================================*/
+
+    redirect("servers.php");
     exit;
 }
-require "header.php";
 require "header_server.php";
 
 ?>
 <table class="tborder" align="center" border="0" cellpadding="0" cellspacing="2">
-<TR><TD>
-Are you Sure You want to delete this record?<BR><BR>
-</TD></TR>
-<TR><TD>
-<?
+    <TR>
+        <TD>
+            Are you Sure You want to delete this record?<BR><BR>
+        </TD>
+    </TR>
+    <TR>
+        <TD>
+            <?
 
-$sql = 'SELECT * FROM servers WHERE id='.$_GET[id];
-$result=mysql_query($sql, $link) or die (mysql_error());;
-while ($row = mysql_fetch_assoc($result)) {
-    echo "<CENTER><B>".$row[name]."</B><BR><BR>";
-    echo '<A HREF="deleteserver.php?id='.$_GET[id].'&sure=yes">Yes, Delete it</A><BR>';
-    echo '<A HREF="servers.php">No, Don\'t Delete It</A></CENTER>';
-?>
-</TD></TR>
-<TR><TD>
+            $sql = 'SELECT * FROM servers WHERE id='.$_GET[id];
+            $result = mysql_query($sql, $link) or die (mysql_error());;
+            while ($row = mysql_fetch_assoc($result)) {
+            echo "<CENTER><B>".$row[name]."</B><BR><BR>";
+            echo '<A HREF="deleteserver.php?id='.$_GET[id].'&sure=yes">Yes, Delete it</A><BR>';
+            echo '<A HREF="servers.php">No, Don\'t Delete It</A></CENTER>';
+            ?>
+        </TD>
+    </TR>
+    <TR>
+        <TD>
 
-</TD></TR>
+        </TD>
+    </TR>
 </TABLE>
-</FORM>
+    </FORM>
 <?
 }
 require "footer.php";
--- deletetrunk.php
+++ deletetrunk.php
@@ -1,11 +1,10 @@
 <?
-include "admin/db_config.php";//mysql_connect('localhost', 'root', '') OR die(mysql_error());
-mysql_select_db("SineDialer", $link);
+require "header.php";
 
 $_POST = array_map(mysql_real_escape_string,$_POST);
 $_GET = array_map(mysql_real_escape_string,$_GET);
 
-if (isset($_GET[id])){
+if (isset($_GET['id'])){
 
 $count = 0;
 $sql="SELECT count(*) from customer where trunkid=".($_GET[id]);
@@ -13,7 +12,7 @@ $result=mysql_query($sql, $link) or die (mysql_error());
 $count+=mysql_result($result,0,0);
 
 if ($count > 0) {
-    require "header.php";
+
     echo "<br /><br />Sorry this trunk is currently being used by a campaign";
 ?>    <meta http-equiv="refresh" content="3;url=trunks.php"><?
 } else {
@@ -28,7 +27,7 @@ $sql = "INSERT INTO log (timestamp, username, activity) VALUES (NOW(), '$_COOKIE
 $result=mysql_query($sql, $link);
 /*================= Log Access ======================================*/
 
-    include("trunks.php");
+    redirect("trunks.php");
     exit;
 }
 }
