7280de89032b531305382471cb1a2a6ce6115e97
matt <MattRiddell@users.noreply.github.com>
2016-04-06 18:55:20 +0000

    Remove bad code

--- setdefault.php
+++ setdefault.php
@@ -1,22 +1,21 @@
 <?
-include "admin/db_config.php";//mysql_connect('localhost', 'root', '') OR die(mysql_error());
-mysql_select_db("SineDialer", $link);
+require "header.php";
 
-$_POST = array_map(mysql_real_escape_string,$_POST);
-$_GET = array_map(mysql_real_escape_string,$_GET);
+$_POST = array_map(mysql_real_escape_string, $_POST);
+$_GET = array_map(mysql_real_escape_string, $_GET);
 
-if (isset($_GET[id])){
-    $id=$_GET[id];
-    $sql="update trunk set current=0";
-    $result=mysql_query($sql, $link) or die (mysql_error());;
-    $sql="update trunk set current=1 where id=$_GET[id]";
-    $result=mysql_query($sql, $link) or die (mysql_error());;
-/*================= Log Access ======================================*/
-$sql = "INSERT INTO log (timestamp, username, activity) VALUES (NOW(), '$_COOKIE[user]', 'Changed default trunk')";
-$result=mysql_query($sql, $link);
-/*================= Log Access ======================================*/
+if (isset($_GET[id])) {
+    $id = $_GET[id];
+    $sql = "update trunk set current=0";
+    $result = mysql_query($sql, $link) or die (mysql_error());;
+    $sql = "update trunk set current=1 where id=$_GET[id]";
+    $result = mysql_query($sql, $link) or die (mysql_error());;
+    /*================= Log Access ======================================*/
+    $sql = "INSERT INTO log (timestamp, username, activity) VALUES (NOW(), '$_COOKIE[user]', 'Changed default trunk')";
+    $result = mysql_query($sql, $link);
+    /*================= Log Access ======================================*/
 
-    include("trunks.php");
+    redirect("trunks.php");
     exit;
 }
 require "footer.php";
