0d6087e5e0af0c9ac804a59b70da96c7069da7c8
matt <MattRiddell@users.noreply.github.com>
2016-04-18 18:37:42 +0000

    Change

--- addbilling.php
+++ addbilling.php
@@ -6,7 +6,7 @@ $sql = 'SELECT campaigngroupid FROM customer WHERE username=\''.$_COOKIE[user].'
 $result = mysql_query($sql, $link) or die (mysql_error());;
 $campaigngroupid = mysql_result($result, 0, 'campaigngroupid');
 
-$sql = "INSERT INTO billing (customerid, accountcode) VALUES (".sanitize($_GET['id']).", '".sanitize($_GET['accountcode'])."')";
+$sql = "INSERT INTO billing (customerid, accountcode) VALUES (".sanitize($_GET['id']).", ".sanitize($_GET['accountcode']).")";
 $result = mysql_query($sql, $link) or die (mysql_error());;
 /*================= Log Access ======================================*/
 $sql = "INSERT INTO log (timestamp, username, activity) VALUES (NOW(), '".sanitize($_COOKIE['user'])."', 'Added a billing record')";
